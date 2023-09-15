a28e8ee8cdeafca6786fb31a3f3c2b70dc495fa2
HiveerLi <ljp.hiveer@gmail.com>
2023-09-15 20:38:49 +0800

    Integrate authing api

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ gem "jwt"
 # Aliyun OSS SDK
 gem 'aliyun-sdk'
 
+gem 'faraday'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'rspec-rails'
---
+++ app/services/authing.rb
@@ -0,0 +1,60 @@
+class Authing
+  include Singleton
+
+  API_DOMAIN = "https://console.authing.cn"
+
+  def get_access_token
+    endpoint = '/api/v3/get-management-token'
+    res = authing_connection.post(endpoint) do |req|
+      req.body = {
+                    accessKeyId: access_key,
+                    accessKeySecret: access_secret
+                  }.to_json
+    end
+    JSON.parse(res.body)["data"]["access_token"]
+  end
+
+  def update_userinfo options
+    endpoint = '/api/v3/update-user'
+    authing_connection.post(endpoint) do |req|
+      req.headers['authorization'] = get_access_token
+      req.headers['x-authing-userpool-id'] = access_key
+      req.body = options.to_json
+    end
+  end
+
+  def get_useinfo user_id
+    endpoint = '/api/v3/get-user'
+    res = authing_connection.get(endpoint) do |req|
+      req.headers['authorization'] = get_access_token
+      req.headers['x-authing-userpool-id'] = access_key
+      req.params['userId'] = user_id
+    end
+    JSON.parse(res.body)["data"]
+  end
+
+  private
+
+  def access_key
+    if Rails.env.development?
+      Rails.application.credentials.authing_api.development.access_key
+    else
+      Rails.application.credentials.authing_api.production.access_key
+    end
+  end
+
+  def access_secret
+    if Rails.env.development?
+      Rails.application.credentials.authing_api.development.access_secret
+    else
+      Rails.application.credentials.authing_api.production.access_secret
+    end
+  end
+
+  def authing_connection
+    Faraday.new(
+      url: API_DOMAIN,
+      headers: {'Content-Type' => 'application/json'}
+    )
+  end
+end
