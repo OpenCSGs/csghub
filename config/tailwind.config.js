b2430bb15c4bc110b144b5e8146ebfe83fbfa21a
HiveerLi <ljp.hiveer@gmail.com>
2023-09-25 14:08:19 +0800

    Customize the media query break point to use max-width

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
   },
   plugins: [],
 }
