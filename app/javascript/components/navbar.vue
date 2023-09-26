5dd2f6ff94e9da0f55e30122b5e7c6b9b427cc58
HiveerLi <ljp.hiveer@gmail.com>
2023-09-26 22:40:13 +0800

    Setup logout link for navbar

--- app/javascript/components/navbar.vue
+++ app/javascript/components/navbar.vue
@@ -31,14 +31,50 @@
           </el-dropdown-menu>
         </template>
       </el-dropdown>
-      <el-avatar :size="40" src="https://empty">
-        <img
-          src="/assets/opencsg_logo.png"
-        />
-      </el-avatar>
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          <el-avatar :size="40" src="https://empty">
+            <img src="/assets/opencsg_logo.png" />
+          </el-avatar>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>
+              <a :href="logout" @click="cleanUpAuthing" class="text-gray-800 dark:text-white bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2 lg:px-3 py-2 lg:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"> Logout </a>
+            </el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item disabled>Action 4</el-dropdown-item>
+            <el-dropdown-item divided>Action 5</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    logout: String
+  },
+  data() {
+    return { }
+  },
+  methods: {
+    cleanUpAuthing() {
+      localStorage.removeItem('_guard_i18nextLng')
+      localStorage.removeItem('_authing_token')
+      localStorage.removeItem('_authing_user')
+      localStorage.removeItem('__authing__multiple_accounts')
+    }
+  },
+  mounted() {
+    console.log(this.logout)
+  }
+}
+</script>
+
 <style>
   .el-dropdown {
     color: white;
