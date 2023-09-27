787cd6c6e1961c37d84477f0ffb3fc7ea902dbcc
HiveerLi <ljp.hiveer@gmail.com>
2023-09-27 19:40:05 +0800

    Load admin js css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link administrate/application.css
+//= link administrate/application.js
