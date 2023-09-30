c408fdc48693d3277f24f6e0370800a3e2a9c0d8
HiveerLi <ljp.hiveer@gmail.com>
2023-09-30 15:19:47 +0800

    New field for space to save cover image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_005010) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_30_071720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_005010) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover_image"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
