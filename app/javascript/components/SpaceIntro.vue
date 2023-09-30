591814888c512a4428f855fe1b45cb01d663c579
HiveerLi <ljp.hiveer@gmail.com>
2023-09-30 09:28:52 +0800

    Layout for no spaces case

--- app/javascript/components/SpaceIntro.vue
+++ app/javascript/components/SpaceIntro.vue
@@ -4,7 +4,7 @@
       嗨！这里是开放的大模型应用空间。
     </p>
     <p>
-      您可以基于 <a href="https://starchain-stg.opencsg.com/" class="text-[#1a8b7b] underline"> StarChain</a> 快速构建你的专属大模型应用，并发布到 OpenCSG 大模型应用空间，分享给社区/朋友/同事，支持托管和API调用。
+      您可以基于 <a href="https://starchain-stg.opencsg.com/" target="_blank" class="text-[#1a8b7b] underline"> StarChain</a> 快速构建你的专属大模型应用，并发布到 OpenCSG 大模型应用空间，分享给社区/朋友/同事，支持托管和API调用。
     </p>
     <p>
       更多玩法 等你发现！
