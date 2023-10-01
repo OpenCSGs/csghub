582daa249c730d09a0b29f0adeb462a48cf9daf3
HiveerLi <ljp.hiveer@gmail.com>
2023-10-01 17:39:43 +0800

    Update column type change migration

--- db/migrate/20230930092558_change_space_type_to_integer_for_space.rb
+++ db/migrate/20230930092558_change_space_type_to_integer_for_space.rb
@@ -1,5 +1,5 @@
 class ChangeSpaceTypeToIntegerForSpace < ActiveRecord::Migration[7.0]
   def change
-    change_column :spaces, :space_type, :integer, using: 'space_type::integer'
+    change_column :spaces, :space_type, 'integer USING CAST(space_type AS integer)
   end
 end
