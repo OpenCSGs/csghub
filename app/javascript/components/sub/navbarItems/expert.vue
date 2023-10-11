2287d1a2838799989501ea7fd31c92cd8fbe2019
Hiveer <ljp.hiveer@gmail.com>
2023-10-11 14:58:09 +0800

    New items in navbar and also refactor the code

---
+++ app/javascript/components/sub/navbarItems/expert.vue
@@ -0,0 +1,9 @@
+<template>
+  <a href="" class="flex items-center hover:text-[#cc00cc]">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+      <path d="M16.5 9C16.5 13.1421 13.1421 16.5 9 16.5C4.85786 16.5 1.5 13.1421 1.5 9C1.5 4.85786 4.85786 1.5 9 1.5C13.1421 1.5 16.5 4.85786 16.5 9Z" stroke="#2F384C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M10.5 6C10.5 6.82843 9.82843 7.5 9 7.5C8.17157 7.5 7.5 6.82843 7.5 6C7.5 5.17157 8.17157 4.5 9 4.5C9.82843 4.5 10.5 5.17157 10.5 6Z" stroke="#2F384C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    <span class="ml-[8px]">技术专家</span>
+  </a>
+</template>
