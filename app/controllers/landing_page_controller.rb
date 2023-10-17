b9fc5fc73404e2993ca001e1d138f24ac9b85b54
Hiveer <ljp.hiveer@gmail.com>
2023-10-17 10:56:47 +0800

    Models, datasets and latest spaces for landing page

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -11,5 +11,6 @@ class LandingPageController < ApplicationController
     @newsData = [{url:'partners/datawhale.png',text:'By using natural language to achieve the integration and utilization of hybrid cloud resources, natural language processing technology can simplify workflows, reduce IT infrastructure costs, and improve collaboration efficiency between teams.'},
                  {url:'partners/aws.png',text:'By using natural language to achieve the integration and utilization of hybrid cloud resources, natural language processing technology can simplify workflows, reduce IT infrastructure costs, and improve collaboration efficiency between teams.'},
                  {url:'partners/nvidia.png',text:'By using natural language to achieve the integration and utilization of hybrid cloud resources, natural language processing technology can simplify workflows, reduce IT infrastructure costs, and improve collaboration efficiency between teams.'}]
+    @latest_spaces = Space.where(space_type: 'public_s').order(created_at: :desc).limit(3)
   end
 end
