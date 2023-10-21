7683d981dd01bc6f33ebd72b86deb49f4e842772
Hiveer <ljp.hiveer@gmail.com>
2023-10-21 09:52:23 +0800

    Add more test cases for PartnerCard

--- app/javascript/components/__tests__/PartnerCard.test.js
+++ app/javascript/components/__tests__/PartnerCard.test.js
@@ -17,5 +17,13 @@ describe('WeatherBanner.vue Implementation Test', () => {
     })
 
     expect(wrapper.vm.theTags).toEqual(expect.arrayContaining(['name', 'age', 'weight']))
+    const tags = wrapper.findAll('span')
+    expect(tags.length).toEqual(3)
+    expect(tags[0].text()).toMatch('name')
+    expect(tags[1].text()).toMatch('age')
+    expect(tags[2].text()).toMatch('weight')
+    const partnerNameLink = wrapper.find('[href="test_link"]')
+    expect(partnerNameLink.text()).toMatch('test')
+    expect(partnerNameLink.attributes().target).toMatch('_blank')
   })
 })
