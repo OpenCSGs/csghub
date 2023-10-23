bf25f39a69d8a5e810582d32a3fb91ae3141750e
Hiveer <ljp.hiveer@gmail.com>
2023-10-23 19:26:29 +0800

    First Rspec unit test for Rails

---
+++ spec/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    login_identity { 'uuid123401' }
+    name { 'Joe' }
+    phone { '17708176699' }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -60,4 +61,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
