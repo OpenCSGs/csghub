daffc8d51b62327f80ef0e3611dd5b217599e0c3
ymh6315431 <yangminghao6315431@163.com>
2023-10-24 13:05:28 +0800

    UserInfo css

--- app/javascript/components/profile/UserInfo.vue
+++ app/javascript/components/profile/UserInfo.vue
@@ -1,38 +1,38 @@
 <template>
-  <div>
-    <!-- <div>
-      <el-avatar :size="56" :src="avatar"> </el-avatar>
-    </div> -->
-    <div>User Name</div>
-    <div>@handle</div>
-    <div class="flex">
-        <div>角色</div>
+    <div>
+      <div>
+       <el-avatar :size="120" :src="avatar"> </el-avatar>
+      </div>
+      <div class="text-[24px] leading-[32px] font-semibold">User Name</div>
+      <div class="text-[16px] text-[#909399] leading-[24px] mb-[24px]">@handle</div>
+      <div class="flex text-[#606266] text-[16px] leading-[24px] gap-[24px] mb-[16px]">
+        <div class="w-[64px]">角色</div>
         <div>Developer</div>
-    </div>
-    <div class="flex">
-        <div>电话</div>
+      </div>
+      <div class="flex text-[#606266] text-[16px] leading-[24px] gap-[24px] mb-[16px]">
+        <div class="w-[64px]">电话</div>
         <div>185 8585 8585</div>
-    </div>
-    <div class="flex">
-        <div>邮箱</div>
+      </div>
+      <div class="flex text-[#606266] text-[16px] leading-[24px] gap-[24px] mb-[16px]">
+        <div class="w-[64px]">邮箱</div>
         <div>username@163.com</div>
-        <div>验证</div>
-    </div>
-    <div class="flex">
-        <div>最后登录</div>
+        <div class="px-[12px] py-[2px] border border-[#DCDFE6] rounded-[4px] text-[12px] leading-[20px] font-medium">验证</div>
+      </div>
+      <div class="flex mb-[24px] text-[#606266] text-[16px] leading-[24px] gap-[24px]">
+        <div class="w-[64px]">最后登录</div>
         <div>2023.08.24 12:00:00</div>
+      </div>
+      <span class="px-[12px] py-[5px] border border-[#DCDFE6] rounded-[4px] text-[14px] leading-[22px] font-medium">Edit profile</span>
     </div>
-    <div>Edit profile</div>
-  </div>
 </template>
 <script>
 export default {
   props: {
-    name:String,
-    tel:String,
-    avatar:String,
-    mail:String,
-    lastTime:String,
+    name: String,
+    tel: String,
+    avatar: String,
+    mail: String,
+    lastTime: String,
   },
   data() {
     return {};
