b64af13203b30087e0515562a06396d2dd1095c9
ymh6315431 <yangminghao6315431@163.com>
2023-10-24 15:39:07 +0800

    UserInfo user

--- app/javascript/components/profile/UserInfo.vue
+++ app/javascript/components/profile/UserInfo.vue
@@ -3,11 +3,11 @@
       <div>
        <el-avatar :size="120" :src="avatar"> </el-avatar>
       </div>
-      <div class="text-[24px] leading-[32px] font-semibold">User Name</div>
-      <div class="text-[16px] text-[#909399] leading-[24px] mb-[24px]">@handle</div>
+      <div class="text-[24px] leading-[32px] font-semibold">{{name}}</div>
+      <!-- <div class="text-[16px] text-[#909399] leading-[24px] mb-[24px]">@handle</div> -->
       <div class="flex text-[#606266] text-[16px] leading-[24px] gap-[24px] mb-[16px]">
         <div class="w-[64px]">角色</div>
-        <div>Developer</div>
+        <div>{{user}}</div>
       </div>
       <div class="flex text-[#606266] text-[16px] leading-[24px] gap-[24px] mb-[16px]">
         <div class="w-[64px]">电话</div>
@@ -33,10 +33,12 @@ export default {
     avatar: String,
     email: String,
     lastTime: String,
+    roles:String
   },
   data() {
     return {
-        time:this.lastTime.slice(0, -6)
+        time:this.lastTime?this.lastTime.slice(0, -6):'',
+        user:this.roles?JSON.parse(this.roles)[0]:[]
     };
   },
   mounted() {},
