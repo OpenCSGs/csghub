4bd55c8253fb7f71636033638ae8bee9576d5921
zxr <1902859534@qq.com>
2023-10-31 18:17:23 +0800

    Code the tagging-model rspec

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tagging, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:tag) }
+    it { should belong_to(:space).optional(true) }
+  end
 end
