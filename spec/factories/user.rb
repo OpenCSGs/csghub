23db23f694e88a6b88da1ad582e835d37f6d2dfb
zxr <1902859534@qq.com>
2023-10-31 18:18:29 +0800

    Code the campaign-model rspec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     login_identity { 'uuid123401' }
     name { 'Joe' }
     phone { '17708176699' }
-
+    avatar { 'avatar/HvbRkkwo' }
   end
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:campaign) { build(:campaign) }
+
+  describe 'associations' do
+    it { should have_rich_text(:content) }
+  end
+
+  describe 'before_create callback' do
+    it 'sets a UUID before creating a campaign' do
+      campaign.save
+      expect(campaign.uuid).not_to be_nil
+      expect(campaign.uuid.length).to eq(12)
+    end
+  end
 end
