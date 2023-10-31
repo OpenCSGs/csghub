0ef3c79767f9761f7363e71d3c9af75204a77fe8
zxr <1902859534@qq.com>
2023-10-31 18:16:43 +0800

    Code the tag-model rspec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:tag) { build(:tag) }
+
+  describe 'associations' do
+    it { should have_many(:taggings).dependent(:destroy) }
+    it { should have_many(:spaces).through(:taggings) }
+  end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:name) }
+  # end
+
+  describe 'DEFAULT_TAGS' do
+    it 'contains the expected default tags' do
+      expected_tags = [
+        { name: '语言模型', color: '#009933' },
+        { name: '图片模型', color: '#ff9900' },
+        { name: '多模态', color: '#cc0066' },
+        { name: '智能体', color: '#cc33ff' }
+      ]
+
+      expect(Tag::DEFAULT_TAGS).to match_array(expected_tags)
+    end
+
+    describe 'as_json' do
+      it 'returns a JSON representation of the tag' do
+        expected_json = { name: 'tag', color: '#ff0000' }
+        expect(tag.as_json).to eq(expected_json)
+      end
+    end
+  end
+
+
+
 end
