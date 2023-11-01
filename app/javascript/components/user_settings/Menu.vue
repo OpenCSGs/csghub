54b55bf7563b1a5c4494530695f030bf94e3b141
ymh6315431 <yangminghao6315431@163.com>
2023-11-01 10:26:50 +0800

    menuclass

--- app/javascript/components/user_settings/Menu.vue
+++ app/javascript/components/user_settings/Menu.vue
@@ -7,10 +7,10 @@
         <div class="text-[24px] leading-[32px] font-semibold mb-[41px]">{{name}}</div>
       <!-- <div class="text-[16px] text-[#909399] leading-[24px] mb-[24px]">@handle</div> -->
         <div class="flex flex-col gap-[16px]">
-          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/profile')"">Profile</div>
-          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/account')"">Account</div>
-          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/password')"">Password</div>
-          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/lastLogin')"">最后登录</div>
+          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/profile')">Profile</div>
+          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/account')">Account</div>
+          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/password')">Password</div>
+          <div class="max-w-[73px] text-[16px] text-[#606266] leading-[24px] cursor-pointer" :class="menuClass('/settings/lastLogin')">最后登录</div>
          </div>
         </div>
     </div>
