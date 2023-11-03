2e84a88ac7e02a6246943b1a30c8e41d5b7e0293
ymh6315431 <yangminghao6315431@163.com>
2023-11-03 16:33:58 +0800

    profile bg

--- app/javascript/components/user_settings/Profile.vue
+++ app/javascript/components/user_settings/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-[54px] md:px-0 sm:px-0">
+  <div class="pl-[80px] sm:mt-[20px] md:mt-[50px] pr-[106px] md:px-0 sm:px-0 sm:w-full py-[36px] bg-[#FAFAFA]">
     <div>
       <el-avatar :size="120" :src="avatar"> </el-avatar>
     </div>
