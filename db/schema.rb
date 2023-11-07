a60e00ba5455869a40b3976afea78d1070a0a188
Hiveer <ljp.hiveer@gmail.com>
2023-11-07 10:24:26 +0800

    Add status to campaign

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_03_025328) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_020819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_03_025328) do
     t.string "organizer_website"
     t.integer "pageviews"
     t.integer "campaign_type"
+    t.integer "status", default: 0
     t.index ["uuid"], name: "index_campaigns_on_uuid"
   end
 
