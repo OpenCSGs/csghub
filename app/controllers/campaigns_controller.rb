73efbdafd000db06dcba0376f4aef6a3e809c443
Hiveer <ljp.hiveer@gmail.com>
2023-11-07 16:43:20 +0800

    Find and show the related applications for a campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,6 @@
 class CampaignsController < ApplicationController
   def show
     @campaign = Campaign.find params[:id]
+    @related_spaces = Space.left_joins(:tags).where(tags: {name: @campaign.name})
   end
 end
