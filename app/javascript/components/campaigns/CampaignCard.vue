0ab97bb12fd24a3e01aa38f98310fbf989556917
Hiveer <ljp.hiveer@gmail.com>
2023-11-08 16:35:28 +0800

    Move campaigns related component to sub directory

--- app/javascript/components/campaigns/CampaignCard.vue
+++ app/javascript/components/campaigns/CampaignCard.vue
@@ -11,12 +11,12 @@
         </div>
         <div class="flex items-center">
           <svg class="mr-1" xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none"><path d="M2.6665 12.6785C2.6665 10.9112 4.09919 9.47852 5.8665 9.47852H10.1332C11.9005 9.47852 13.3332 10.9112 13.3332 12.6785V12.6785C13.3332 13.8567 12.378 14.8118 11.1998 14.8118H4.79984C3.62163 14.8118 2.6665 13.8567 2.6665 12.6785V12.6785Z" stroke="#909399" stroke-linecap="round" stroke-linejoin="round"/><path d="M10.6665 4.14518C10.6665 5.61794 9.4726 6.81185 7.99984 6.81185C6.52708 6.81185 5.33317 5.61794 5.33317 4.14518C5.33317 2.67242 6.52708 1.47852 7.99984 1.47852C9.4726 1.47852 10.6665 2.67242 10.6665 4.14518Z" stroke="#909399" stroke-linecap="round" stroke-linejoin="round"/></svg>
-          {{ leads_count }}
+          {{ leadsCount }}
         </div>
       </div>
     </div>
     <div class="text-[#909399] text-[14px] leading-[22px] mb-1">
-      {{ start_date }} ~ {{ end_date }}
+      {{ startDate }} ~ {{ endDate }}
     </div>
     <div class="text-[#303133] text-[14px] leading-[22px] w-[60%] overflow-hidden overflow-ellipsis line-clamp-1 mb-10">
       {{ content }}
@@ -24,12 +24,12 @@
     <div class="flex items-center justify-between text-[14px] leading-[22px]">
       <div class="text-[#303133]">
         举办方：
-        <a :href="organizer_website" class="text-[#409EFF]" target="_blank">
+        <a :href="organizerWebsite" class="text-[#409EFF]" target="_blank">
           {{ organizer }}
         </a>
       </div>
-      <a href="#" class="text-white font-semibold w-[88px] h-[32px] flex items-center justify-center bg-[#409EFF] rounded-[100px]">
-        查看更多
+      <a :href="'/campaigns/' + campaignId" class="text-white font-semibold w-[88px] h-[32px] flex items-center justify-center bg-[#409EFF] rounded-[100px]">
+        了解更多
       </a>
     </div>
   </a>
@@ -38,14 +38,20 @@
 <script>
   export default {
     props: {
+      campaignId: Number,
       name: String,
-      start_date: String,
-      end_date: String,
+      startDate: String,
+      endDate: String,
       content: String,
       organizer: String,
-      organizer_website: String,
+      organizerWebsite: String,
       pageviews: Number,
-      leads_count: Number
+      leadsCount: Number,
+      formUrl: String
+    },
+    mounted() {
+      console.log(this.name)
+      console.log(this.formUrl)
     }
   }
 </script>
