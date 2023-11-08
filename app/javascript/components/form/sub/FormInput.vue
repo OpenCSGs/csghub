4c80ce391bf286f0ca4f8683fa586c91d4e2476a
zxr <1902859534@qq.com>
2023-11-08 16:45:00 +0800

    Moving svg to FormLabel.vue

--- app/javascript/components/form/sub/FormInput.vue
+++ app/javascript/components/form/sub/FormInput.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-    <div class="flex items-center gap-[10px]">
-      <form-label :for_field="field" :label="label"/>
-      <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-        <path
-            d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-            fill="#F56C6C"/>
-      </svg>
+    <div class="flex items-center gap-y-[10px] gap-x-[4px]">
+      <form-label :for_field="field" :label="label" :closeSvg=false />
     </div>
     <el-input v-if="type_placeholder === false" v-model="inputValue" @input="updateValue" clearable
               :placeholder="placeholder" class="w-full h-[40px] text-[#606266] mt-[10px]"/>
@@ -23,7 +18,8 @@ export default {
     placeholder: String,
     field: String,
     type_placeholder: Boolean,
-    label: String
+    label: String,
+    closeSvg: Boolean
   },
   components: {
     FormLabel
