0e770d6f47d808c9ee7b976cfed7504af0d27ead
zxr <1902859534@qq.com>
2023-11-08 16:01:16 +0800

    Update Expertform.vue Using sub

--- app/javascript/components/form/Expertform.vue
+++ app/javascript/components/form/Expertform.vue
@@ -3,7 +3,7 @@
     <div
         class="absolute flex flex-col bg-white w-[500px] h-auto border rounded-lg p-[36px] gap-y-[24px] border-[#DCDFE6] bg-gradient-to-r shadow-xl">
 
-      <div v-if="divVisible === true" class="flex items-center justify-center px-[16px] py-[9px] w-full font-normal text-[13px] text-[#909399] rounded-[4px] leading-[22px] outline-0 bg-[#F4F4F5] gap-[8px]">
+      <div v-if="divTipVisible === true" class="flex items-center justify-center px-[16px] py-[9px] w-full font-normal text-[13px] text-[#909399] rounded-[4px] leading-[22px] outline-0 bg-[#F4F4F5] gap-[8px]">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
           <path
               d="M10 1.25C12.4742 1.315 14.5348 2.17104 16.1819 3.81813C17.829 5.46521 18.685 7.52583 18.75 10C18.685 12.4742 17.829 14.5348 16.1819 16.1819C14.5348 17.829 12.4742 18.685 10 18.75C7.52583 18.685 5.46521 17.829 3.81813 16.1819C2.17104 14.5348 1.315 12.4742 1.25 10C1.315 7.52583 2.17104 5.46521 3.81813 3.81813C5.46521 2.17104 7.52583 1.315 10 1.25ZM11.3088 6.62125C11.6475 6.62125 11.9275 6.52042 12.1488 6.31875C12.37 6.11708 12.4806 5.84687 12.4806 5.50812C12.4806 5.16937 12.37 4.89917 12.1488 4.6975C11.9275 4.49583 11.6508 4.395 11.3188 4.395C10.9867 4.395 10.71 4.49583 10.4887 4.6975C10.2675 4.89917 10.1569 5.16937 10.1569 5.50812C10.1569 5.84687 10.2675 6.11708 10.4887 6.31875C10.71 6.52042 10.9835 6.62125 11.3094 6.62125H11.3088ZM11.5431 13.6525C11.5431 13.5742 11.5496 13.47 11.5625 13.34C11.5754 13.21 11.5754 13.0863 11.5625 12.9688L10.5275 14.16C10.4233 14.2771 10.3158 14.3683 10.205 14.4338C10.0942 14.4992 9.99979 14.5188 9.92188 14.4925C9.80479 14.4404 9.75271 14.3492 9.76562 14.2188L11.4844 8.80875C11.5494 8.44417 11.4908 8.13167 11.3088 7.87125C11.1267 7.61083 10.8337 7.45458 10.43 7.4025C9.97417 7.41542 9.47604 7.6075 8.93563 7.97875C8.39521 8.35 7.92313 8.82208 7.51937 9.395V9.68813C7.50646 9.81854 7.50646 9.94229 7.51937 10.0594L8.55438 8.86812C8.65854 8.75104 8.76604 8.65979 8.87687 8.59438C8.98771 8.52896 9.07562 8.50937 9.14062 8.53562C9.27104 8.60062 9.31667 8.70479 9.2775 8.84812L7.57812 14.2387C7.48688 14.5642 7.5325 14.854 7.715 15.1081C7.8975 15.3623 8.21646 15.5348 8.67188 15.6256C9.32312 15.6127 9.87 15.424 10.3125 15.0594C10.755 14.6948 11.1652 14.226 11.5431 13.6531V13.6525Z"
@@ -17,68 +17,13 @@
         </svg>
       </div>
 
-      <form ref="forms" @submit.prevent="submitTheForm" class="flex flex-col text-[#303133] text-sm gap-y-[8px]">
-        <div class="flex items-center gap-[4px]">
-          <form-label for_field="user_name" label="姓名"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-        <form-input @update:inputValue="handleUserNameInputValue" field="user_name" placeholder="请输入"/>
-
-        <div class="flex items-center gap-[4px] mt-[16px]">
-          <form-label for_field="job_name" label="职位名称"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-        <form-input @update:inputValue="handleJobNameInputValue" placeholder="请输入"/>
-
-        <div class="flex items-center gap-[4px] mt-[16px]">
-          <form-label for_field="phone" label="电话号码"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-        <form-input @update:inputValue="handlePhoneInputValue" placeholder="请输入"/>
-
-        <div class="flex items-center gap-[4px] mt-[16px]">
-          <form-label for_field="company_name" label="公司名称"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-        <form-input @update:inputValue="handleCompanyNameInputValue" placeholder="请输入"/>
-
-        <div class="flex items-center gap-[4px] mt-[16px]">
-          <form-label for_field="expertise" label="擅长领域"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-
-        <form-input @update:inputValue="handleExpertiseInputValue" field="expertise" placeholder="请输入" :type_placeholder=true />
-
-        <div class="flex items-center gap-[4px] mt-[16px]">
-          <form-label for_field="desc" label="个人介绍"/>
-          <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
-            <path
-                d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
-                fill="#F56C6C"/>
-          </svg>
-        </div>
-
-          <form-input @update:inputValue="handleDescInputValue" field="expertise" placeholder="请输入" :type_placeholder=true />
+      <form ref="forms" @submit.prevent="submitTheForm" class="flex flex-col gap-y-[24px] text-[#303133] text-sm">
+        <form-input @update:inputValue="handleUserNameInputValue" field="user_name" placeholder="请输入" label="姓名"/>
+        <form-input @update:inputValue="handleUserNameInputValue" field="job_name" placeholder="请输入" label="职位名称"/>
+        <form-input @update:inputValue="handleUserNameInputValue" field="phone" placeholder="请输入" label="电话号码"/>
+        <form-input @update:inputValue="handleUserNameInputValue" field="company_name" placeholder="请输入" label="公司名称"/>
+        <form-input @update:inputValue="handleUserNameInputValue" field="expertise" placeholder="请输入" label="擅长领域" :type_placeholder=true />
+        <form-input @update:inputValue="handleUserNameInputValue" field="desc" placeholder="请输入" label="个人介绍" :type_placeholder=true />
 
         <div class="flex mt-[16px] gap-x-[16px]">
           <input class="flex w-[70px] text-white bg-[#409EFF] rounded-[4px] leading-[40px] cursor-pointer" type="submit"
@@ -105,12 +50,12 @@ const divTipVisible = ref(true)
 
 const { cookies } = useCookies()
 
-let user_name = ''
-let job_name = ''
-let phone = ''
-let company_name = ''
-let expertise = ''
-let desc = ''
+let user_name = null
+let job_name = null
+let phone = null
+let company_name = null
+let expertise = null
+let desc = null
 
 // 父组件接受子组件传来的值
 const handleUserNameInputValue = (value) => {
@@ -137,22 +82,22 @@ const closeTipDiv = () => {
 };
 
 const submitTheForm = () => {
-  if (user_name == "") {
+  if (user_name == null) {
     ElMessage({message: "请您填写姓名", type: "warning"})
     return
-  } else if (job_name == "") {
+  } else if (job_name == null) {
     ElMessage({message: "请您填写职位名称", type: "warning"})
     return
-  } else if (phone == "") {
+  } else if (phone == null) {
     ElMessage({message: "请您填写电话号码", type: "warning"})
     return
-  } else if (company_name == "") {
+  } else if (company_name == null) {
     ElMessage({message: "请您填写公司名称", type: "warning"})
     return
-  } else if (expertise == "") {
+  } else if (expertise == null) {
     ElMessage({message: "请您填写擅长领域", type: "warning"})
     return
-  } else if (desc == "") {
+  } else if (desc == null) {
     ElMessage({message: "请您填写个人介绍", type: "warning"})
     return
   }
--- app/javascript/components/form/sub/FormInput.vue
+++ app/javascript/components/form/sub/FormInput.vue
@@ -1,14 +1,32 @@
 <template>
-  <el-input v-if="type_placeholder === false" v-model="inputValue" @input="updateValue" clearable :placeholder="placeholder" class="w-full h-[40px] text-[#606266]"/>
-  <el-input v-else v-model="inputValue" @input="updateValue" clearable type="textarea" :placeholder="placeholder" class="w-full h-[70px] text-[#606266]"/>
+  <div>
+    <div class="flex items-center gap-[10px]">
+      <form-label :for_field="field" :label="label"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none">
+        <path
+            d="M2.21714 5.2179L3.35474 3.8499L4.49234 5.2179L5.12594 4.7571L4.20434 3.2595L5.77394 2.6115L5.52914 1.8771L3.88754 2.2659L3.74354 0.537903H2.96594L2.82194 2.2803L1.18034 1.8771L0.921143 2.6115L2.49074 3.2595L1.58354 4.7571L2.21714 5.2179Z"
+            fill="#F56C6C"/>
+      </svg>
+    </div>
+    <el-input v-if="type_placeholder === false" v-model="inputValue" @input="updateValue" clearable
+              :placeholder="placeholder" class="w-full h-[40px] text-[#606266] mt-[10px]"/>
+    <el-input v-else v-model="inputValue" @input="updateValue" clearable type="textarea" :placeholder="placeholder"
+              class="w-full h-auto text-[#606266] mt-[10px]"/>
+  </div>
 </template>
 <script>
+import FormLabel from "./FormLabel.vue"
+
 export default {
   props: {
     name_field: String,
     placeholder: String,
     field: String,
     type_placeholder: Boolean,
+    label: String
+  },
+  components: {
+    FormLabel
   },
   data() {
     return {
