a9e1314b203f332d28e73dde5857520997f6e919
zhendi <zhendi.wang@gmail.com>
2023-11-10 10:39:42 +0800

    remove authenticate_user for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,6 +1,4 @@
 class CampaignsController < ApplicationController
-  before_action :authenticate_user
-
   def index
     @total_campaigns = Campaign.all.order(created_at: :desc)
     @campaigns = @total_campaigns.page(params[:page]).map(&:with_content_and_leads_count)
