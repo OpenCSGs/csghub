c80cd0069adeee7cb1b5eccb34c1e733e3f0e30c
ymh6315431 <yangminghao6315431@163.com>
2023-11-13 19:06:19 +0800

    Code refactor leads_controller.rb

--- app/controllers/api/leads_controller.rb
+++ app/controllers/api/leads_controller.rb
@@ -1,21 +1,19 @@
 class Api::LeadsController < Api::ApplicationController
 
-  def create_params
-    params.require(:lead).permit(:name, :title, :phone, :company, :expertise, :introduction, :email, :company_site, :industry, :lead_source, :lead_type)
-  end
-
   def create
     lead = Lead.new(create_params)
 
     if lead.save
-      render json: { message: "Lead created" }
+      render json: { message: "线索创建成功" }
     else
-      if Lead.find_by(phone: create_params[:phone], lead_type: create_params[:lead_type])
-        render json: { message: "手机号已存在" }, status: :bad_request
-      else
-        render json: { message: "未包含必填字段" }, status: :bad_request
-      end
+      render json: { message: "线索创建失败" }, status: :bad_request
     end
+  end
+
+  private
+
+  def create_params
+    params.require(:lead).permit(:name, :title, :phone, :company, :expertise, :introduction, :email, :company_site, :industry, :lead_source, :lead_type)
   end
 end
 
