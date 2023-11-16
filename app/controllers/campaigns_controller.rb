7e18a18cf0c15d7c3c69786e85b4f69294e06fd9
zxr <1902859534@qq.com>
2023-11-16 18:07:11 +0800

    Add field release of campaigns to show campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,12 +1,16 @@
 class CampaignsController < ApplicationController
   def index
-    @total_campaigns = Campaign.all.order(created_at: :desc)
+    @release_campaigns = Campaign.release
+    @total_campaigns = @release_campaigns.all.order(created_at: :desc)
     @campaigns = @total_campaigns.page(params[:page]).map(&:with_content_and_leads_count)
-    @recommended_campaigns = Campaign.recommended.map(&:banner_attributes)
+    @recommended_campaigns = @release_campaigns.recommended.map(&:banner_attributes)
   end
 
   def show
-    @campaign = Campaign.find params[:id]
+    @campaign = Campaign.find_by(id: params[:id], release: true)
+    if @campaign.nil?
+      return redirect_to root_path, alert: '未找到符合条件的活动'
+    end
     @related_spaces = Space.left_joins(:tags).where(tags: {name: @campaign.name})
     # Increment the pageview count of the campaign when it is viewed
     @campaign.pageviews += 1
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_061750) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_092026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_061750) do
     t.integer "campaign_type"
     t.integer "status", default: 0
     t.boolean "recommended", default: false
+    t.boolean "release", default: false
     t.index ["uuid"], name: "index_campaigns_on_uuid"
   end
 
