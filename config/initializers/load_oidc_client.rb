4bf06c878d3941d4ad1abe6ad8740c9437278dec
Hiveer <ljp.hiveer@gmail.com>
2023-11-20 18:08:11 +0800

    Bug fix

--- config/initializers/load_oidc_client.rb
+++ config/initializers/load_oidc_client.rb
@@ -1,12 +1,14 @@
 ActiveSupport::Reloader.to_prepare do
-  oidc_configs = SystemConfig.first.oidc_configs
+  if ActiveRecord::Base.connection.data_source_exists?('system_configs') && SystemConfig.first.present?
+    oidc_configs = SystemConfig.first.oidc_configs
 
-  OPENID_CLIENT = OpenIDConnect::Client.new(
-    identifier: oidc_configs['identifier'],
-    secret: oidc_configs['secret'],
-    redirect_uri: oidc_configs['redirect_uri'],
-    authorization_endpoint: oidc_configs['authorization_endpoint'],
-    token_endpoint: oidc_configs['token_endpoint'],
-    userinfo_endpoint: oidc_configs['userinfo_endpoint']
-  )
+    OPENID_CLIENT = OpenIDConnect::Client.new(
+      identifier: oidc_configs['identifier'],
+      secret: oidc_configs['secret'],
+      redirect_uri: oidc_configs['redirect_uri'],
+      authorization_endpoint: oidc_configs['authorization_endpoint'],
+      token_endpoint: oidc_configs['token_endpoint'],
+      userinfo_endpoint: oidc_configs['userinfo_endpoint']
+    )
+  end
 end
