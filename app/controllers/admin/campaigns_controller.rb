9f8a729eff6ecf5c26c361a26a70d81c99d35233
zxr <1902859534@qq.com>
2023-11-21 18:07:44 +0800

    当活动状态为报名中的时候，更新活动表单的状态为 active

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -2,11 +2,19 @@ module Admin
   class CampaignsController < Admin::ApplicationController
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
-    #
-    # def update
-    #   super
-    #   send_foo_updated_email(requested_resource)
-    # end
+    def update
+      super
+      #  send_foo_updated_email(requested_resource)
+      @campaign = Campaign.find(params[:id])
+      @lead_form = LeadForm.find_by(campaign_id: @campaign.id)
+      if @campaign.signing_up?
+        @lead_form.update(lead_form_status: 'active')
+        @lead_form.save
+      else
+        @lead_form.update(lead_form_status: 'inactive')
+        @lead_form.save
+      end
+    end
 
     # Override this method to specify custom lookup behavior.
     # This will be used to set the resource for the `show`, `edit`, and `update`
