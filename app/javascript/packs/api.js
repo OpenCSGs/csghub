4217326e3a6a99aa8ca74019d95d505be8495144
minghao.yang <mh.yang@opencsg.com>
2023-11-21 23:02:47 +0800

    only fetch

--- app/javascript/packs/api.js
+++ app/javascript/packs/api.js
@@ -1,15 +1,7 @@
 const csrfFetch = (url, options) => {
   const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
   options.headers = { "X-CSRF-Token": csrfToken, ...options.headers };
-  return fetch(url, options).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-      return response.json();
-  }).catch((error) => {
-    console.error(error);
-    throw new Error("网络请求失败");
-  });
+  return fetch(url, options)
 };
 
 export default csrfFetch;
