203b816e038c3f2e0365a833438eeb4f5cd85af5
zxr <1902859534@qq.com>
2023-11-22 09:40:36 +0800

    redirect to campaigns_path

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -9,7 +9,7 @@ class CampaignsController < ApplicationController
   def show
     @campaign = Campaign.release.find_by(id: params[:id])
     if @campaign.nil?
-      return redirect_to root_path, alert: '未找到符合条件的活动'
+      return redirect_to campaigns_path, alert: '未找到符合条件的活动'
     end
     @related_spaces = Space.left_joins(:tags).where(tags: {name: @campaign.name})
     # Increment the pageview count of the campaign when it is viewed
