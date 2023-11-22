2005990931f8c2bb34e28dd16bdcb93155496249
zxr <1902859534@qq.com>
2023-11-22 10:24:51 +0800

    Opimize the add-field-release

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,14 +1,13 @@
 class CampaignsController < ApplicationController
   def index
-    @released_campaigns = Campaign.release
-    @total_campaigns = @released_campaigns.order(created_at: :desc)
+    @total_campaigns = Campaign.release.order(created_at: :desc)
     @campaigns = @total_campaigns.page(params[:page]).map(&:with_content_and_leads_count)
-    @recommended_campaigns = @released_campaigns.recommended.map(&:banner_attributes)
+    @recommended_campaigns = Campaign.release.recommended.map(&:banner_attributes)
   end
 
   def show
     @campaign = Campaign.release.find_by(id: params[:id])
-    if @campaign.nil?
+    unless @campaign
       return redirect_to campaigns_path, alert: '未找到符合条件的活动'
     end
     @related_spaces = Space.left_joins(:tags).where(tags: {name: @campaign.name})
