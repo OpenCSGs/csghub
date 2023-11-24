a7afebc9bffef92c0ab6ff94ffe2835db4c7d2a5
zxr <1902859534@qq.com>
2023-11-24 15:24:35 +0800

    Delete log

--- app/javascript/components/campaigns/CampaignCards.vue
+++ app/javascript/components/campaigns/CampaignCards.vue
@@ -60,9 +60,7 @@
         theRecommendedCampaigns: this.recommendedCampaigns
       }
     },
-    mounted() {
-      console.log(this.theRecommendedCampaigns.length)
-    },
+    mounted() {},
     methods: {
       async nextPage() {
         const campaignsUpdateEndpoint = `api/campaigns?page=${this.currentPage}`;
