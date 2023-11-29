c0cc2ff9c02243e5e31e2ac91c61f1a28515664c
Hiveer <ljp.hiveer@gmail.com>
2023-11-29 16:21:51 +0800

    Rails g model Organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_113109) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_081517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -130,6 +130,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_113109) do
     t.text "expertise"
     t.text "introduction"
     t.string "company_site"
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "nickname"
+    t.string "logo"
+    t.string "homepage"
+    t.string "org_type"
+    t.boolean "verified"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_organizations_on_name"
   end
 
   create_table "spaces", force: :cascade do |t|
