d2d4d4321982a959eab452216144fc85ce4618d5
Hiveer <ljp.hiveer@gmail.com>
2023-11-29 17:21:49 +0800

    Define org type and org roles

--- app/models/org_membership.rb
+++ app/models/org_membership.rb
@@ -1,4 +1,6 @@
 class OrgMembership < ApplicationRecord
   belongs_to :organization
   belongs_to :user
+
+  enum :role, [:read, :write, :admin]
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,3 @@
 class Organization < ApplicationRecord
+  enum :org_type, {'企业': 'company', '高校': 'university', '非营利组织': 'non_profit', '社区组织': 'community'}
 end
