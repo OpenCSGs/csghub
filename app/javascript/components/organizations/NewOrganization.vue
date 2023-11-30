6d9c709553c3995671646985b4a1efc79a1adf06
Hiveer <ljp.hiveer@gmail.com>
2023-11-30 17:14:36 +0800

    Call create organization api

--- app/javascript/components/organizations/NewOrganization.vue
+++ app/javascript/components/organizations/NewOrganization.vue
@@ -3,33 +3,107 @@
     <h3 class="text-[#303133] text-[20px] font-[600]">新建组织</h3>
     <div class="mt-[16px]">
       <div class="flex sm:flex-col gap-[16px] mb-[16px]">
-        <div class="w-[284px]">
+        <div class="w-[284px] sm:w-auto">
           <p class="text-[#303133] text-[14px] mb-[8px]">组织命名空间</p>
-          <el-input v-model="input" placeholder="Please input" input-style="height: 40px" />
+          <el-input v-model="orgName" placeholder="8-20位字母数字以及 _ 构成的字符串" input-style="height: 40px" />
         </div>
-        <div class="w-[284px]">
+        <div class="w-[284px] sm:w-auto">
           <p class="text-[#303133] text-[14px] mb-[8px]">组织别名</p>
-          <el-input v-model="input" placeholder="Please input" input-style="height: 40px" />
+          <el-input v-model="orgNickname" placeholder="请输入" input-style="height: 40px" />
         </div>
       </div>
       <div class="flex sm:flex-col gap-[16px] mb-[16px]">
-        <div class="w-[284px]">
+        <div class="max-w-[284px]">
           <p class="text-[#303133] text-[14px] mb-[8px]">组织头像</p>
-          <el-input type='file' v-model="input" placeholder="Please input" input-style="height: 40px" />
+          <div>
+            <input type="file" ref="orgAvatarFileInput" class="hidden" @change="previewAvatar" />
+            <el-button class="mr-[8px]" @click="uploadOrgAvatar">点击上传</el-button>
+            <span class="text-[#606266] text-[14px]">{{ orgAvatar }}</span>
+          </div>
         </div>
-        <div class="w-[284px]">
-          <p class="text-[#303133] text-[14px] mb-[8px]">组织类型</p>
-          <el-input v-model="input" placeholder="Please input" input-style="height: 40px" />
-        </div>
+      </div>
+      <div class="max-w-[284px] mb-[16px]">
+        <p class="text-[#303133] text-[14px] mb-[8px]">组织类型</p>
+        <el-select v-model="orgTypeValue" placeholder="选择" size="large">
+          <el-option
+            v-for="item in theOrgTypes"
+            :key="item[1]"
+            :label="item[0]"
+            :value="item[1]"
+          />
+        </el-select>
       </div>
       <div class="mb-[16px]">
         <p class="text-[#303133] text-[14px] mb-[8px]">组织主页</p>
-        <el-input v-model="input" placeholder="Please input" input-style="height: 40px" />
+        <el-input v-model="orgHomepage" placeholder="https://example.com" input-style="height: 40px" />
       </div>
-      <button class="bg-[#8AA2FF] w-full rounded-[4px] px-[20px] py-[9px] text-white">创建组织</button>
+      <button class="bg-[#8AA2FF] w-full rounded-[4px] px-[20px] py-[9px] text-white" @click="createOrg">创建组织</button>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+  import { nextTick, ref, inject, onMounted } from 'vue'
+  import csrfFetch from "../../packs/csrfFetch.js"
+  import { ElInput, ElMessage } from 'element-plus'
+
+  const props = defineProps({
+    orgTypes: String
+  })
+
+  const theOrgTypes = JSON.parse(props.orgTypes)
+  const orgTypeValue = ref('')
+  const orgAvatar = ref('待上传')
+  const orgAvatarFileInput = ref(null)
+  const orgName = ref('')
+  const orgNickname = ref('')
+  const orgHomepage = ref('')
+
+  const uploadOrgAvatar = () => {
+    orgAvatarFileInput.value.click()
+  }
+
+  const previewAvatar = () => {
+    orgAvatar.value = orgAvatarFileInput.value.files[0].name
+  }
+
+  const createOrg = () => {
+    submitOrgForm().then((data) => {
+      ElMessage({
+        message: '组织创建成功',
+        type: 'success'
+      })
+    })
+    .catch(err => {
+      ElMessage({
+        message: err.message,
+        type: 'warning'
+      })
+    })
+  }
+
+  async function submitOrgForm() {
+    const orgCreateEndpoint = `/internal_api/organizations`;
+    const formData = new FormData()
+    const orgAvatarFile = orgAvatarFileInput.value.files[0]
+    formData.append('name', orgName)
+    formData.append('nickname', orgNickname)
+    formData.append('org_type', orgTypeValue)
+    formData.append('homepage', orgHomepage)
+    if ( orgAvatarFile != undefined) {
+      formData.append("logo", orgAvatarFile);
+    }
+    const options = {
+      method: 'POST',
+      body: formData
+    };
+
+    const response = await csrfFetch(orgCreateEndpoint, options)
+
+    if (!response.ok) {
+      return response.json().then(data => { throw new Error(data.message) })
+    } else {
+      return response.json();
+    }
+  }
 </script>
