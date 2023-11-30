e8c1dd52695c0831fa54522395d6be8a42f26cbb
Hiveer <ljp.hiveer@gmail.com>
2023-11-30 23:02:47 +0800

    Validation for organization name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,9 @@
 class Organization < ApplicationRecord
   enum :org_type, {'企业': 'company', '高校': 'university', '非营利组织': 'non_profit', '社区组织': 'community'}
 
+  validates_uniqueness_of :name
+  validates :name, format: { with: /\A(?=.{8,20}$)(?![_])(?!.*[_]{2})[a-zA-Z0-9_]+(?<![_])\Z/ }
+
   has_many :org_memberships, dependent: :destroy
   has_many :users, through: :org_memberships
 
