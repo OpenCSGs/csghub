e65a8cf8e19678d1e3d713d7c5f3142a9b41c9e1
Hiveer <ljp.hiveer@gmail.com>
2023-12-04 22:11:08 +0800

    Update remove not related changes in db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -168,25 +168,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_134018) do
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
-  create_table "ssh_keys", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.text "ssh_key", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_ssh_keys_on_user_id"
-  end
-
   create_table "system_api_keys", force: :cascade do |t|
     t.string "service"
     t.string "secret_key"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "system_configs", force: :cascade do |t|
-    t.string "application_env"
-    t.jsonb "oidc_configs", default: {}
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -232,7 +216,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_134018) do
   add_foreign_key "lead_forms", "campaigns"
   add_foreign_key "org_memberships", "organizations"
   add_foreign_key "org_memberships", "users"
-  add_foreign_key "ssh_keys", "users"
   add_foreign_key "taggings", "spaces"
   add_foreign_key "taggings", "tags"
 end
