5cf444a5f9ad49374e361f43bcc68f2eec7d2fac
Hiveer <ljp.hiveer@gmail.com>
2023-12-04 21:46:38 +0800

    Initialize key value before system api key create

--- app/models/system_api_key.rb
+++ app/models/system_api_key.rb
@@ -1,2 +1,9 @@
 class SystemApiKey < ApplicationRecord
+  validate_presence_of :service, :secret_key
+
+  before_create :initialize_key
+
+  def initialize_key
+    self.secret_key = SecureRandom.base64(128)
+  end
 end
