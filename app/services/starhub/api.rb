768e0cc023bcfcf64120ae78a0defba9fd30ec7a
zxr <1902859534@qq.com>
2023-12-05 16:54:35 +0800

    Create api about datasets

--- app/services/starhub/api.rb
+++ app/services/starhub/api.rb
@@ -23,6 +23,32 @@ module Starhub
       res = @client.get("/models/#{username}/#{model_name}/branches")
     end
 
+    def get_model_tags(username, model_name, options = {})
+      res = @client.get("/models/#{username}/#{model_name}/tags")
+    end
+
+    # datasets
+    def get_datasets_detail(username, dataset_name, options = {})
+      res = @client.get("/datasets/#{username}/#{dataset_name}/detail")
+    end
+
+    def get_datasets_files(username, dataset_name, options = {})
+      options[:path] ||= '/'
+      res = @client.get("/datasets/#{username}/#{dataset_name}/tree?path=#{options[:path]}")
+    end
+
+    def get_datasets_commit(username, dataset_name, options = {})
+      res = @client.get("/datasets/#{username}/#{dataset_name}/last_commit")
+    end
+
+    def get_datasets_branches(username, dataset_name, options = {})
+      res = @client.get("/datasets/#{username}/#{dataset_name}/branches")
+    end
+
+    def get_datasets_tags(username, dataset_name, options = {})
+      res = @client.get("/datasets/#{username}/#{dataset_name}/tags")
+    end
+
     # TODO: add more starhub api
   end
 end
