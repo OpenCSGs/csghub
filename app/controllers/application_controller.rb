f32e680dcd9be1422d27814ad1ad5e531a008e02
Hiveer <ljp.hiveer@gmail.com>
2023-12-06 17:10:50 +0800

    Code refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,18 @@ class ApplicationController < ActionController::Base
     elsif helpers.logged_in_other_system?
       authing_id_token = cookies[:idToken]
       user_infos = JWT.decode(authing_id_token, nil, false).first
-      user = User.find_by_login_identity(user_infos['sub'])
-      unless user
-        user = User.create(login_identity: user_infos['sub'],
-                          avatar: user_infos['picture'],
-                          name: user_infos['username'],
-                          phone: user_infos['phone_number'],
-                          phone_verified: user_infos['phone_number_verified'],
-                          email: user_infos['email'],
-                          email_verified: user_infos['email_verified'],
-                          gender: user_infos['gender'],
-                          last_login_at: Time.zone.now)
+
+      user = User.find_or_create_by(login_identity: user_infos['sub']) do |u|
+        u.roles = :personal_user
+        u.avatar = user_infos['avatar']
+        u.name = user_infos['name']
+        u.phone = user_infos['phone']
+        u.email = user_infos['email']
+        u.email_verified = user_infos['emailVerified']
+        u.gender = user_infos['gender']
+        u.last_login_at = Time.now
       end
+
       helpers.log_in user
     else
       session[:original_request_path] = request.fullpath
@@ -35,18 +35,18 @@ class ApplicationController < ActionController::Base
     if helpers.logged_in_other_system?
       oidc_id_token = cookies[:idToken]
       user_infos = JWT.decode(oidc_id_token, nil, false).first
-      user = User.find_by_login_identity(user_infos['sub'])
-      unless user
-        user = User.create(login_identity: user_infos['sub'],
-                          avatar: user_infos['picture'],
-                          name: user_infos['username'],
-                          phone: user_infos['phone_number'],
-                          phone_verified: user_infos['phone_number_verified'],
-                          email: user_infos['email'],
-                          email_verified: user_infos['email_verified'],
-                          gender: user_infos['gender'],
-                          last_login_at: Time.zone.now)
+
+      user = User.find_or_create_by(login_identity: user_infos['sub']) do |u|
+        u.roles = :personal_user
+        u.avatar = user_infos['avatar']
+        u.name = user_infos['name']
+        u.phone = user_infos['phone']
+        u.email = user_infos['email']
+        u.email_verified = user_infos['emailVerified']
+        u.gender = user_infos['gender']
+        u.last_login_at = Time.now
       end
+
       helpers.log_in user
     end
   end
