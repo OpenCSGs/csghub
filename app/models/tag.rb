c0e1a88a5c2f9004459bc4f4f671e5465ea4e50d
Hiveer <ljp.hiveer@gmail.com>
2023-12-08 16:38:47 +0800

    Define tag field color and zhname mapping

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -20,18 +20,18 @@ class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :spaces, through: :taggings
 
-  TAG_FIELD_COLOR_MAPPINGS = [
-    {computer_vision: '#db7a7a'},
-    {natural_language_processing: '#7f71de'},
-    {audio_processing: '#538f72'},
-    {multimodal_technology: '#e69832'},
-    {multimodal: '#e69832'},
-    {text_processing: '#ff33cc'},
-    {graphics: '#0073e6'},
-    {audio: '#cccc00'},
-    {video: '#33cccc'},
-    {scientific_computing: '#33cc33'}
-  ]
+  TAG_FIELD_COLOR_MAPPINGS = {
+   computer_vision: {color: '#db7a7a', zh_name: '计算机视觉'},
+   natural_language_processing: {color: '#7f71de', zh_name: '自然语言处理'},
+   audio_processing: {color: '#538f72', zh_name: '语音处理'},
+   multimodal_technology: {color: '#e69832', zh_name: '多模态'},
+   multimodal: {color: '#e69832', zh_name: '多模态'},
+   text_processing: {color: '#ff33cc', zh_name: '文本'},
+   graphics: {color: '#0073e6', zh_name: '图像'},
+   audio: {color: '#cccc00', zh_name: '音频'},
+   video: {color: '#33cccc', zh_name: '视频'},
+   scientific_computing: {color: '#33cc33', zh_name: '科学计算'}
+  }
 
   DEFAULT_TAGS = [
     {name: '语言模型', color: '#009933'},
@@ -43,7 +43,7 @@ class Tag < ApplicationRecord
   def as_json options = nil
     {
       name: name,
-      color: color
+      zh_name: zh_name
     }
   end
 end
