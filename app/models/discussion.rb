e9163f6b7534cc2480c4566d1dfc3eb5bb240621
Hiveer <ljp.hiveer@gmail.com>
2023-12-13 15:30:45 +0800

    Setup create discussions API

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,25 @@
 class Discussion < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   has_many :comments, as: :commentable
+  belongs_to :user
+
+  def as_json_data
+    {
+      id: id,
+      title: title,
+      time: time_ago_in_words(created_at),
+      user: user_json
+    }
+  end
+
+  private
+
+  def user_json
+    {
+      id: user.id,
+      name: user.display_name,
+      avatar: user.avatar
+    }
+  end
 end
