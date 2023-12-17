1a1d77aa18d4c5f1b1ef9986d8d57c8c12425add
Hiveer <ljp.hiveer@gmail.com>
2023-12-17 18:25:58 +0800

    Sync org to starhub and update the syncing status

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -32,10 +32,21 @@ class Organization < ApplicationRecord
     }
   end
 
+  def starhub_synced!
+    self.starhub_synced = true
+    self.save
+  end
+
+  def starhub_synced?
+    starhub_synced == true
+  end
+
   private
 
   def sync_to_starhub_server
-    res = Starhub.api.create_organization(name, nickname, email)
+    res = Starhub.api.create_organization(creator.name, name, nickname, homepage)
+    debugger
+    starhub_synced! if res.status >= 200 && res.status <= 299
   end
 
   def unique_name_by_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_17_101304) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_101805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -174,6 +174,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_17_101304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "creator_id"
+    t.boolean "starhub_synced", default: false
     t.index ["name"], name: "index_organizations_on_name"
   end
 
