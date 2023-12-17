885268a4891ab76dc46de6d350a821a0493798dc
zxr <1902859534@qq.com>
2023-12-17 15:04:13 +0800

    Add authorization in headers

--- app/services/starhub/client.rb
+++ app/services/starhub/client.rb
@@ -22,7 +22,7 @@ module Starhub
     end
 
     def request(verb, path, options = {})
-      headers = options[:headers] || {}
+      headers = options[:headers] || {Authorization:"Bearer f3a7b9c1d6e5f8e2a1b5d4f9e6a2b8d7c3a4e2b1d9f6e7a8d2c5a7b4c1e3f5b8a1d4f9b7d6e2f8a5d3b1e7f9c6a8b2d1e4f7d5b6e9f2a4b3c8e1d7f995hd82hf"}
       headers['content-type'] ||= 'application/json'
       headers['Authorization'] ||= Rails.application.credentials.starhub_api.send("#{Rails.env}").token
 
