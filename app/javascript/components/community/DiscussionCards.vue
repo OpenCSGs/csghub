e068889f86f9355e0f8b6c790cd2834c748729ef
minghao.yang <mh.yang@opencsg.com>
2023-12-18 17:30:42 +0800

    Api getComment

--- app/javascript/components/community/DiscussionCards.vue
+++ app/javascript/components/community/DiscussionCards.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col gap-[16px]">
-    <DiscussionCard v-for="card in cards"
+    <DiscussionCard v-for="card in cards"  @click="updateDetails(card)"
       :key="card.id"
+      :commentId="card.id"
       :num="card.num"
       :title="card.title"
       :time="card.created_at"
@@ -20,12 +21,29 @@ export default {
     DiscussionCard
   },
   data() {
-    return {};
+    return {
+      type:'Discussion',
+      commentData:[]
+    };
   },
   mounted() {},
   methods: {
+    updateDetails(card){
+      console.log(card);
+      this.$emit("updateDetails",card)
+      this.getComment(card.id)
+    },
     changeFlag(flag) {
       this.$emit("changeFlag",flag);
+    },
+    
+    async getComment(commentId){
+      const commentCreateEndpoint = `/internal_api/comments?commentable_type=${this.type}&commentable_id=${commentId}`
+      const response = await fetch(commentCreateEndpoint);
+      response.json().then((data) => {
+        console.log(data);
+        this.commentData=data
+      })
     }
   },
 };
