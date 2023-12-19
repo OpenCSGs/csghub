1bf340cb4a074132d86c4397f57f4c1d3c01f3c9
Hiveer <ljp.hiveer@gmail.com>
2023-12-19 18:14:02 +0800

    Set the default encoding format for application

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the Rails application.
 Rails.application.initialize!
