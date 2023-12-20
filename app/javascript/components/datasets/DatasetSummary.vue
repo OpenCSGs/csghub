34e52b77114531e2af686f295d8b4710d2253b52
minghao.yang <mh.yang@opencsg.com>
2023-12-20 17:42:17 +0800

    Datasets en to zh

--- app/javascript/components/datasets/DatasetSummary.vue
+++ app/javascript/components/datasets/DatasetSummary.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-[300px] md:px-5 md:flex-col-reverse">
     <div class="max-w-[80%] pt-4 pb-10 pr-5 break-words flex-1 markdown-body border-t border-b border-[#EBEEF5] md:border-t-0" v-html="mdParser.render(markdownContent)"></div>
     <div class="py-4 w-[20%] border-l border-[#EBEEF5] md:border-l-0 md:border-b md:w-full md:pl-0">
-      <div class="text-[#606266] text-base font-medium leading-[22px] pl-4 md:pl-0">Download last month</div>
+      <div class="text-[#606266] text-base font-medium leading-[22px] pl-4 md:pl-0">月下载量</div>
       <div class="text-[#303133] text-base font-semibold leading-6 mt-1 pl-4 md:pl-0">{{ downloadCount }}</div>
       <DatasetClone :http-clone-url="httpCloneUrl" :ssh-clone-url="sshCloneUrl" />
     </div>
