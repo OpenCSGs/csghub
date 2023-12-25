e0b5fced62a008f3155a5006bb8ea6222ee37825
Hiveer <ljp.hiveer@gmail.com>
2023-12-25 19:37:07 +0800

    Invite org member pop up

--- app/javascript/components/organizations/OrganizationDetail.vue
+++ app/javascript/components/organizations/OrganizationDetail.vue
@@ -29,12 +29,45 @@
             </svg>
             <span class="text-[#303133] text-[20px]">团队成员 <span class="text-[#A8ABB2] text-[20px]">{{ members.length }} </span></span>
           </h3>
-          <button class="px-[12px] py-[8px] my-[16px] flex gap-[4px] rounded-md border border-gray-300 text-[14px]">
+          <button @click="dialogVisible = true" class="px-[12px] py-[8px] my-[16px] flex gap-[4px] rounded-md border border-gray-300 text-[14px]">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
               <path d="M9.99984 12.9167H6.24984C5.08687 12.9167 4.50538 12.9167 4.03222 13.0602C2.96688 13.3834 2.1332 14.217 1.81004 15.2824C1.6665 15.7555 1.6665 16.337 1.6665 17.5M15.8332 17.5V12.5M13.3332 15H18.3332M12.0832 6.25C12.0832 8.32107 10.4042 10 8.33317 10C6.2621 10 4.58317 8.32107 4.58317 6.25C4.58317 4.17893 6.2621 2.5 8.33317 2.5C10.4042 2.5 12.0832 4.17893 12.0832 6.25Z" stroke="#344054" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
             <span>邀请</span>
           </button>
+          <el-dialog
+            v-model="dialogVisible"
+            title="邀请新成员"
+            width="350"
+          >
+            <span>你正在邀请新成员到组织 <span class="font-bold">{{ organization.name }}</span></span>
+            <div class="mt-[20px]">
+              <div class="mb-[20px]">
+                <p class="text-[#344054] text-[14px] mb-[6px]">用户名</p>
+                <el-input v-model="userNameInput" placeholder="Please input" />
+              </div>
+              <div>
+                <p class="text-[#344054] text-[14px] mb-[6px]">角色</p>
+                <el-select v-model="userRoleInput" placeholder="Select" class="w-full h-[40px]">
+                  <el-option
+                    v-for="item in roleMappings"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  />
+                </el-select>
+              </div>
+            </div>
+            <template #footer>
+              <span class="dialog-footer">
+                <el-button @click="dialogVisible = false">取消</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                  确认
+                </el-button>
+              </span>
+            </template>
+          </el-dialog>
           <div class="mt-[16px] flex">
             <a v-for="user in members" :href="`/profile/${user.name}`">
               <img :src="user.avatar_url" class="h-[32px] w-[32px] rounded-[50%] border" />
@@ -93,6 +126,23 @@
   const datasetList = props.datasets.data
   const hasModels = props.models.total === 0 ? false : true
   const hasDatasets = props.datasets.total === 0 ? false : true
+  const dialogVisible = ref(false)
 
+  const userNameInput = ref('')
+  const userRoleInput = ref('')
+  const roleMappings = [
+    {
+      value: 'read',
+      label: 'read'
+    },
+    {
+      value: 'write',
+      label: 'write'
+    },
+    {
+      value: 'admin',
+      label: 'admin'
+    }
+  ]
   onMounted(() => {})
 </script>
