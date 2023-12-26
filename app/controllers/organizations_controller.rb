e22014a44d1229900225574a6c3d869ba3b5667e
Hiveer <ljp.hiveer@gmail.com>
2023-12-26 16:42:59 +0800

    Return org members not org users

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,7 +10,7 @@ class OrganizationsController < ApplicationController
       flash[:alert] = "未找到对应的组织"
       return redirect_to "/profile/#{current_user.name}"
     end
-    @members = @organization.users
+    @members = @organization.members
     @models = Starhub.api.get_org_models(@organization.name, current_user.name)
     @datasets = Starhub.api.get_org_datasets(@organization.name, current_user.name)
   end
