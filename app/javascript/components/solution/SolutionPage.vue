23bc09f36ba91d24d34727b75b715fb5f5159324
minghao.yang <mh.yang@opencsg.com>
2023-12-28 18:14:37 +0800

    SolutionPage

--- app/javascript/components/solution/SolutionPage.vue
+++ app/javascript/components/solution/SolutionPage.vue
@@ -1,20 +1,35 @@
 <template>
-    <div>
-        SolutionPage
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {},
-  
-    data() {
-      return {};
-    },
-  
-    components: {},
-  
-    mounted() {}
-  }
-  </script>
-  
+  <div>
+    <SolutionBanner />
+    <SolutionKnowledgeBase />
+    <SolutionStarAIO />
+    <SolutionAIImg />
+    <SolutionProgramming />
+  </div>
+</template>
+
+<script>
+import SolutionBanner from "./SolutionBanner.vue";
+import SolutionKnowledgeBase from "./SolutionKnowledgeBase.vue";
+import SolutionStarAIO from "./SolutionStarAIO.vue";
+import SolutionAIImg from "./SolutionAIImg.vue";
+import SolutionProgramming from "./SolutionProgramming.vue";
+
+export default {
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  components: {
+    SolutionBanner,
+    SolutionKnowledgeBase,
+    SolutionStarAIO,
+    SolutionAIImg,
+    SolutionProgramming
+  },
+
+  mounted() {},
+};
+</script>
