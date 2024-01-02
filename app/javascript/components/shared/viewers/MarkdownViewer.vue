e3eb6bec8e55221fc8a3c30967898416c779d20f
zhendi <zhendi.wang@gmail.com>
2024-01-02 17:58:21 +0800

    add markdown-it-anchor

--- app/javascript/components/shared/viewers/MarkdownViewer.vue
+++ app/javascript/components/shared/viewers/MarkdownViewer.vue
@@ -5,6 +5,7 @@
 <script setup>
   import { ref } from 'vue';
   import MarkdownIt from 'markdown-it';
+  import markdownItAnchor from 'markdown-it-anchor'
   import parseMD from 'parse-md'
   import 'github-markdown-css';
 
@@ -21,5 +22,9 @@
 
   markdownContent.value = props.setDefaultText ? (content.trim() || defaultText) : content
 
-  const mdParser = new MarkdownIt({ html: true });
+  const anchorOptions = {
+    tabIndex: false,
+  }
+
+  const mdParser = new MarkdownIt({ html: true }).use(markdownItAnchor, anchorOptions);
 </script>
