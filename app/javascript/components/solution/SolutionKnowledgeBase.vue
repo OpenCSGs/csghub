cde57fe625f922546a5f9a0f139c730f8cab59e6
Hiveer <ljp.hiveer@gmail.com>
2024-01-02 12:59:40 +0800

    Optimize the video popup for solution

--- app/javascript/components/solution/SolutionKnowledgeBase.vue
+++ app/javascript/components/solution/SolutionKnowledgeBase.vue
@@ -5,11 +5,14 @@
         <div class="mb-[12px] border-[#3250BD] leading-[20px] text-[14px] text-[#223B99] border-[1.5px] rounded-[16px] py-[4px] px-[12px]">解决方案 #1</div>
         <div class="text-[36px] leading-[44px] text-[#101828]">OpenCSG EKB AI 智能企业知识库</div>
         <div class="my-[20px] max-w-[768px] text-[20px] text-[#606266] leading-[30px] font-light">EKB AI 将文档结构化并转换为数字资，实现信息数字化存储和管理，夯实企业资产的数字化转型基础。</div>
-        <div class="flex gap-[16px]">
+        <div class="flex gap-[16px] solution">
           <div @click="openDialog" class="cursor-pointer py-[8px] px-[12px] flex text-[14px] leading-[20px] text-[#344054] gap-[6px] border border-[#D0D5DD] rounded-[8px]"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><g clip-path="url(#clip0_661_4)"><path d="M9.99984 18.3337C14.6022 18.3337 18.3332 14.6027 18.3332 10.0003C18.3332 5.39795 14.6022 1.66699 9.99984 1.66699C5.39746 1.66699 1.6665 5.39795 1.6665 10.0003C1.6665 14.6027 5.39746 18.3337 9.99984 18.3337Z" stroke="#344054" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M7.9165 7.47144C7.9165 7.0737 7.9165 6.87483 7.99962 6.76381C8.07206 6.66705 8.18293 6.60652 8.30349 6.59791C8.44182 6.58803 8.60911 6.69557 8.94368 6.91065L12.8775 9.43954C13.1678 9.62618 13.313 9.71949 13.3631 9.83815C13.4069 9.94183 13.4069 10.0588 13.3631 10.1625C13.313 10.2812 13.1678 10.3745 12.8775 10.5611L8.94368 13.09C8.60911 13.3051 8.44182 13.4126 8.30349 13.4027C8.18293 13.3941 8.07206 13.3336 7.99962 13.2368C7.9165 13.1258 7.9165 12.927 7.9165 12.5292V7.47144Z" stroke="#344054" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g><defs><clipPath id="clip0_661_4"><rect width="20" height="20" fill="white"/></clipPath></defs></svg>
             产品演示
           </div>
-          <el-dialog v-model="dialogVisible" @close="closeDialog" :width="dialogWidth" :style="{ borderRadius: '10px', padding: dialogPadding }">
+          <el-dialog v-model="dialogVisible"
+                     @close="closeDialog"
+                     :width="dialogWidth"
+                     :style="{ borderRadius: '5px' }">
             <video ref="videoPlayer" style="width: 100%;" controls autoplay muted>
               <source :src="videoSrc" type="video/mp4" />
             </video>
@@ -52,7 +55,7 @@ export default {
             {id:"5",title:"金融级私有化加密部署，保证企业私有数据安全",desc:"我们采用金融级私有化加密部署技术，确保您企业的私有数据始终得到最高级别的安全保护，防止泄密风险。"}]
     };
   },
-  
+
 
   components: {},
   computed: {
@@ -62,16 +65,8 @@ export default {
       } else {
         return '50%';
       }
-    },
-    dialogPadding(){
-      if (window.innerWidth < 768) {
-        return '0px';
-      } else {
-        return '10px';
-      }
     }
   },
-
   mounted() {},
   methods: {
   openDialog() {
@@ -84,3 +79,13 @@ export default {
 }
 };
 </script>
+<style>
+  .solution {
+    .el-dialog__header {
+     display: none;
+    }
+    .el-dialog__body {
+      padding: 10px !important;
+    }
+  }
+</style>
