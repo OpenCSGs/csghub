c5ebe45c05b73d22fcec2fe9919b7f7abde28f3c
Hiveer <ljp.hiveer@gmail.com>
2024-01-08 19:49:42 +0800

    Add password hash to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_28_023742) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_08_114930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -282,6 +282,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_023742) do
     t.string "git_token"
     t.boolean "starhub_synced", default: false
     t.string "git_token_name"
+    t.string "password_hash"
     t.index ["login_identity"], name: "index_users_on_login_identity", unique: true
   end
 
