fa9cdb127da12fde3baa950825f603948113d94e
zxr <2221258383@qq.com>
2024-01-09 15:38:01 +0800

    Add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_28_023742) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_09_073643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -262,6 +262,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_28_023742) do
     t.string "tag_field"
     t.string "zh_name"
     t.text "desc"
+    t.string "display_name"
   end
 
   create_table "users", force: :cascade do |t|
