48ec32fd033984409b8b1476c477d232d0329543
minghao.yang <mh.yang@opencsg.com>
2024-01-16 19:04:42 +0800

    SSh key error message change

--- app/controllers/internal_api/application_controller.rb
+++ app/controllers/internal_api/application_controller.rb
@@ -1,7 +1,7 @@
 class InternalApi::ApplicationController < ApplicationController
   rescue_from StarhubError do |e|
     log_error e.message, e.backtrace
-    render json: {message: e.message}, status: 500
+    render json: {message: "Git服务器报错"}, status: 500
   end
 
   rescue_from SensitiveContentError do |e|
