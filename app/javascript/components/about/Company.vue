3c4144c5a0c235dd4845718630feffd6c2ab8b93
minghao.yang <mh.yang@opencsg.com>
2024-01-17 17:34:39 +0800

    Draft Company

---
+++ app/javascript/components/about/Company.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="company bg-[#F9FAFB]">
+      <div class="m-auto max-w-[1280px] pt-[96px] md:py-[40px] md:px-[20px]">
+        <div class="flex justify-between items-center gap-[64px] md:flex-col md:gap-[20px]">
+          <div>
+            <div class="text-[16px] leading-[24px] text-[#223B99] mb-[10px]">联系我们</div>
+            <div class="text-[36px] leading-[44px] text-[#101828] mb-[20px]">与我们的团队交流</div>
+            <div class="text-[20px] leading-[30px] text-[#475467]">我们欢迎您的到访！请联系我们。</div>
+          </div>
+          <div class="flex-col gap-[16px]">
+            <div>
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
+              <div>
+                <div class="text-[20px] leading-[30px] text-[#101828]">北京</div>
+                <div class="text-[16px] leading-[24px] font-light text-[#475467]">海淀区 知春路23号 量子银座大厦 11F OpenCSG</div>
+              </div>
+            </div>
+            <div>
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
+              <div>
+                <div class="text-[20px] leading-[30px] text-[#101828]">北京</div>
+                <div class="text-[16px] leading-[24px] font-light text-[#475467]">海淀区 知春路23号 量子银座大厦 11F OpenCSG</div>
+              </div>
+            </div>
+          </div>
+        </div>
+       <div></div>
+      </div>
+    </div>
+  </template>
