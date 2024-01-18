0b0b45f804bf0785f650366aac3f8b785b9a334c
minghao.yang <mh.yang@opencsg.com>
2024-01-18 14:49:02 +0800

    Company text

--- app/javascript/components/about/Company.vue
+++ app/javascript/components/about/Company.vue
@@ -7,24 +7,27 @@
             <div class="text-[36px] leading-[44px] text-[#101828] mb-[20px]">与我们的团队交流</div>
             <div class="text-[20px] leading-[30px] text-[#475467]">我们欢迎您的到访！请联系我们。</div>
           </div>
-          <div class="flex-col gap-[16px]">
-            <div>
+          <div class="flex-col">
+            <div class="flex gap-[16px] mb-[32px]">
               <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
               <div>
-                <div class="text-[20px] leading-[30px] text-[#101828]">北京</div>
+                <div class="text-[20px] leading-[30px] text-[#101828] mb-[10px]">北京</div>
                 <div class="text-[16px] leading-[24px] font-light text-[#475467]">海淀区 知春路23号 量子银座大厦 11F OpenCSG</div>
               </div>
             </div>
-            <div>
+            <div class="flex gap-[16px]">
               <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
               <div>
-                <div class="text-[20px] leading-[30px] text-[#101828]">北京</div>
-                <div class="text-[16px] leading-[24px] font-light text-[#475467]">海淀区 知春路23号 量子银座大厦 11F OpenCSG</div>
+                <div class="text-[20px] leading-[30px] text-[#101828] mb-[10px]">上海</div>
+                <div class="text-[16px] leading-[24px] font-light text-[#475467]">徐汇区 龙台路180号 国际传媒港F2 5F OpenCSG</div>
               </div>
             </div>
           </div>
         </div>
-       <div></div>
+       <div class="mt-[48px] flex-col justify-between items-center font-light text-[#475467] text-[20px] leading-[30px]">
+         <div class="mb-[30px]">北京开放传神科技有限公司（OpenCSG）成立于2023年，是一家致力于大模型生态社区建设，汇集人工智能行业上下游企业链共同为大模型在垂直行业的应用提供解决方案和工具平台的公司。</div>
+         <div>OpenCSG的‘传神’开放社区基于自主研发的算力算法平台和应用开发工具平台，为用户提供一站式的大模型应用构建服务，帮助用户高效且低成本地创建个性化的大模型应用。公司成功实现了在金融、政务、制造业等领域的项目落地，并积极与生态合作伙伴共同探索在医疗、工业、媒体等更多领域的落地应用。目前OpenCSG已完成由国信中数、联想创投数千万元的天使轮融资。</div>
+       </div>
       </div>
     </div>
   </template>
