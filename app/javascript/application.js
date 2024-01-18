cf16faf642eff38b08b24c2ce6956c4a98399875
Hiveer <ljp.hiveer@gmail.com>
2024-01-18 16:50:27 +0800

    Move footer to footer repo and rename from foot to footer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,12 +7,11 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-// import Navbar from "./components/TheNavbar.vue"
 import Navbar from "./components/navbar/NewNavbar.vue"
 import SpaceIntro from "./components/SpaceIntro.vue"
 import SpaceCard from "./components/SpaceCard.vue"
 import SpaceCards from "./components/SpaceCards.vue"
-import Foot from "./components/TheFooter.vue"
+import Footer from "./components/TheFooter.vue"
 import CommentList from "./components/comment/CommentList.vue"
 import ExpertCard from "./components/ExpertCard.vue"
 import PartnerCard from "./components/PartnerCard.vue"
@@ -41,7 +40,7 @@ import OrganizationDetail from "./components/organizations/OrganizationDetail.vu
 import SolutionPage from "./components/solution/SolutionPage.vue"
 import ProductPage from "./components/product/ProductPage.vue"
 import About from "./components/about/About.vue"
-import OnPremiseFooter from "./components/OnPremiseFooter.vue"
+import OnPremiseFooter from "./components/footer/OnPremiseFooter.vue"
 
 const app = createApp({
   components: {
@@ -58,7 +57,7 @@ const app = createApp({
     ModelDetail,
     PartnerCard,
     PartnerForm,
-    Foot,
+    Footer,
     CampaignCard,
     CampaignCards,
     Profile,
