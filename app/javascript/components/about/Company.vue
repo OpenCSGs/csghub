bb08afa63bf3dc5e338f146ec02bb35341a80f64
minghao.yang <mh.yang@opencsg.com>
2024-01-19 15:51:57 +0800

    Update mobile responsive

--- app/javascript/components/about/Company.vue
+++ app/javascript/components/about/Company.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="company">
-      <div class="bg-[#F9FAFB] pb-40 md:pb-[20px]">
-        <div class="m-auto max-w-[1280px] px-[20px] pt-[96px] md:py-[40px] md:px-[20px]">
-          <div class="flex justify-between gap-[64px] md:flex-col md:gap-[20px] px-[32px]">
+      <div class="bg-[#F9FAFB] pb-40 md:pb-[112px]">
+        <div class="m-auto max-w-[1280px] px-[20px] pt-[96px] md:pt-[64px] md:py-[40px] md:px-[16px]">
+          <div class="flex justify-between md:justify-normal gap-[64px] md:flex-col md:gap-[48px] px-[32px]">
             <div>
-              <div class="text-[16px] leading-[24px] text-[#223B99] mb-[10px]">联系我们</div>
-              <div class="text-[36px] leading-[44px] text-[#101828] mb-[20px]">与我们的团队交流</div>
-              <div class="text-[20px] leading-[30px] text-[#475467]">我们欢迎您的到访！请联系我们。</div>
+              <div class="text-[16px] md:text-[14px] md:leading-[20px] leading-[24px] text-[#223B99] mb-[10px]">联系我们</div>
+              <div class="text-[36px] md:text-[30px] md:leading-[38px] leading-[44px] text-[#101828] mb-[20px]">与我们的团队交流</div>
+              <div class="text-[20px] md:text-[18px] md:leading-[28px] leading-[30px] text-[#475467]">我们欢迎您的到访！请联系我们。</div>
             </div>
             <div class="flex-col">
               <div class="flex gap-[16px] mb-[32px]">
                 <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
                 <div>
-                  <div class="text-[20px] leading-[30px] text-[#101828] mb-[10px]">北京</div>
+                  <div class="text-[20px] md:text-[18px] md:leading-[28px] leading-[30px] text-[#101828] mb-[10px]">北京</div>
                   <div class="text-[16px] leading-[24px] font-light text-[#475467]">海淀区 知春路23号 量子银座大厦 11F OpenCSG</div>
                 </div>
               </div>
@@ -32,8 +32,8 @@
         </div>
       </div>
       <div class="bg-[#FFF]">
-        <div class="m-auto max-w-[1280px] px-[32px] pb-[96px] md:pb-[20px]">
-          <img src="/images/company/company_bg.png" class="w-full relative md:bottom-[20px] bottom-[96px]" alt="OpenCSG"/>
+        <div class="m-auto max-w-[1280px] px-[32px] md:px-[16px] pb-[96px] md:pb-[64px]">
+          <img src="/images/company/company_bg.png" class="w-full relative md:bottom-[64px] bottom-[96px]" alt="OpenCSG"/>
         </div>
       </div>
     </div>
