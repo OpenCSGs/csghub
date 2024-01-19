3d24de7497a5f3e71a72d425816e28a930b917db
minghao.yang <mh.yang@opencsg.com>
2024-01-19 15:54:45 +0800

    Update text font size

--- app/javascript/components/about/Company.vue
+++ app/javascript/components/about/Company.vue
@@ -19,7 +19,7 @@
               <div class="flex gap-[16px]">
                 <div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path d="M12 12.5C13.6569 12.5 15 11.1569 15 9.5C15 7.84315 13.6569 6.5 12 6.5C10.3431 6.5 9 7.84315 9 9.5C9 11.1569 10.3431 12.5 12 12.5Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 22C14 18 20 15.4183 20 10C20 5.58172 16.4183 2 12 2C7.58172 2 4 5.58172 4 10C4 15.4183 10 18 12 22Z" stroke="#3250BD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div>
                 <div>
-                  <div class="text-[20px] leading-[30px] text-[#101828] mb-[10px]">上海</div>
+                  <div class="text-[20px] md:text-[18px] md:leading-[28px] leading-[30px] text-[#101828] mb-[10px]">上海</div>
                   <div class="text-[16px] leading-[24px] font-light text-[#475467]">徐汇区 龙台路180号 国际传媒港F2 5F OpenCSG</div>
                 </div>
               </div>
