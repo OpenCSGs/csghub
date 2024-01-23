8bd2c22f0e42f428056bcc4941107f8fa8e302c9
Hiveer <ljp.hiveer@gmail.com>
2024-01-23 11:38:38 +0800

    Set profile to be public and show data for the user and also check if it is the current user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,13 @@
 class ProfileController < ApplicationController
-  before_action :authenticate_user
   before_action :check_user_info_integrity
 
   def index
     @user ||= User.find_by(name: params[:user_id])
     return redirect_to errors_not_found_path unless @user
 
-    @models = Starhub.api.get_user_models(@user.name, @user.name)
-    @datasets = Starhub.api.get_user_datasets(@user.name, @user.name)
+    @models = Starhub.api.get_user_models(@user.name, current_user&.name)
+    @datasets = Starhub.api.get_user_datasets(@user.name, current_user&.name)
     @organizations = @user.organizations
+    @is_current_user_access = current_user.present? && (current_user == @user)
   end
 end
