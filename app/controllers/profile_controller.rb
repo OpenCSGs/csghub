ece09179ec1d467de67d57806132f5a861e145bd
Hiveer <ljp.hiveer@gmail.com>
2024-01-23 10:53:16 +0800

    Set the user for profile by reading from params not get the current user

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,8 +3,11 @@ class ProfileController < ApplicationController
   before_action :check_user_info_integrity
 
   def index
-    @models = Starhub.api.get_user_models(current_user.name, current_user.name)
-    @datasets = Starhub.api.get_user_datasets(current_user.name, current_user.name)
-    @organizations = current_user.organizations
+    @user ||= User.find_by(name: params[:user_id])
+    return redirect_to errors_not_found_path unless @user
+
+    @models = Starhub.api.get_user_models(@user.name, @user.name)
+    @datasets = Starhub.api.get_user_datasets(@user.name, @user.name)
+    @organizations = @user.organizations
   end
 end
