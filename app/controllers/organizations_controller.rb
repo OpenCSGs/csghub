bcad6ad59f414813f5dd326c31935058f016336f
zxr <2221258383@qq.com>
2024-01-24 15:02:02 +0800

    change org to public

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,4 @@
 class OrganizationsController < ApplicationController
-  before_action :authenticate_user
   before_action :check_user_info_integrity
 
   def new
@@ -9,15 +8,12 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find_by(name: params[:id])
     unless @organization
       flash[:alert] = "未找到对应的组织"
-      return redirect_to "/profile/#{current_user.name}"
+      return redirect_to errors_not_found_path unless @organization
     end
-    user_org_role = current_user.org_role(@organization)
-    unless user_org_role
-      return redirect_to errors_unauthorized_path
-    end
+    user_org_role = current_user && current_user.org_role(@organization)
     @admin = user_org_role == 'admin'
     @members = @organization.members
-    @models = Starhub.api.get_org_models(@organization.name, current_user.name)
-    @datasets = Starhub.api.get_org_datasets(@organization.name, current_user.name)
+    @models = Starhub.api.get_org_models(@organization.name, current_user&.name)
+    @datasets = Starhub.api.get_org_datasets(@organization.name, current_user&.name)
   end
 end
