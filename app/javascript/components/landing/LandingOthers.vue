4a026c0f252283aaeb6f43cbd4c074fa52b650a6
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-01-25 10:42:21 +0800

    User LandingOthers

---
+++ app/javascript/components/landing/LandingOthers.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <JoinUs />
+    <LandingSolution />
+  </div>
+</template>
+
+<script>
+import JoinUs from "./JoinUs.vue";
+import LandingSolution from "./LandingSolution.vue";
+export default {
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  components: {
+    JoinUs,
+    LandingSolution
+  },
+
+  mounted() {},
+};
+</script>
+<style scoped></style>
