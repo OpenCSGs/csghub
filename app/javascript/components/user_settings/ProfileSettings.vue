9a6238a3d7672e6877bfdb3a8c005f615ce9c3ad
Hiveer <ljp.hiveer@gmail.com>
2024-01-26 17:16:59 +0800

    处理用户 email 为空的 case (#46)
    * Hide ssh key and git token part on user settings menu if user email is
    blank
    
    * Remove ;
    
    * Pass in email for ssh key and git token component

--- app/javascript/components/user_settings/ProfileSettings.vue
+++ app/javascript/components/user_settings/ProfileSettings.vue
@@ -2,6 +2,7 @@
   <div class="flex justify-center md:flex-col px-[24px] py-[36px] my-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white">
     <Menu class="max-w-[411px] md:mb-[24px]"
           :name="profileName"
+          :email="email"
           :displayName="profileDisplayName"
           :avatar="profileAvatar">
     </Menu>
