f2e7e32c346001c7ed1f0c44fd65888f9cdf4f87
zhendi <zhendi.wang@gmail.com>
2024-02-02 19:44:34 +0800

    update code editor

--- app/javascript/components/shared/CodeEditor.vue
+++ app/javascript/components/shared/CodeEditor.vue
@@ -3,8 +3,6 @@
     <Codemirror
       :modelValue="modelValue"
       @change="onChange"
-      :options="options"
-      :autofocus="true"
     />
   </div>
 </template>
@@ -18,19 +16,6 @@ const props = defineProps({
 })
 
 const emits = defineEmits(['update:modelValue'])
-
-const options = {
-  line: true,
-  tabSize: 2, // 制表符的宽度
-  indentUnit: 2, // 一个块应该缩进多少个空格，默认值为 2
-  firstLineNumber: 1, // 从哪个数字开始计算行数，默认值为 1
-  smartIndent: true, // 上下文缩进
-  lineNumbers: false, // 是否显示行号
-  styleActiveLine: true, // 高亮选中行
-  viewportMargin: Infinity, //处理高度自适应时搭配使用
-  showCursorWhenSelecting: true, // 当选择处于活动状态时是否应绘制游标
-  autofocus: true, // 自动获取焦点
-}
 
 const onChange = (code) => {
   if (props.modelValue !== code) {
