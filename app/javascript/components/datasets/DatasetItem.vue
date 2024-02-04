5fcba1b28b682b5b856d4a334f4f351ede6a22cb
Hiveer <ljp.hiveer@gmail.com>
2024-02-04 12:44:05 +0800

    Bug fix: update the api to retrieve model dataset detail

--- app/javascript/components/datasets/DatasetItem.vue
+++ app/javascript/components/datasets/DatasetItem.vue
@@ -5,7 +5,7 @@
       <div class="dataset-path text-sm text-[#303133] font-medium text-ellipsis overflow-hidden whitespace-nowrap">{{ dataset.path }}</div>
     </div>
     <div class="flex items-center gap-[8px] text-xs text-[#909399]">
-      <span>更新时间：{{ dataset.last_updated_at && dataset.last_updated_at.substring(0, 10) }}</span>
+      <span>更新时间：{{ dataset.updated_at && dataset.updated_at.substring(0, 10) }}</span>
       <span>
         <svg xmlns="http://www.w3.org/2000/svg" width="1" height="8" viewBox="0 0 1 8" fill="none">
           <path d="M0.5 0V8" stroke="#DCDFE6"/>
