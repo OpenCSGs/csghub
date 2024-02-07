3562a57d27aa6a4a0a467baef3d4a05c2885875c
zxr <2221258383@qq.com>
2024-02-07 12:10:40 +0800

    Update the authority of upload controller

--- app/controllers/internal_api/models_controller.rb
+++ app/controllers/internal_api/models_controller.rb
@@ -1,8 +1,8 @@
 class InternalApi::ModelsController < InternalApi::ApplicationController
   before_action :authenticate_user, except: [:index, :files, :readme]
-  before_action :validate_model, only: [:update, :destroy, :create_file]
+  before_action :validate_model, only: [:update, :destroy, :create_file, upload_file]
   before_action :validate_manage, only: [:update, :destroy]
-  before_action :validate_write, only: [:create_file]
+  before_action :validate_write, only: [:create_file, upload_file]
   before_action :validate_authorization, only: [:files, :readme]
 
   def index
