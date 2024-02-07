b71c84f8cb756e171ed8d81ecb51d3975ade5eb9
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-02-07 17:14:58 +0800

    Code Indent

--- app/javascript/components/organizations/OrganizationEdit.vue
+++ app/javascript/components/organizations/OrganizationEdit.vue
@@ -27,11 +27,11 @@
         <el-avatar :size="60" :src="avatarUrl"> </el-avatar>
         <div class="flex  gap-[20px] items-center">
           <div @click="uploadImage" class="flex gap-[4px] text-[14px] border border-[#3250BD] px-[12px] py-[8px] leading-[20px] text-center rounded-[8px] text-white cursor-pointer bg-[#3250BD]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path d="M3.33366 13.5352C2.32867 12.8625 1.66699 11.7168 1.66699 10.4167C1.66699 8.46369 3.15992 6.85941 5.06678 6.68281C5.45684 4.31011 7.5172 2.5 10.0003 2.5C12.4835 2.5 14.5438 4.31011 14.9339 6.68281C16.8407 6.85941 18.3337 8.46369 18.3337 10.4167C18.3337 11.7168 17.672 12.8625 16.667 13.5352M6.66699 13.3333L10.0003 10M10.0003 10L13.3337 13.3333M10.0003 10V17.5" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/></svg>
-          上传头像
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path d="M3.33366 13.5352C2.32867 12.8625 1.66699 11.7168 1.66699 10.4167C1.66699 8.46369 3.15992 6.85941 5.06678 6.68281C5.45684 4.31011 7.5172 2.5 10.0003 2.5C12.4835 2.5 14.5438 4.31011 14.9339 6.68281C16.8407 6.85941 18.3337 8.46369 18.3337 10.4167C18.3337 11.7168 17.672 12.8625 16.667 13.5352M6.66699 13.3333L10.0003 10M10.0003 10L13.3337 13.3333M10.0003 10V17.5" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            上传头像
           </div>
           <div @click="removeImage" class="text-[14px] leading-[20px] text-center text-[#223B99] cursor-pointer">
-          移除
+            移除
           </div>
         </div>
       </div>
