343bb1898347473730b53517a305f51abf0de020
zxr <2221258383@qq.com>
2024-02-20 17:51:11 +0800

    Create the migrate table

---
+++ db/migrate/20240220094231_application_spaces.rb
@@ -0,0 +1,19 @@
+class ApplicationSpaces < ActiveRecord::Migration[7.0]
+  def change
+    create_table "application_spaces", force: :cascade do |t|
+      t.string "name"
+      t.string "license"
+      t.string "visibility"
+      t.string "owner_type", null: false
+      t.string "cover_image"
+      t.string "sdk"
+      t.string "cloud_resource"
+      t.bigint "owner_id", null: false
+      t.bigint "creator_id"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["creator_id"], name: "index_datasets_on_creator_id"
+      t.index ["owner_type", "owner_id"], name: "index_datasets_on_owner"
+    end
+  end
+end
