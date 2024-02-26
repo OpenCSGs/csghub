e795260faecae80e4111f3cbc1c28dc0f375b15a
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-02-26 16:04:27 +0800

    Add comments to daily_paper.rb

--- app/models/daily_paper.rb
+++ app/models/daily_paper.rb
@@ -7,6 +7,8 @@ class DailyPaper < ApplicationRecord
   belongs_to :recommender, class_name: 'User', foreign_key: :user_id
   delegate :name, :avatar_url, to: :recommender, prefix: true, allow_nil: true
 
+  has_many :comments, as: :commentable, dependent: :destroy
+
   scope :search_by, ->(keywords) { 
     keywords.present? ? 
       where('title ILIKE :keywords OR article_link ILIKE :keywords OR description ILIKE :keywords OR recommendation ILIKE :keywords', keywords: "%#{keywords}%") : self
@@ -14,11 +16,13 @@ class DailyPaper < ApplicationRecord
 
   def as_json options={}
     {
+      id:id,
       uuid: uuid,
       title: title,
       published_at: published_at,
       recommendation: recommendation,
       cover_url: cover_url,
+      comments:comments.as_json,
       recommender_name: recommender_name,
       recommender_avatar_url: recommender_avatar_url,
       article_link: article_link,
