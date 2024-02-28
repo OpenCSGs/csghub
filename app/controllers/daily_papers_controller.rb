99ef010eae072fc4818368c84f7e76c54a9ebecd
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-02-28 13:07:54 +0800

    处理model_data和dataset_data为空的情况

--- app/controllers/daily_papers_controller.rb
+++ app/controllers/daily_papers_controller.rb
@@ -16,7 +16,15 @@ class DailyPapersController < ApplicationController
 
     model_list_result = Starhub.api.models_by_paths @daily_paper.model_links
     dataset_list_result = Starhub.api.datasets_by_paths @daily_paper.dataset_links
-    @model_data = JSON.parse(model_list_result)['data']
-    @dataset_data = JSON.parse(dataset_list_result)['data']
+    @model_data = []
+    if @daily_paper.model_links.present?
+      model_list_result = Starhub.api.models_by_paths @daily_paper.model_links
+      @model_data = JSON.parse(model_list_result)['data']
+    end
+    @dataset_data = []
+    if @daily_paper.dataset_links.present?
+      dataset_list_result = Starhub.api.datasets_by_paths @daily_paper.dataset_links
+      @dataset_data = JSON.parse(dataset_list_result)['data']
+    end 
   end
 end
