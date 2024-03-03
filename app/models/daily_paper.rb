c927ebed84f1f0d79d3b773d629fbc023030d5fa
Hiveer <ljp.hiveer@gmail.com>
2024-03-03 20:15:51 +0800

    Bug fix: bad icon, bad value type, deprecated attr

--- app/models/daily_paper.rb
+++ app/models/daily_paper.rb
@@ -9,8 +9,8 @@ class DailyPaper < ApplicationRecord
 
   has_many :comments, as: :commentable, dependent: :destroy
 
-  scope :search_by, ->(keywords) { 
-    keywords.present? ? 
+  scope :search_by, ->(keywords) {
+    keywords.present? ?
       where('title ILIKE :keywords OR article_link ILIKE :keywords OR description ILIKE :keywords OR recommendation ILIKE :keywords', keywords: "%#{keywords}%") : self
   }
 
@@ -28,7 +28,7 @@ class DailyPaper < ApplicationRecord
       article_link: article_link,
       pdf_link: pdf_link,
       description: description,
-      video_link: video_link,
+      video_link: video_link || '',
       model_links: model_links,
       dataset_links: dataset_links,
       created_at: created_at,
