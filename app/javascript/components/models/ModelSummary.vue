c397a01e56a637303792d682704dd827f84d3ee9
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-03-05 22:08:00 +0800

    DatasetSummary ModelSummary i18n

--- app/javascript/components/models/ModelSummary.vue
+++ app/javascript/components/models/ModelSummary.vue
@@ -10,7 +10,7 @@
       </MarkdownViewer>
     </div>
     <div class="p-4 w-[20%] border-l border-[#EBEEF5] md:border-l-0 md:border-b md:w-full md:pl-0">
-      <div class="text-[#606266] text-base font-medium leading-[22px]">下载热度</div>
+      <div class="text-[#606266] text-base font-medium leading-[22px]">{{ $t('all.downloadCount') }}</div>
       <div class="text-[#303133] text-base font-semibold leading-6 mt-1">{{ downloadCount }}</div>
     </div>
   </div>
