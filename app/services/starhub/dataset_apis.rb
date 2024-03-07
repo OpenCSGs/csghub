7bd6b6641b8295a0d6e49fa313b183eaff1116cb
Hiveer <ljp.hiveer@gmail.com>
2024-03-07 14:41:13 +0800

    Extract the code repo access API to be a shared module

---
+++ app/services/starhub/dataset_apis.rb
@@ -0,0 +1,67 @@
+module Starhub
+  module DatasetApis
+    def get_dataset_detail_data_in_parallel(namespace, dataset_name, options = {})
+      get_repo_detail_data_in_parallel('datasets', namespace, dataset_name, options)
+    end
+
+    def get_dataset_detail_files_data_in_parallel(namespace, dataset_name, options = {})
+      get_repo_detail_files_data_in_parallel('datasets', namespace, dataset_name, options)
+    end
+
+    def get_dataset_detail_blob_data_in_parallel(namespace, dataset_name, options = {})
+      get_repo_detail_blob_data_in_parallel('datasets', namespace, dataset_name, options)
+    end
+
+    def get_datasets(current_user, keyword, sort_by, task_tag, framework_tag, license_tag, page = 1, per = 16)
+      get_repos('datasets', current_user, keyword, sort_by, task_tag, framework_tag, license_tag, page, per)
+    end
+
+    def get_datasets_detail(namespace, dataset_name, options = {})
+      get_repo_detail('datasets', namespace, dataset_name, options)
+    end
+
+    def get_datasets_files(namespace, dataset_name, options = {})
+      get_repo_files('datasets', namespace, dataset_name, options)
+    end
+
+    def get_datasets_last_commit(namespace, dataset_name, options = {})
+      get_repo_last_commit('datasets', namespace, dataset_name, options)
+    end
+
+    def get_datasets_branches(namespace, dataset_name, options = {})
+      get_repo_branches('datasets', namespace, dataset_name, options)
+    end
+
+    def get_datasets_file_content(namespace, dataset_name, path, options = {})
+      get_repo_file_content('datasets', namespace, dataset_name, path, options)
+    end
+
+    def create_dataset(username, dataset_name, namespace, nickname, desc, options = {})
+      create_repo('datasets', username, dataset_name, namespace, nickname, desc, options)
+    end
+
+    def delete_dataset(namespace, dataset_name, params = {})
+      delete_repo('datasets', namespace, dataset_name, params)
+    end
+
+    def update_dataset(username, dataset_name, namespace, nickname, desc, options = {})
+      update_repo('datasets', username, dataset_name, namespace, nickname, desc, options)
+    end
+
+    def get_datasets_tags(namespace, dataset_name, options = {})
+      get_repo_tags('datasets', namespace, dataset_name, options)
+    end
+
+    def download_datasets_file(namespace, dataset_name, path, options = {})
+      download_repo_file('datasets', namespace, dataset_name, path, options)
+    end
+
+    def create_dataset_file(namespace, dataset_name, path, options = {})
+      create_repo_file('datasets', namespace, dataset_name, path, options)
+    end
+
+    def upload_datasets_file(namespace, dataset_name, options = {})
+      upload_repo_file('datasets', namespace, dataset_name, options)
+    end
+  end
+end
---
+++ app/services/starhub/shared_repo_apis.rb
@@ -0,0 +1,120 @@
+module Starhub
+  module SharedRepoApis
+    def get_repo_detail_data_in_parallel(repo_type, namespace, repo_name, options = {})
+      options[:path] ||= '/'
+      options[:ref] ||= 'main'
+      paths = [
+        "/#{repo_type}/#{namespace}/#{repo_name}?current_user=#{options[:current_user]}",
+        "/#{repo_type}/#{namespace}/#{repo_name}/branches"
+      ]
+      @client.get_in_parallel(paths, options)
+    end
+
+    def get_repo_detail_files_data_in_parallel(repo_type, namespace, repo_name, options = {})
+      options[:path] ||= '/'
+      options[:ref] ||= 'main'
+      paths = [
+        "/#{repo_type}/#{namespace}/#{repo_name}/last_commit?ref=#{options[:ref]}",
+        "/#{repo_type}/#{namespace}/#{repo_name}/tree?#{options[:path]}&ref=#{options[:ref]}"
+      ]
+      @client.get_in_parallel(paths, options)
+    end
+
+    def get_repo_detail_blob_data_in_parallel(repo_type, namespace, repo_name, options = {})
+      options[:path] ||= '/'
+      options[:ref] ||= 'main'
+      paths = [
+        "/#{repo_type}/#{namespace}/#{repo_name}?current_user=#{options[:current_user]}",
+        "/#{repo_type}/#{namespace}/#{repo_name}/last_commit?ref=#{options[:ref]}",
+        "/#{repo_type}/#{namespace}/#{repo_name}/branches",
+        "/#{repo_type}/#{namespace}/#{repo_name}/raw/#{options[:path]}?ref=#{options[:ref]}"
+      ]
+      @client.get_in_parallel(paths, options)
+    end
+
+    def get_repos(repo_type, current_user, keyword, sort_by, task_tag, framework_tag, license_tag, page = 1, per = 16)
+      url = "/#{repo_type}?per=#{per}&page=#{page}"
+      url += "&current_user=#{current_user}" if current_user.present?
+      url += "&search=#{keyword}" if keyword.present?
+      url += "&sort=#{sort_by}" if sort_by.present?
+      url += "&task_tag=#{task_tag}" if task_tag.present?
+      url += "&framework_tag=#{framework_tag}" if framework_tag.present?
+      url += "&license_tag=#{license_tag}" if license_tag.present?
+      res = @client.get(url)
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def get_repo_detail(repo_type, namespace, repo_name, options = {})
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}?current_user=#{options[:current_user]}")
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def get_repo_files(repo_type, namespace, repo_name, options = {})
+      options[:path] ||= '/'
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}/tree?path=#{options[:path]}&ref=#{options[:ref]}")
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def get_repo_last_commit(repo_type, namespace, repo_name, options = {})
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}/last_commit?ref=#{options[:ref]}")
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def get_repo_branches(repo_type, namespace, repo_name, options = {})
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}/branches")
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def get_repo_file_content(repo_type, namespace, repo_name, path, options = {})
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}/raw/#{path}?ref=#{options[:ref]}")
+      raise StarhubError, res.body unless res.success?
+      res.body.force_encoding('UTF-8')
+    end
+
+    def create_repo(repo_type, username, repo_name, namespace, nickname, desc, options = {})
+      options[:username] = username
+      options[:name] = repo_name
+      options[:namespace] = namespace
+      options[:nickname] = nickname
+      options[:description] = desc
+      @client.post("/#{repo_type}", options)
+    end
+
+    def delete_repo(repo_type, namespace, repo_name, params = {})
+      @client.delete("/#{repo_type}/#{namespace}/#{repo_name}")
+    end
+
+    def update_repo(repo_type, username, repo_name, namespace, nickname, desc, options = {})
+      options[:username] = username
+      options[:name] = repo_name
+      options[:nickname] = nickname
+      options[:description] = desc
+      res = @client.put("/#{repo_type}/#{namespace}/#{repo_name}", options)
+    end
+
+    def get_repo_tags(repo_type, namespace, repo_name, options = {})
+      res = @client.get("/#{repo_type}/#{namespace}/#{repo_name}/tags")
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def download_repo_file(repo_type, namespace, repo_name, path, options = {})
+      res = @client.get("/#{repp_type}/#{namespace}/#{repo_name}/download/#{path}", options)
+      raise StarhubError, res.body unless res.success?
+      res.body
+    end
+
+    def create_repo_file(repo_type, namespace, repo_name, path, options = {})
+      @client.post("/#{repo_type}/#{namespace}/#{repo_name}/raw/#{path}", options)
+    end
+
+    def upload_repo_file(repo_type, namespace, repo_name, options = {})
+      @client.upload("/#{repo_type}/#{namespace}/#{repo_name}/upload_file", options)
+    end
+  end
+end
