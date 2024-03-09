9c0170d0c4d3886221bc43f6c7281100eaaf37e1
Just beat it <justBL@gmail.com>
2024-03-09 17:59:18 +0800

    通过创建时间去做倒排

--- app/controllers/daily_papers_controller.rb
+++ app/controllers/daily_papers_controller.rb
@@ -1,7 +1,7 @@
 class DailyPapersController < ApplicationController
   def index
     @daily_papers = DailyPaper.search_by(params[:keywords])
-                              .order(published_at: :desc)
+                              .order(created_at: :desc)
                               .page(params[:page]).per(9)
     @keywords = params[:keywords]
     @page = params[:page]
