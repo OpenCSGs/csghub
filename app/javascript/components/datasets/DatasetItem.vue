bba45839420bb9356c5b633a02b04485c2b11c97
XiaoXiAI <123750281+zzxr6@users.noreply.github.com>
2024-03-11 17:08:54 +0800

    更新模型/数据集的卡片  (#76)
    * Add nickname and tag in modelcard
    
    * Add nickname,desc and tag in datasetcard
    
    * Add task-tag in modelcard and datasetcard
    
    * Add task-tag in datasetcard
    
    * reset modeldetail
    
    * reset datasetdetail
    
    * reset datasetdetail
    
    * Update the DatasetItem.vue and ModelItem.vue
    
    * Update the card of model and dataset
    
    ---------
    
    Co-authored-by: zxr <2221258383@qq.com>

--- app/javascript/components/datasets/DatasetItem.vue
+++ app/javascript/components/datasets/DatasetItem.vue
@@ -2,10 +2,21 @@
   <a :href="`/datasets/${ dataset.path }`"
      class="dataset-card hover:active-dataset-card p-4 sm:w-full border border-gray-200 bg-white rounded-xl">
     <div class="flex items-center mb-[5px] w-[399px] sm:w-auto">
-      <div class="dataset-path text-sm text-[#303133] font-medium text-ellipsis overflow-hidden whitespace-nowrap">{{ dataset.path }}</div>
+      <div class="dataset-path text-sm text-[#303133] font-medium text-ellipsis overflow-hidden whitespace-nowrap">
+        {{ getPath }}
+      </div>
     </div>
+    <p class="h-[40px] w-[399px] sm:w-auto leading-[18px] mb-[5px] text-[#909399] text-xs overflow-hidden overflow-ellipsis line-clamp-2">
+      {{ dataset.description }}
+    </p>
     <div class="flex items-center gap-[8px] text-xs text-[#909399]">
-      <span>更新时间：{{ dataset.updated_at && dataset.updated_at.substring(0, 10) }}</span>
+      <span v-if="getTaskTagName">{{ getTaskTagName }}</span>
+      <span v-if="getTaskTagName">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1" height="8" viewBox="0 0 1 8" fill="none">
+          <path d="M0.5 0V8" stroke="#DCDFE6"/>
+        </svg>
+      </span>
+      <span>更新时间：{{ dataset.updated_at.substring(0, 10) }}</span>
       <span>
         <svg xmlns="http://www.w3.org/2000/svg" width="1" height="8" viewBox="0 0 1 8" fill="none">
           <path d="M0.5 0V8" stroke="#DCDFE6"/>
@@ -23,15 +34,24 @@
 </template>
 
 <script setup>
-  import { computed } from 'vue'
+import { computed } from 'vue'
 
-  const props = defineProps({
-    dataset: Object,
-  })
+const props = defineProps({
+  dataset: Object,
+})
 
-  const visibility = computed(() => {
-    return props.dataset.private ? '私有' : '公开'
-  })
+const getPath = computed(() => {
+  return props.dataset.path.split('/')[0] + '/' + (props.dataset.nickname === '' ? props.dataset.name : props.dataset.nickname)
+})
+
+const visibility = computed(() => {
+  return props.dataset.private ? '私有' : '公开'
+})
+
+const getTaskTagName = computed(() => {
+  const taskTag = (props.dataset.tags || []).find(tag => tag.category === "task")
+  return taskTag ? taskTag["show_name"] : null
+})
 </script>
 
 <style scoped>
