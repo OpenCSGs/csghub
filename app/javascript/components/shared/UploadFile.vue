1619ce8655e91524f9711233a769128fd64de05e
Hiveer <ljp.hiveer@gmail.com>
2024-03-13 09:25:37 +0800

    Fix bad locale key

--- app/javascript/components/shared/UploadFile.vue
+++ app/javascript/components/shared/UploadFile.vue
@@ -31,7 +31,7 @@
             </svg>
           </el-icon>
           <div class="el-upload__text py-[16px] px-[24px]">
-            {{ $t('all.dragOr') }} <em>{{ $t('all.clickUpload') }}</em>（{{ $t('all.onlyOneFile') }}）
+            {{ $t('shared.dragOr') }} <em>{{ $t('all.clickUpload') }}</em>（{{ $t('shared.onlyOneFile') }}）
           </div>
           <template #tip>
             <div class="el-upload__tip">
