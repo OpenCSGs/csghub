1c7f40a4cf2eaaf9d032dc72824fe31553c83939
Zhendi.Wang <zhendi.wang@gmail.com>
2024-03-14 09:51:31 +0800

    fix clone url (#81)

--- app/javascript/components/shared/RepoTabs.vue
+++ app/javascript/components/shared/RepoTabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <repo-clone :repo-type="repoType" :clone-http-url="repoDetail.repository.http_clone_url" :clone-ssh-url="repoDetail.repository.ssh_clone_url" />
+    <repo-clone :repo-type="repoType" :http-clone-url="repoDetail.repository.http_clone_url" :ssh-clone-url="repoDetail.repository.ssh_clone_url" />
     <tab-container :default-tab="defaultTab" :settingsVisibility="settingsVisibility">
       <template #summary>
         <repo-summary :repo-type="repoType" :namespace-path="repoDetail.path" :download-count="repoDetail.downloads" />
