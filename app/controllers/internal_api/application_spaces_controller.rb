0b1dca289bc90fc10a940ec7ac6bcfb10ce2ea21
Hiveer <ljp.hiveer@gmail.com>
2024-03-14 10:54:52 +0800

    Update application space internal api

--- app/controllers/internal_api/application_spaces_controller.rb
+++ app/controllers/internal_api/application_spaces_controller.rb
@@ -8,11 +8,6 @@ class InternalApi::ApplicationSpacesController < InternalApi::ApplicationControl
 
   def create
     application_space = current_user.created_application_spaces.build(create_params)
-    if params[:file].present?
-      cover_image_code = AwsS3.instance.upload 'application-space-cover-image', params[:file]
-      cover_image_url = AwsS3.instance.download cover_image_code
-      application_space.cover_image = cover_image_url
-    end
     if application_space.save
       render json: { path: application_space.path, message: I18n.t('application_space.space_created') }, status: :created
     else
@@ -23,6 +18,6 @@ class InternalApi::ApplicationSpacesController < InternalApi::ApplicationControl
   private
 
   def create_params
-    params.permit(:name, :nickname, :desc, :sdk, :cloud_resource, :owner_id, :owner_type, :visibility, :license)
+    params.permit(:name, :nickname, :desc, :sdk, :cloud_resource, :owner_id, :owner_type, :visibility, :license, :cover_image)
   end
 end
