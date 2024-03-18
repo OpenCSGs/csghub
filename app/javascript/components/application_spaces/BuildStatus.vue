4f7f852d6351f2d7e3f03207954275b8dc0f7991
Hiveer <ljp.hiveer@gmail.com>
2024-03-18 09:35:13 +0800

    Update application space build status

--- app/javascript/components/application_spaces/BuildStatus.vue
+++ app/javascript/components/application_spaces/BuildStatus.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center gap-[6px] border border-[#FEDF89] bg-[#FFFAEB] rounded-[6px] py-[2px] px-[8px]">
+  <div class="flex items-center gap-[6px] border border-[#99AFFF] bg-[#CED8FF] rounded-[6px] py-[2px] px-[8px]">
     <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
-      <circle cx="4" cy="4" r="3" fill="#4781F5"/>
+      <circle cx="4" cy="4" r="3" fill="#4D6AD6"/>
     </svg>
-    <p class="text-[14px] text-[#B54708] font-[400] leading-[20px]"> {{ statusName }} </p>
+    <p class="text-[14px] text-[#223B99] font-[400] leading-[20px]"> {{ statusName }} </p>
   </div>
 </template>
 
