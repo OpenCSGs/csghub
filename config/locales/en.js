a1270d0f022e85f7020147f8d48093f35786244f
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-03-21 11:45:59 +0800

    Update the name

--- config/locales/en.js
+++ config/locales/en.js
@@ -1,16 +1,16 @@
-import { rule } from './en/rule';
-import { models } from './en/models';
-import { comment } from './en/comment';
-import { datasets } from './en/datasets';
-import { organization } from './en/organization';
-import { navbar } from './en/navbar';
-import { all } from './en/all';
-import { community } from './en/community';
-import { profile } from './en/profile';
-import { gitToken } from './en/gitToken';
-import { sshKey } from './en/sshKey';
-import { shared } from './en/shared';
-import { repo } from './en/repo';
+import { rule } from './en_js/rule';
+import { models } from './en_js/models';
+import { comment } from './en_js/comment';
+import { datasets } from './en_js/datasets';
+import { organization } from './en_js/organization';
+import { navbar } from './en_js/navbar';
+import { all } from './en_js/all';
+import { community } from './en_js/community';
+import { profile } from './en_js/profile';
+import { gitToken } from './en_js/gitToken';
+import { sshKey } from './en_js/sshKey';
+import { shared } from './en_js/shared';
+import { repo } from './en_js/repo';
 
 export default {
   rule,
--- config/locales/zh.js
+++ config/locales/zh.js
@@ -1,16 +1,16 @@
-import { rule } from './zh/rule';
-import { models } from './zh/models';
-import { comment } from './zh/comment';
-import { datasets } from './zh/datasets';
-import { organization } from './zh/organization';
-import { navbar } from './zh/navbar';
-import { all } from './zh/all';
-import { community } from './zh/community';
-import { profile } from './zh/profile';
-import { gitToken } from './zh/gitToken';
-import { sshKey } from './zh/sshKey';
-import { shared } from './zh/shared';
-import { repo } from './zh/repo';
+import { rule } from './zh_js/rule';
+import { models } from './zh_js/models';
+import { comment } from './zh_js/comment';
+import { datasets } from './zh_js/datasets';
+import { organization } from './zh_js/organization';
+import { navbar } from './zh_js/navbar';
+import { all } from './zh_js/all';
+import { community } from './zh_js/community';
+import { profile } from './zh_js/profile';
+import { gitToken } from './zh_js/gitToken';
+import { sshKey } from './zh_js/sshKey';
+import { shared } from './zh_js/shared';
+import { repo } from './zh_js/repo';
 
 export default {
   rule,
