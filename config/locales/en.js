b4f825d2cfef80c56ae5d867c02579687d124b72
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-03-21 11:43:39 +0800

    Move the en and zh

---
+++ config/locales/en.js
@@ -0,0 +1,29 @@
+import { rule } from './en/rule';
+import { models } from './en/models';
+import { comment } from './en/comment';
+import { datasets } from './en/datasets';
+import { organization } from './en/organization';
+import { navbar } from './en/navbar';
+import { all } from './en/all';
+import { community } from './en/community';
+import { profile } from './en/profile';
+import { gitToken } from './en/gitToken';
+import { sshKey } from './en/sshKey';
+import { shared } from './en/shared';
+import { repo } from './en/repo';
+
+export default {
+  rule,
+  models,
+  comment,
+  datasets,
+  organization,
+  navbar,
+  all,
+  community,
+  profile,
+  gitToken,
+  sshKey,
+  shared,
+  repo
+};
---
+++ config/locales/zh.js
@@ -0,0 +1,29 @@
+import { rule } from './zh/rule';
+import { models } from './zh/models';
+import { comment } from './zh/comment';
+import { datasets } from './zh/datasets';
+import { organization } from './zh/organization';
+import { navbar } from './zh/navbar';
+import { all } from './zh/all';
+import { community } from './zh/community';
+import { profile } from './zh/profile';
+import { gitToken } from './zh/gitToken';
+import { sshKey } from './zh/sshKey';
+import { shared } from './zh/shared';
+import { repo } from './zh/repo';
+
+export default {
+  rule,
+  models,
+  comment,
+  datasets,
+  organization,
+  navbar,
+  all,
+  community,
+  profile,
+  gitToken,
+  sshKey,
+  shared,
+  repo
+};
