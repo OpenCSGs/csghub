fe2e02fdefe97e0120005dcd4113a5e14a035520
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-03-21 11:03:31 +0800

    字体大小 视频大小调整

--- app/javascript/components/ekb/EkbBanner.vue
+++ app/javascript/components/ekb/EkbBanner.vue
@@ -2,12 +2,12 @@
   <div class="text-center w-full bg-white">
     <div class="max-w-[1280px] m-auto py-[96px]">
       <div
-        class="m-auto text-[72px] md:text-[48px] md:leading-[60px] text-[#101828] leading-[90px]"
+        class="m-auto text-[60px] md:text-[48px] md:leading-[60px] text-[#101828] leading-[72px]"
       >
         {{ $t("ekb.title") }}
       </div>
       <div
-        class="m-auto mb-[24px] font-semibold text-[64px] md:text-[36px] leading-[90px] md:leading-[50px] bg-clip-text text-transparent bg-gradient-to-r from-[#2E1492] via-[#B73FA9] to-[#A45FE6]"
+        class="m-auto mb-[24px] font-semibold text-[60px] md:text-[36px] leading-[72px] md:leading-[50px] bg-clip-text text-transparent bg-gradient-to-r from-[#2E1492] via-[#B73FA9] to-[#A45FE6]"
       >
         {{ $t("ekb.desc") }}
       </div>
@@ -22,7 +22,7 @@
         {{ $t("solution.knowledge.demo") }}
       </a>
       <div class="grow flex justify-center items-center">
-        <div class="w-[592px] md:w-full">
+        <div class="max-w-[916px] md:w-full">
           <video style="width: 100%" controls autoplay muted>
             <source
               src="https://opencsg-public-resource.oss-accelerate.aliyuncs.com/video/EKBAI.mp4"
