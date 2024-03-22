0c203ae5a2afdae127e5e944f6625929a2b2c525
Hiveer <ljp.hiveer@gmail.com>
2024-03-22 17:07:06 +0800

    Code refactor to extract helper method

---
+++ app/helpers/repo_helper.rb
@@ -0,0 +1,10 @@
+module RepoHelper
+  def name_for_meta_title repo
+    JSON.parse(repo).fetch('data', {}).fetch('nickname', '') ||
+    JSON.parse(repo).fetch('data', {}).fetch('name', '')
+  end
+
+  def desc_for_meta_description repo
+    JSON.parse(repo).fetch('data', {}).fetch('description', '')
+  end
+end
