7d49316cdbd5c3d7a6b86843c8d06985ff5d482b
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-03-26 12:12:55 +0800

    修复readme_content可能为nil

--- app/controllers/internal_api/datasets_controller.rb
+++ app/controllers/internal_api/datasets_controller.rb
@@ -98,7 +98,7 @@ class InternalApi::DatasetsController < InternalApi::ApplicationController
     end_index = metadata_data.index('---', 3)
   
     # 提取数据部分
-    readme_content = metadata_data[end_index+4 .. -1]
+    readme_content = metadata_data[end_index+4 .. -1] || ""
 
     # 更新或添加 tags
     metadata_hash['tags'] = tags
