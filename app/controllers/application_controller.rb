2f1ca6d2b407df5d27f3c9cd9c52cf6eb0707da9
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-03-26 09:33:18 +0800

    去多余代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,7 +69,6 @@ class ApplicationController < ActionController::Base
     default_browser = default_browser == 'zh' ? default_browser : 'en'
     I18n.locale = params[:locale] || cookies[:locale] || default_browser
     cookies[:locale] = I18n.locale
-    local = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 
   def login_by_user_infos user_infos
