fa47262bba7e9eb64267bec3a1b78024407af737
Hiveer <ljp.hiveer@gmail.com>
2024-03-31 22:33:56 +0800

    Only when the user logged in and server synced, then raise alert if jwt token not there

--- app/javascript/packs/checkLogin.js
+++ app/javascript/packs/checkLogin.js
@@ -3,8 +3,9 @@ import { ElMessage } from "element-plus";
 const { cookies } = useCookies();
 const jwt = cookies.get('user_token');
 const login_identity = cookies.get('login_identity');
+const user_synced = cookies.get('user_synced')
 
-if(login_identity && !jwt) {
+if(login_identity && user_synced && !jwt) {
   ElMessage({
     message: '用户权限未生效，请重新登录!',
     type: 'warning'
