1d97f481b3f26842aeb8b22be4c69cd3c77bc0c8
Hiveer <ljp.hiveer@gmail.com>
2024-03-31 22:40:38 +0800

    Bug fix

--- app/javascript/packs/checkLogin.js
+++ app/javascript/packs/checkLogin.js
@@ -5,7 +5,9 @@ const jwt = cookies.get('user_token');
 const login_identity = cookies.get('login_identity');
 const user_synced = cookies.get('user_synced')
 
-if(login_identity && user_synced && !jwt) {
+const userSynced = user_synced.toString() === true
+
+if(login_identity && userSynced && !jwt) {
   ElMessage({
     message: '用户权限未生效，请重新登录!',
     type: 'warning'
