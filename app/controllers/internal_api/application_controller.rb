d937589e880fce6f154cd5a3680cd984df9fe0df
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-04-01 14:37:03 +0800

    Fix the bug when update email

--- app/controllers/internal_api/application_controller.rb
+++ app/controllers/internal_api/application_controller.rb
@@ -2,7 +2,9 @@ class InternalApi::ApplicationController < ApplicationController
   rescue_from StarhubError do |e|
     log_error e.message, e.backtrace
     handled_ssh = render_ssh_error e
-    render json: {message: "Git服务器报错"}, status: 500 unless handled_ssh
+    handled_user_edit = render_user_edit_error e
+    render_git = handled_ssh || handled_user_edit
+    render json: {message: "Git服务器报错"}, status: 500 unless render_git
   end
 
   rescue_from SensitiveContentError do |e|
@@ -19,6 +21,15 @@ class InternalApi::ApplicationController < ApplicationController
       return true
     elsif message.include?("key,error")
       render json: {message: I18n.t('ssh_key.non_deploy')}, status: 500
+      return true
+    end
+    false
+  end
+
+  def render_user_edit_error(e)
+    message = JSON.parse(e.message)["msg"]
+    if message.include?("user,error")
+      render json: {message: I18n.t('user_edit.email')}, status: 500
       return true
     end
     false
