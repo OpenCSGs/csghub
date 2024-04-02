8e65ae5c5dc8b2d679983b1bfddc67ced0a7228d
XiaoXiAI <123750281+zzxr6@users.noreply.github.com>
2024-04-02 08:30:24 +0800

    Update application_controller.rb

--- app/controllers/internal_api/application_controller.rb
+++ app/controllers/internal_api/application_controller.rb
@@ -1,7 +1,7 @@
 class InternalApi::ApplicationController < ApplicationController
   rescue_from StarhubError do |e|
     log_error e.message, e.backtrace
-    render json: {message: e.message}, status: 500
+    render json: { message: (JSON.parse(e.message)["msg"] rescue e.message) }, status: 500
   end
 
   rescue_from SensitiveContentError do |e|
