eb78f3960caecd47454c23de099a35b66952feca
Hiveer <ljp.hiveer@gmail.com>
2024-04-03 11:00:44 +0800

    Check jwt token expire time each time when loading the page

--- app/javascript/packs/checkLogin.js
+++ app/javascript/packs/checkLogin.js
@@ -1,15 +1,25 @@
 import { useCookies } from "vue3-cookies";
 import { ElMessage } from "element-plus";
+import csrfFetch from "./csrfFetch";
 const { cookies } = useCookies();
 const jwt = cookies.get('user_token');
-const login_identity = cookies.get('login_identity');
-const user_synced = cookies.get('user_synced')
+const loginIdentity = cookies.get('login_identity');
+const userSyncedStr = cookies.get('user_synced');
+const userSynced = !!userSyncedStr && userSyncedStr.toString() === 'true';
 
-const userSynced = !!user_synced && user_synced.toString() === 'true'
-
-if(login_identity && userSynced && !jwt) {
+if(loginIdentity && userSynced && !jwt) {
   ElMessage({
     message: '用户权限未生效，请重新登录!',
     type: 'warning'
   });
+} else {
+  const expireTimeStr = cookies.get('token_expire_at');
+  const expireTime = new Date(expireTimeStr);
+  const currentTime = new Date();
+  const differenceInMilliseconds = expireTime - currentTime;
+  let differenceInMinutes = differenceInMilliseconds / 1000 / 60;
+  if (differenceInMinutes < 10) {
+    const options = {method: 'PUT'}
+    csrfFetch('/internal_api/users/jwt_token', options)
+  }
 }
