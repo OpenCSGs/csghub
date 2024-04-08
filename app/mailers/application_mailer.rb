6aac6b253059417d926a3f5566a30176cb52c4d3
Hiveer <ljp.hiveer@gmail.com>
2024-04-08 15:00:33 +0800

    Setup default mailer settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,25 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  if Rails.env.production?
+    default from: 'community@opencsg.com'
+  else
+    default from: 'feedback@opencsg.com'
+  end
+
   layout "mailer"
+
+  private
+
+  def subject_prefix
+    if ENV.fetch('RAILS_ENV', '') == 'production'
+      "OpenCSG"
+    else
+      "测试(opencsg)"
+    end
+  end
+
+  def internal_group
+    [
+      'pingl@opencsg.com'
+    ]
+  end
 end
