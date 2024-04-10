2c63e90aad47245e61f74139c0fbd14f53c5e552
Hiveer <ljp.hiveer@gmail.com>
2024-04-10 15:35:11 +0800

    Uniform naming convention for user, repo, organization

--- app/models/model.rb
+++ app/models/model.rb
@@ -35,7 +35,7 @@ class Model < ApplicationRecord
   after_update :update_starhub_server_model
   before_save :detect_sensitive_content
 
-  validates :name, format: { with: /\A(?=.{2,70}$)(?!.*[_]{2})(?!.*[-]{2})(?!.*[.]{2})[a-zA-Z0-9_.-]+\Z/ }
+  validates :name, format: { with: NAME_RULE }
 
   validates :name, uniqueness: { scope: [:owner_type, :owner_id], case_sensitive: false }
 
