a4b94f822b210d4fdb43cba776826451deb18459
Hiveer <ljp.hiveer@gmail.com>
2024-04-10 11:38:01 +0800

    根据最新设计：优化组织新建页面layout (#137)
    * 优化组织创建和更新界面
    
    * Make nickname and homepage optional
    
    * Bug fix
    
    ---------
    
    Co-authored-by: Just beat it <justBL@gmail.com>

--- config/locales/en_js/rule.js
+++ config/locales/en_js/rule.js
@@ -2,5 +2,5 @@ export const rule = {
   nameRule:
     "String of 2-70 charaters, numbers and _.- (_.-discontinuous)",
   nameSpaceRule:
-    "String of 2-20 charaters, numbers and _",
+    "String of 2-20 charaters, numbers and _- (_-discontinuous)",
 }
