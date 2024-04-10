e2c1ea066b1653229de5965c32c8b01b297989d5
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-04-10 13:25:27 +0800

    Update ssh_keys_controller.rb

--- app/controllers/internal_api/ssh_keys_controller.rb
+++ app/controllers/internal_api/ssh_keys_controller.rb
@@ -1,36 +1,14 @@
 class InternalApi::SshKeysController < InternalApi::ApplicationController
   def create
-<<<<<<< Updated upstream
     res = Starhub.api.create_ssh_key(current_user.name, params[:name], params[:ssh_key])
     raise StarhubError, res.body unless res.success?
-    render json: { message: '添加成功' }
+    render json: { message: I18n.t('repo.createSuccess') }
   end
 
   def destroy
     res = Starhub.api.delete_ssh_key(current_user.name, params[:id])
     raise StarhubError, res.body unless res.success?
-    render json: {message: "SshKey destroyed"}
-=======
-    @ssh_key = SshKey.new(create_params)
-    @ssh_key.user = current_user
-    if @ssh_key.save
-      render json: { message: I18n.t('repo.createSuccess') }
-    else
-      render json: { message: @ssh_key.errors.full_messages.to_sentence }, status: :bad_request
-    end
-  end
-
-  def destroy
-    @ssh_key = SshKey.find_by(id: params[:id])
-    return render json: { message: "SshKey not found" }, status: :not_found unless @ssh_key
-    return render json: { message: "Unauthorized" }, status: :unauthorized unless @ssh_key.user.id == @ssh_key.user_id
-
-    if @ssh_key.destroy
-      render json: {message: I18n.t('repo.delSuccess')}
-    else
-      render json: {message: I18n.t('repo.delFailed')}, status: :bad_request
-    end
->>>>>>> Stashed changes
+    render json: {message: I18n.t('repo.delSuccess')}
   end
 
   private
