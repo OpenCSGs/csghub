9c85fb826370faa92dd1b2aaefcc1735636fa1ca
Hiveer <ljp.hiveer@gmail.com>
2024-04-11 08:17:12 +0800

    组织成员管理增删改 (#139)
    * Add organization edit and delete member feature
    
    * Revert back the name convention for orgnization settgings
    
    * Add more locales for organization
    
    * Use new org membership update api
    
    ---------
    
    Co-authored-by: Just beat it <justBL@gmail.com>

--- app/controllers/internal_api/organizations_controller.rb
+++ app/controllers/internal_api/organizations_controller.rb
@@ -1,5 +1,7 @@
 class InternalApi::OrganizationsController < InternalApi::ApplicationController
   before_action :authenticate_user, except: [:models, :datasets, :codes, :spaces]
+  before_action :set_org, only: [:update, :members, :remove_member, :update_member]
+  before_action :set_user, only: [:remove_member, :update_member]
 
   def create
     new_org = Organization.new organization_params
@@ -16,15 +18,13 @@ class InternalApi::OrganizationsController < InternalApi::ApplicationController
   end
 
   def update
-    org = Organization.find_by(name: params[:name])
-    return render json: {message: '未找到组织'}, status: 404 unless org
-    authorize org
+    authorize @org
 
     # 更新组织的信息
-    org.assign_attributes(organization_params)
+    @org.assign_attributes(organization_params)
 
     # 保存组织信息
-    if org.save
+    if @org.save
       render json: {message: '组织更新成功'}
     else
       render json: {message: "组织更新失败"}, status: 500
@@ -32,7 +32,7 @@ class InternalApi::OrganizationsController < InternalApi::ApplicationController
   rescue Pundit::NotAuthorizedError
     render json: {message: '更新未授权!'}, status: 401
   end
-  
+
   def new_members
     unless OrgMembership.roles.keys.include? params[:user_role]
       return render json: {message: '请提供角色信息'}, status: 400
@@ -84,6 +84,54 @@ class InternalApi::OrganizationsController < InternalApi::ApplicationController
     render json: Starhub.api.get_org_codes(params[:namespace], current_user&.name, { per: params[:per] })
   end
 
+  def members
+    authorize @org
+
+    users = @org.users.order(id: :desc).page(params[:page]).per(params[:per] || 8)
+
+    members = users.map do |user|
+      user.as_json.merge(role: user.org_role(@org))
+    end
+    render json: { members: members.as_json, total_count: users.total_count}
+  end
+
+  def remove_member
+    authorize @org
+    return render json: {message: '不能删除自己'}, status: 400 if current_user == @user
+
+    if @org.is_last_admin?(@user)
+      return render json: {message: '不能删除组织最后一个管理员！'}, status: 400
+    end
+
+    user_org_role = @user.org_role(@org)
+
+    Organization.transaction do
+      @user.org_membership_by_org(@org).destroy
+      sync_delete_membership(@org, @user, user_org_role)
+    end
+    render json: {message: '删除组织成员成功'}
+  rescue => e
+    render json: {message: "删除组织成员失败, #{e.message}"}, status: 400
+  end
+
+  def update_member
+    authorize @org
+
+    if @org.is_last_admin?(@user) && params[:user_role] != 'admin'
+      return render json: {message: '不能修改组织最后一个管理员的权限！'}, status: 400
+    end
+
+    old_role = @user.org_role @org
+
+    Organization.transaction do
+      @user.set_org_role(@org, params[:user_role])
+      sync_update_membership(@org, @user, params[:user_role], old_role)
+    end
+    render json: {message: '更新组织成员成功'}
+  rescue => e
+    render json: {message: "更新组织成员失败, #{e.message}"}, status: 400
+  end
+
   private
 
   def organization_params
@@ -100,8 +148,19 @@ class InternalApi::OrganizationsController < InternalApi::ApplicationController
     raise StarhubError, res.body unless res.success?
   end
 
-  def sync_update_membership(org, user, role)
-    sync_delete_membership(org, user, role)
-    sync_create_membership(org, user)
+  def sync_update_membership(org, user, new_role, old_role)
+    res = Starhub.api.update_membership(org.name, current_user.name, new_role, old_role, user.name)
+    raise StarhubError, res.body unless res.success?
   end
+
+  def set_org
+    @org = Organization.find_by(name: params[:id])
+    return render json: {message: '未找到组织'}, status: 404 unless @org
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+    return render json: {message: '未找到用户'}, status: 404 unless @user
+  end
+
 end
--- app/javascript/components/organizations/OrganizationSettings.vue
+++ app/javascript/components/organizations/OrganizationSettings.vue
@@ -1,27 +1,40 @@
 <template>
-    <div class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white">
-      <Menu class="max-w-[411px] md:mb-[24px]" 
-            :nickName="organizationNickname"
-            :homepage="organizationHomepage"
-            :name="organization.name"
-            :logo="organizationAvatar">
-      </Menu>
-      <OrganizationEdit class="grow py-[24px]"
-                        :organization="organization"
-                        @updateOrganization="updateOrganization" >
-      </OrganizationEdit>
-    </div>
-  </template>
-  <script>
+  <div class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white">
+    <Menu class="max-w-[411px] md:mb-[24px]" 
+          :nickName="organizationNickname"
+          :homepage="organizationHomepage"
+          :name="organization.name"
+          :logo="organizationAvatar">
+    </Menu>
+    <OrganizationEdit
+      v-if="action === 'edit'"
+      class="grow py-[24px]"
+      :organization="organization"
+      @updateOrganization="updateOrganization" >
+    </OrganizationEdit>
+    <OrganizationMembers
+      v-if="action === 'members'"
+      class="grow py-[24px]"
+      :admin="admin"
+      :organization="organization"
+    >
+    </OrganizationMembers>
+  </div>
+</template>
+<script>
   import Menu from "./Menu.vue";
   import OrganizationEdit from "./OrganizationEdit.vue";
+  import OrganizationMembers from "./OrganizationMembers.vue";
   export default {
     props: {
-      organization: Object
+      organization: Object,
+      action: String,
+      admin: Boolean,
     },
     components: {
       Menu,
       OrganizationEdit,
+      OrganizationMembers,
     },
     data() {
       return {
@@ -30,7 +43,6 @@
         organizationAvatar: this.organization.avatar,
       };
     },
-    mounted() {},
     methods: {
       updateOrganization(data) {
         const { nickname, homepage, logo } = data;
@@ -38,7 +50,7 @@
         this.organizationHomepage = homepage || this.organizationHomepage;
         this.organizationAvatar = logo || this.organizationAvatar;
       },
-    },
-  };
-  </script>
+    }
+  }
+</script>
   
