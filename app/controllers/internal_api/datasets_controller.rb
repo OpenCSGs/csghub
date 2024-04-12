eb6a33581d7058ae6cfcdcc47acdbbac04fad17d
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-04-12 22:19:07 +0800

    Csghub  flile avatar (#145)
    * last_commit_user
    
    * use last_commit_user avatar
    
    * data.last_commit_user

--- app/controllers/internal_api/datasets_controller.rb
+++ app/controllers/internal_api/datasets_controller.rb
@@ -21,7 +21,8 @@ class InternalApi::DatasetsController < InternalApi::ApplicationController
 
   def files
     last_commit, files = csghub_api.get_dataset_detail_files_data_in_parallel(params[:namespace], params[:dataset_name], files_options)
-    render json: { last_commit: JSON.parse(last_commit)['data'], files: JSON.parse(files)['data'] }
+    last_commit_user = User.find_by(name: JSON.parse(last_commit)["data"]["committer_name"])
+    render json: { last_commit: JSON.parse(last_commit)['data'], files: JSON.parse(files)['data'], last_commit_user: last_commit_user }
   end
 
   def readme
