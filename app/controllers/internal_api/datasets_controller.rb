e7c6b1a83a2de9b78e63ca10884e5fd3c1b4facd
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-04-15 18:01:24 +0800

    Update datasets_controller.rb

--- app/controllers/internal_api/datasets_controller.rb
+++ app/controllers/internal_api/datasets_controller.rb
@@ -12,8 +12,8 @@ class InternalApi::DatasetsController < InternalApi::ApplicationController
                                         params[:sort],
                                         params[:task_tag],
                                         params[:framework_tag],
-                                        params[:license_tag],
                                         params[:language_tag],
+                                        params[:license_tag],
                                         params[:page],
                                         params[:per_page])
     api_response = JSON.parse(res_body)
