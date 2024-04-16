7b4d4e36d5f1d82f21dfe6ac454010d664d49e0e
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-04-16 16:48:14 +0800

    组织页面InviteMember组件margin

--- app/javascript/components/organizations/OrganizationDetail.vue
+++ app/javascript/components/organizations/OrganizationDetail.vue
@@ -43,6 +43,7 @@
           <InviteMember :org-name="organization.name"
                         @resetMemberList="resetMemberList"
                         :admin="admin"
+                        class="my-[16px]"
           />
 
           <div class="mt-[16px] flex flex-wrap gap-[8px]">
