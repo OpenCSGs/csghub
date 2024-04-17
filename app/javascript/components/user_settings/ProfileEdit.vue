d7bc09044370d96b62174ae7870182df9234abee
Hiveer <ljp.hiveer@gmail.com>
2024-04-17 16:42:13 +0800

    Enable user to edit phone

--- app/javascript/components/user_settings/ProfileEdit.vue
+++ app/javascript/components/user_settings/ProfileEdit.vue
@@ -14,6 +14,8 @@
         {{ $t('profile.edit.removeAvatar') }}
       </div>
     </div>
+
+    <!-- name -->
     <div>
       <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.userName') }}</div>
       <p class="text-gray-500 text-[12px] italic">{{ $t('rule.nameRule') }}</p>
@@ -30,6 +32,8 @@
                 :placeholder="this.$t('all.userName')">
       </el-input>
     </div>
+
+    <!-- nickname -->
     <div>
       <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.nickName') }}</div>
       <el-input class="max-w-[400px]"
@@ -37,15 +41,47 @@
                 :placeholder="this.$t('all.nickName')">
       </el-input>
     </div>
+
+    <!-- phone -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.phone') }}<svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none"><path d="M2.21714 5.21809L3.35474 3.85009L4.49234 5.21809L5.12594 4.75729L4.20434 3.25969L5.77394 2.61169L5.52914 1.87729L3.88754 2.26609L3.74354 0.538086H2.96594L2.82194 2.28049L1.18034 1.87729L0.921143 2.61169L2.49074 3.25969L1.58354 4.75729L2.21714 5.21809Z" fill="#F56C6C"/></svg>
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.phone') }}
+        <svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none"><path d="M2.21714 5.21809L3.35474 3.85009L4.49234 5.21809L5.12594 4.75729L4.20434 3.25969L5.77394 2.61169L5.52914 1.87729L3.88754 2.26609L3.74354 0.538086H2.96594L2.82194 2.28049L1.18034 1.87729L0.921143 2.61169L2.49074 3.25969L1.58354 4.75729L2.21714 5.21809Z" fill="#F56C6C"/></svg>
       </div>
       <el-input class="max-w-[400px]"
                 v-model="inputPhone"
-                disabled
+                ref="phoneInputElement"
+                :disabled="!updatePhoneEnabled"
                 :placeholder="this.$t('all.phone')">
       </el-input>
+      <div v-if="updatePhoneEnabled" class="flex gap-[13px] max-w-[400px] items-center mt-[16px]">
+        <div class="flex items-center">
+          <p class="w-[88px] h-[40px] text-[#98A2B3] flex items-center justify-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200">验证码</p>
+          <input class="w-[117px] px-[12px] py-[8px] h-[40px] rounded-r-md border bg-white"
+                 v-model="inputSmsCode">
+          </input>
+        </div>
+        <button v-if="inCountDown"
+                class="rounded-md border border-gray-200 bg-gray-200 shadow-sm px-[14px] py-[8px] font-[500] text-[#98A2B3] text-[14px] leading-[20px]"
+        >
+          {{ countdown }}s 后重试
+        </button>
+        <button v-else
+                @click="sendSmsCode"
+                class="rounded-md border border-[#409EFF] bg-[#409EFF] shadow-sm px-[14px] py-[8px] font-[500] text-white text-[14px] leading-[20px]"
+        >
+          发送
+        </button>
+      </div>
+      <el-button v-else
+                 class="ml-[12px]"
+                 @click="enableUpdatePhone"
+      >
+        更新
+      </el-button>
     </div>
+
+    <!-- email -->
     <div>
       <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.email') }}<svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none"><path d="M2.21714 5.21809L3.35474 3.85009L4.49234 5.21809L5.12594 4.75729L4.20434 3.25969L5.77394 2.61169L5.52914 1.87729L3.88754 2.26609L3.74354 0.538086H2.96594L2.82194 2.28049L1.18034 1.87729L0.921143 2.61169L2.49074 3.25969L1.58354 4.75729L2.21714 5.21809Z" fill="#F56C6C"/></svg>
       </div>
@@ -80,10 +116,60 @@ export default {
       inputPhone: this.phone,
       inputEmail: this.email,
       avatarUrl: this.avatar,
+      inputSmsCode: '',
+      generatedSmscode: '',
+      updatePhoneEnabled: false,
+      inCountDown: false,
+      countdown: 30,
+      intervalId: null
     };
   },
   mounted() {},
   methods: {
+    async sendSmsCode() {
+      const profileSmsEndpoint = `/internal_api/users/sms`;
+      this.generatedSmscode = Math.floor(100000 + Math.random() * 900000).toString()
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          phone: this.inputPhone,
+          code: this.generatedSmscode
+        })
+      }
+      const response = await csrfFetch(profileSmsEndpoint, options)
+      if (!response.ok) {
+        response.json().then(data => {
+          ElMessage({
+            message: data.Message,
+            type: "warning",
+          })
+        })
+      } else {
+        this.inCountDown = true
+        this.intervalId = setInterval(() => {
+          if (this.countdown > 0) {
+            this.countdown--
+          } else {
+            clearInterval(this.intervalId)
+            this.inCountDown = false
+          }
+        }, 1000)
+
+        ElMessage({
+          message: "已发送",
+          type: "success",
+        })
+      }
+    },
+    enableUpdatePhone() {
+      this.updatePhoneEnabled = true
+      this.$refs.phoneInputElement.disabled = false
+    },
+    disableUpdatePhone() {
+      this.updatePhoneEnabled = false
+      this.$refs.phoneInputElement.disabled = true
+    },
     uploadImage() {
       this.$refs.fileInput.click();
     },
@@ -101,8 +187,17 @@ export default {
       if (file !== undefined) {
         formData.append("avatar", file);
       }
+      if (this.generatedSmscode !== this.inputSmsCode) {
+        ElMessage({
+          message: "验证码不匹配",
+          type: "warning",
+        });
+
+        return
+      }
       formData.append("name", this.inputName);
       formData.append("nickname", this.inputNickname);
+      formData.append("phone", this.inputPhone);
       formData.append("email", this.inputEmail);
       const options = {
         method: "PUT",
