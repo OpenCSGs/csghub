ad73b3ed6b900d0077b3cf29c786dade39a58c9b
Hiveer <ljp.hiveer@gmail.com>
2024-04-17 17:41:24 +0800

    Sync phone to csghub server

--- app/models/user.rb
+++ app/models/user.rb
@@ -143,10 +143,10 @@ class User < ApplicationRecord
     Starhub.api(session_ip).image_secure_check('profilePhotoCheck', bucket_name, avatar) if avatar.to_s.match(/^avatar\/*/)
 
     if starhub_synced?
-      res = Starhub.api(session_ip).update_user(name, nickname, email)
+      res = Starhub.api(session_ip).update_user(name, nickname, email, phone)
       raise StarhubError, res.body unless res.success?
     else
-      res = Starhub.api(session_ip).create_user(name, nickname, email)
+      res = Starhub.api(session_ip).create_user(name, nickname, email, phone)
       raise StarhubError, res.body unless res.success?
       starhub_synced!
     end
