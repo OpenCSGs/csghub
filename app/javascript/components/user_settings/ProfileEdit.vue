53c86b48008be819f4e9a8213751cd38675af547
Hiveer <ljp.hiveer@gmail.com>
2024-04-19 12:31:08 +0800

    Bug fix

--- app/javascript/components/user_settings/ProfileEdit.vue
+++ app/javascript/components/user_settings/ProfileEdit.vue
@@ -169,6 +169,7 @@ export default {
     },
     disableUpdatePhone() {
       this.updatePhoneEnabled = false
+      this.inputSmsCode = ''
       this.$refs.phoneInputElement.disabled = true
     },
     uploadImage() {
