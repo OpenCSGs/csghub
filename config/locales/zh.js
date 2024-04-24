e7f67590c54712e6b91a49a82d0ac6a48501fc9a
Hiveer <ljp.hiveer@gmail.com>
2024-04-24 17:04:09 +0800

    Rename landing.js locale to landing_page.js

--- config/locales/zh.js
+++ config/locales/zh.js
@@ -25,7 +25,7 @@ import { product } from './zh_js/product';
 import { solution } from './zh_js/solution';
 import { dailyPaper } from './zh_js/dailyPaper';
 import { form } from './zh_js/form';
-import { landing } from './zh_js/landing';
+import { landing_page } from './zh_js/landing_page';
 import { footer } from './zh_js/footer';
 import { ekb } from './zh_js/ekb';
 
@@ -60,7 +60,7 @@ export default {
   solution,
   dailyPaper,
   form,
-  landing,
+  landing_page,
   footer,
   ekb
 };
--- config/locales/zh_js/landing_page.js
+++ config/locales/zh_js/landing_page.js
@@ -1,4 +1,4 @@
-export const landing = {
+export const landingPage = {
   "join1": "加入我们",
   "join2": "我们正在寻找优秀的人才",
   "join3": "我们是一支分布在全国的分布式远程团队，打造中国版的 HuggingFace+优质社区，加入我们吧",
