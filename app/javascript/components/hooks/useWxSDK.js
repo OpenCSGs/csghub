8c2ad737167d44942a9e29d1c37239913c9ed96a
Hiveer <ljp.hiveer@gmail.com>
2024-04-25 10:48:47 +0800

    Update wechat share logo and remove debugger

--- app/javascript/components/hooks/useWxSDK.js
+++ app/javascript/components/hooks/useWxSDK.js
@@ -17,12 +17,11 @@ export default useWxSDK = () => {
       wx.checkJsApi({
         jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'],
         success: (res) => {
-          debugger
           if (res.checkResult.onMenuShareTimeline) {
             wx.onMenuShareTimeline({
               title: shareInfo.title, // 分享标题
               link: shareInfo.link, // 分享链接，可以不是当前页面，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: 'https://stg.opencsg.com/wechat_share_logo.png',
+              imgUrl: 'https://opencsg.com/wechat_share_logo.png',
               success: function () {
                 // 用户确认分享后执行的回调函数
                 console.log('yes, share moment ok')
@@ -41,7 +40,7 @@ export default useWxSDK = () => {
               title: shareInfo.title, // 分享标题
               desc: shareInfo.desc,
               link: shareInfo.link, // 分享链接，可以不是当前页面，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-              imgUrl: 'https://stg.opencsg.com/wechat_share_logo.png',
+              imgUrl: 'https://opencsg.com/wechat_share_logo.png',
               type: 'link', // 分享类型,music、video或link，不填默认为link
               success: function () {
                 // 用户确认分享后执行的回调函数
