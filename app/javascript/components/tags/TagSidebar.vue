f6bf20d08a43357d56b1250753edc10702fc5f73
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-04-25 14:59:28 +0800

    Update indent and language_tag_svg

--- app/javascript/components/tags/TagSidebar.vue
+++ app/javascript/components/tags/TagSidebar.vue
@@ -103,10 +103,10 @@
         />
         <div class="flex gap-[8px] flex-wrap">
           <span v-for="languageTag in theLanguageTags" 
-                    class="text-[14px] text-[#303133] px-[8px] py-[4px] rounded-[4px] cursor-pointer flex items-center gap-[4px]"
-                    :data-tag_name="languageTag.name"
-                    :style="setLanguageTagColor(languageTag.label)"
-                    @click="setActiveLanguageTag(languageTag.label)"
+                class="text-[14px] text-[#303133] px-[8px] py-[4px] rounded-[4px] cursor-pointer flex items-center gap-[4px]"
+                :data-tag_name="languageTag.name"
+                :style="setLanguageTagColor(languageTag.label)"
+                @click="setActiveLanguageTag(languageTag.label)"
           >
             <svg :stroke="setSvgColor(languageTag.label)" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 6C11 8.76142 8.76142 11 6 11M11 6C11 3.23858 8.76142 1 6 1M11 6C11 4.89543 8.76142 4 6 4C3.23858 4 1 4.89543 1 6M11 6C11 7.10457 8.76142 8 6 8C3.23858 8 1 7.10457 1 6M6 11C3.23858 11 1 8.76142 1 6M6 11C7.10457 11 8 8.76142 8 6C8 3.23858 7.10457 1 6 1M6 11C4.89543 11 4 8.76142 4 6C4 3.23858 4.89543 1 6 1M1 6C1 3.23858 3.23858 1 6 1" stroke-width="0.75" stroke-linecap="round" stroke-linejoin="round"/></svg>
             {{ cookies.get('locale') === 'en' ? languageTag.name : languageTag.zh_name }}
