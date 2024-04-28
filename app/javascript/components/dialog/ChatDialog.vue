e04259994330743ecaa7022cf30a0204282df20a
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-04-28 20:50:53 +0800

    传神奇和 chat 组件

--- app/javascript/components/dialog/ChatDialog.vue
+++ app/javascript/components/dialog/ChatDialog.vue
@@ -1,52 +1,34 @@
 <template>
-  <el-dialog
-    v-model="dialogVisible"
-    width="98%"
-    top="10vh"
-    :show-close="true"
-    :close-on-click-modal="false"
-    :before-close="handleClose"
-    :style="{ borderRadius: '10px' }"
-    class="max-w-[438px] md:w-full chatDialog"
-  >
-    <template #header="{ close }">
-      <div class="flex gap-4 items-center pt-6">
-        <SvgIcon name="ekb_icon" />
-        <div class="flex flex-col">
-          <div class="text-[20px] leading-[30px] text-[#101828]">传奇兽</div>
-          <div class="text-[14px] leading-[20px] text-[#475467] font-light">更加智能的大语言模型知识助手</div>
-        </div>
+  <div v-if="dialogVisible" class="mt-2 min-w-[438px] bg-white p-6 md:p-1 rounded-md shadow-md md:min-w-[340px] md:w-full z-20 relative">
+    <div class="flex gap-4 items-center border-b pb-6 mb-6 border-[#EAECF0] relative">
+      <SvgIcon name="ekb_icon" />
+      <div class="flex flex-col">
+        <div class="text-[20px] leading-[30px] text-[#101828]">传奇兽</div>
+        <div class="text-[14px] leading-[20px] text-[#475467] font-light">更加智能的大语言模型知识助手</div>
       </div>
-      <div class="flex w-full my-6 bg-[#EAECF0] h-[1px]"></div>
-    </template>
-    <div class="w-full h-full min-h-[600px] md:min-h-[400px]">
-      <iframe  v-if="chatSrc" :src="chatSrc" width="100%" class="min-h-[600px] md:min-h-[400px] rounded-[10px]" />
+      <SvgIcon @click="handleClose" name="close" class="absolute cursor-pointer right-0 top-0"/>
     </div>
-  </el-dialog>
+    <div class="w-full h-full min-h-[600px] md:min-h-[400px] overflow-hidden">
+      <iframe v-if="chatSrc" :src="chatSrc" width="100%" class="min-h-[600px] md:min-h-[400px] rounded-[10px]" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-  import { ref, onMounted, inject, computed } from 'vue';
-  import useChatDialogStore from '../../stores/ChatStore.js';
-  const chatStore = useChatDialogStore();
+  import { ref } from 'vue';
   const chatSrc = 'https://ekb.opencsg.com/chat?app_id=2dwvScahVOWtR9v4SfF3z0e5UWc&theme=light&lang=zh-CN'
-  
-  const props = defineProps({
-    isShow: String
-  });
+  const dialogVisible = ref(false)
 
-  const dialogVisible = computed({
-    get: () => chatStore.isVisibility,
-    set: (value) => chatStore.updateVisibility(value),
-  });
-
-  const openDialog = () => {
+  const handleOpen = () =>{
     dialogVisible.value = true;
-  };
+  }
 
   const handleClose = () => {
     dialogVisible.value = false;
   };
+  defineExpose({
+    handleOpen
+  })
 </script>
 <style>
 .chatDialog {
---
+++ app/javascript/components/dialog/LittleMonster.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fixed bottom-[50px] right-[30px] z-20">
+    <ChatDialog ref="chatRef" />
+    <el-popover placement="top-start" :width="204" trigger="hover">
+      <h3 class="text-center mb-[3px]">联系小助手了解更多</h3>
+      <img :src="littleAssistant" width="180" height="180" class="z-10">
+      <template #reference>
+        <img :src="monsterGif" class="w-[120px] sm:w-[60px]" @click="showChatDialog">
+      </template>
+    </el-popover>
+  </div>
+</template>
+<script setup>
+  import { ref, onMounted, inject, computed } from 'vue';
+  import useChatDialogStore from '../../stores/ChatStore.js';
+  import ChatDialog from './ChatDialog.vue'
+  const chatRef =ref(null)
+
+  const props = defineProps({
+    littleAssistant: String,
+    monsterGif: String,
+  });
+
+  const showChatDialog = () =>{
+    chatRef.value.handleOpen()
+  }
+
+</script>
