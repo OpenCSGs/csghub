64a06d7e9df3572b41be1015707b700e655e10c8
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-04-29 19:26:11 +0800

    去掉二维码

--- app/javascript/components/dialog/LittleMonster.vue
+++ app/javascript/components/dialog/LittleMonster.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="fixed bottom-[50px] right-[30px] z-20">
     <ChatDialog ref="chatRef" />
-    <el-popover placement="top-start" :width="204" trigger="hover">
-      <h3 class="text-center mb-[3px]">联系小助手了解更多</h3>
-      <img :src="littleAssistant" width="180" height="180" class="z-10">
-      <template #reference>
-        <img :src="monsterGif" class="w-[120px] sm:w-[60px]" @click="showChatDialog">
-      </template>
-    </el-popover>
+    <img :src="monsterGif" class="w-[120px] sm:w-[60px]" @click="showChatDialog">
   </div>
 </template>
 <script setup>
