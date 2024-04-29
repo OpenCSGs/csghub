f797106255470611f4f8bd69f19f07062e45edb0
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-04-29 13:54:46 +0800

    小屏 h5 chat w-max

--- app/javascript/components/dialog/ChatDialog.vue
+++ app/javascript/components/dialog/ChatDialog.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="dialogVisible" class="h-[75vh] fixed flex flex-col justify-between top-2 right-0 mt-2 min-w-[438px] bg-white p-6 md:p-1 rounded-md shadow-md md:min-w-[300px] md:w-full z-20 border border-[[#EAECF0]]">
+  <div v-if="dialogVisible" class="h-[75vh] fixed flex flex-col justify-between top-2 right-0 mt-2 min-w-[438px] bg-white p-6 md:p-1 rounded-md shadow-md md:min-w-[300px] md:w-full sm:max-w-[375px]  z-20 border border-[[#EAECF0]]">
     <div class="flex gap-4 items-center border-b pb-6 mb-6 border-[#EAECF0] relative">
       <SvgIcon name="ekb_icon" />
       <div class="flex flex-col">
@@ -8,7 +8,7 @@
       </div>
       <SvgIcon @click="handleClose" name="close" class="absolute cursor-pointer right-0 top-0"/>
     </div>
-    <div class="w-full h-full overflow-hidden">
+    <div class="w-full h-full sm:max-w-[375px] overflow-hidden">
       <iframe v-if="chatSrc" :src="chatSrc" width="100%" height="100%" class="rounded-[10px]" />
     </div>
   </div>
