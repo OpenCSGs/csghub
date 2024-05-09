ff6471b20eac076b3e721453163e444e0ce0b684
Hiveer <ljp.hiveer@gmail.com>
2024-05-09 23:49:30 +0800

    Initialize deplayed job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_25_131331) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_09_151316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -144,6 +144,21 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_25_131331) do
     t.string "origin"
     t.index ["creator_id"], name: "index_datasets_on_creator_id"
     t.index ["owner_type", "owner_id"], name: "index_datasets_on_owner"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "discussions", force: :cascade do |t|
