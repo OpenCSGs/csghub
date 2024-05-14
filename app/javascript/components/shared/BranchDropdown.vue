69f2850b8999e4b2b17917e0acdefc3c4bf70f18
Zhendi.Wang <zhendi.wang@gmail.com>
2024-05-14 16:45:06 +0800

    Update branch dropdown (#213)
    * update branch dropdown
    
    * update icon
    
    * code indent
    
    * update changeBranch
    
    * update filelist changeBranch

---
+++ app/javascript/components/shared/BranchDropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <el-dropdown>
+      <el-button type="default">
+        <SvgIcon name="branch" class="mr-1" />
+        {{ currentBranch }}
+        <el-icon class="ml-1 el-icon--right">
+          <arrow-down />
+        </el-icon>
+      </el-button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item
+            v-for="branch in branches"
+            :key="branch.name"
+            @click="handleClick(branch.name)"
+          >
+            {{ branch.name }}
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    currentBranch: String,
+    branches: Array
+  })
+
+  const emit = defineEmits(['changeBranch'])
+
+  const handleClick = (branch) => {
+    emit('changeBranch', branch)
+  }
+</script>
