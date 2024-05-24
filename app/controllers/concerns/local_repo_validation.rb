d218bdf9328abe62302cfa19eba12e920474a7e0
Hiveer <ljp.hiveer@gmail.com>
2024-05-24 08:06:57 +0800

    Enable the endpoint for local repo validation

--- app/controllers/concerns/local_repo_validation.rb
+++ app/controllers/concerns/local_repo_validation.rb
@@ -2,7 +2,18 @@ module LocalRepoValidation
   extend ActiveSupport::Concern
 
   included do
-    before_action only: [:show, :files, :blob, :new_file, :upload_file, :edit_file, :resolve, :community, :settings, :commits, :commit] do
+    before_action only: [:show,
+                         :files,
+                         :blob,
+                         :new_file,
+                         :upload_file,
+                         :edit_file,
+                         :resolve,
+                         :community,
+                         :settings,
+                         :commits,
+                         :commit,
+                         :logs] do
       local_repo_validation
     end
 
@@ -45,6 +56,8 @@ module LocalRepoValidation
       @local_code = @owner && @owner.codes.find_by(name: params[:code_name])
     when 'application_spaces'
       @local_application_space = @owner && @owner.application_spaces.find_by(name: params[:application_space_name])
+    when 'endpoints'
+      @local_endpoint = @owner && @owner.endpoints.find_by(name: params[:endpoint_name])
     end
   end
 
