7ce12b1c9c7ce1428df5b7dfd05493173f0f67d2
zhendi <zhendi.wang@gmail.com>
2024-05-27 20:18:49 +0800

    update endpoint api

--- app/services/starhub/endpoint_apis.rb
+++ app/services/starhub/endpoint_apis.rb
@@ -6,7 +6,7 @@ module Starhub
       options[:min_replica] = endpoint.min_replica
       options[:max_replica] = endpoint.max_replica
       options[:runtime_framework_id] = endpoint.framework_id
-      options[:secure_level] = endpoint.endpoint_private? ? 1 : 0
+      options[:secure_level] = endpoint.endpoint_private? ? 2 : 1
       @client.post("/models/#{endpoint.model_path}/run?current_user=#{endpoint.owner.name}", options)
     end
   end
