ca940a2fb760197eec353e458300dbc2cf7a5813
Hiveer <ljp.hiveer@gmail.com>
2024-05-29 09:19:37 +0800

    Show available endpoint instances on logs page

--- app/javascript/components/endpoints/EndpointLogs.vue
+++ app/javascript/components/endpoints/EndpointLogs.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>Test</div>
+  <div class="my-[16px]">
+    <el-select v-model="currentInstance" placeholder="Select" style="width: 240px">
+      <el-option
+        v-for="instance in instances"
+        :key="instance.name"
+        :label="instance.name"
+        :value="instance.name"
+      />
+    </el-select>
+    <div class="h-[80vh] border mt-[8px]">
+
+    </div>
+  </div>
 </template>
 
 <script setup>
+  import { ref, computed } from 'vue'
+
   const props = defineProps({
-    instances: Object,
+    instances: Array,
     modelId: String,
     deployId: Number
   })
+
+  const defaultInstance = computed(() => {
+    if (props.instances.length !== 0) {
+      return props.instances[0].name
+    } else {
+      return ''
+    }
+  })
+
+  const currentInstance = ref(defaultInstance)
 </script>
