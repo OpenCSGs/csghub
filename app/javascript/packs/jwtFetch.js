d1daad69d5be22ee092c90a9ff2301a8b6819eb4
XiaoXiAI <123750281+zzxr6@users.noreply.github.com>
2024-05-31 14:27:39 +0800

    未登录下跳转登录页面 (#230)
    * Update jwtFetch.js
    
    * Add forceLogin
    
    ---------
    
    Co-authored-by: 朱鑫睿 <xinrui.zhu@opencsg.com>

--- app/javascript/packs/jwtFetch.js
+++ app/javascript/packs/jwtFetch.js
@@ -1,8 +1,11 @@
 import { useCookies } from "vue3-cookies";
 const { cookies } = useCookies();
 
-const jwtFetch = (url, options = {}) => {
+const jwtFetch = (url, options = {}, forceLogin = false) => {
   const jwtToken = cookies.get('user_token')
+  if (forceLogin && !jwtToken) {
+    window.location.href = "/login"
+  }
   options.headers = { "Authorization": `Bearer ${jwtToken}`, ...options.headers };
   return fetch(url, options)
 };
