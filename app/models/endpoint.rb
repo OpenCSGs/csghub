a9546c89e63df0c7302c856319bdf2a0209dc195
zhendi <zhendi.wang@gmail.com>
2024-06-06 16:00:04 +0800

    update endpoint attributes in settings page

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -6,6 +6,7 @@ class Endpoint < ApplicationRecord
 
   before_save :detect_sensitive_content
   after_create :sync_created_endpoint_to_starhub_server
+  after_update :sync_updated_endpoint_to_starhub_server
   after_destroy :delete_endpoint_from_starhub_server
 
   validates :name, format: { with: NAME_RULE }
@@ -18,6 +19,11 @@ class Endpoint < ApplicationRecord
     res = Starhub.api(creator.session_ip).create_endpoint(self)
     raise StarhubError, res.body unless res.success?
     self.update_column('endpoint_id', JSON.parse(res.body)['data']['deploy_id'])
+  end
+
+  def sync_updated_endpoint_to_starhub_server
+    res = Starhub.api(creator.session_ip).update_endpoint(self)
+    raise StarhubError, res.body unless res.success?
   end
 
   def delete_endpoint_from_starhub_server
