5f03e6f6952e8cc657bfd6ab3efa0ac2b7f3d6bb
Hiveer <ljp.hiveer@gmail.com>
2024-06-07 17:01:46 +0800

    Adjust the font weight

--- app/javascript/components/new_admin/AdminNavbar.vue
+++ app/javascript/components/new_admin/AdminNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-[60px] border">
     <div class="max-w-[1280px] m-auto h-full flex justify-between items-center">
-      <p> OpenCSG Admin Pannel </p>
+      <p class="font-[700]"> OpenCSG Admin Pannel </p>
       <a href="/" target="_blank" class="font-[500] text-[#009900] underline"> Open Site </a>
       <p>{{ userName }} ( {{ userRoles }} )</p>
     </div>
