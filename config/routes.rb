3c57e24d92b84217819e948c9080245bffba9047
HiveerLi <pingl@opencsg.com>
2024-06-08 03:33:24 +0000

    Revert "Merge branch 'main__jwt-ssh' into 'main'"
    This reverts merge request !789

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
     resources :campaigns, only: [:index]
     resources :comments, only: [:create, :destroy, :index]
     resources :leads, only: [:create]
+    resources :ssh_keys, only: [:create, :destroy]
     resources :access_token, only: [] do
       collection do
         post 'refresh', to: 'access-tokens/refresh'
