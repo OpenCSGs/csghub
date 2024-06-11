6007aa2c17a89112ca5075530fafe0f283920b64
Hiveer <ljp.hiveer@gmail.com>
2024-06-11 11:15:07 +0800

    Optimize the new admin layout

--- app/javascript/components/new_admin/AdminMenu.vue
+++ app/javascript/components/new_admin/AdminMenu.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex flex-col gap-[8px] w-[200px] p-[20px] border rounded-[10px]">
-    <RouterLink to="/new_admin/">Go to Home</RouterLink>
-    <RouterLink to="/new_admin/email_sending">Email Sending</RouterLink>
+  <div class="flex flex-col gap-[8px] w-[260px] p-[20px] border rounded-[10px] bg-[#F4F4F6]">
+    <RouterLink to="/new_admin/" class="underline">Go to Home</RouterLink>
+    <RouterLink to="/new_admin/email_sending" class="underline">Email Sending</RouterLink>
   </div>
 </template>
 
