4429637c029d621162c383a643fab92efde6bd8e
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-06-13 09:52:31 +0800

    profile repos use jwt (#249)
    * use jwt profile
    
    * 去无用props
    
    * use Promise.all
    
    * let =>const
    
    ---------
    
    Co-authored-by: minghao.yang <mh.yang@opencsg.com>

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,11 +5,6 @@ class ProfileController < ApplicationController
     @user ||= User.find_by(name: params[:user_id])
     return redirect_to errors_not_found_path unless @user
 
-    @models = csghub_api.get_user_models(@user.name, current_user&.name)
-    @datasets = csghub_api.get_user_datasets(@user.name, current_user&.name)
-    @spaces = csghub_api.get_user_application_spaces(@user.name, current_user&.name)
-    @codes = csghub_api.get_user_codes(@user.name, current_user&.name)
-    @endpoints = csghub_api.get_user_endpoints(@user.name, current_user&.name)
     @organizations = @user.organizations
     @is_current_user_access = current_user.present? && (current_user == @user)
     @initiator = "profile"
@@ -19,10 +14,6 @@ class ProfileController < ApplicationController
     @user ||= User.find_by(name: params[:user_id])
     return redirect_to errors_not_found_path unless @user
 
-    @models = csghub_api.get_user_likes(@user.name, 'models')
-    @datasets = csghub_api.get_user_likes(@user.name, 'datasets')
-    @spaces = csghub_api.get_user_likes(@user.name, 'spaces')
-    @codes = csghub_api.get_user_likes(@user.name, 'codes')
     @organizations = @user.organizations
     @is_current_user_access = current_user.present? && (current_user == @user)
     @initiator = "likes"
