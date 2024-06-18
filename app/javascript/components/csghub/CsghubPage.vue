4da37fbc0b2b2df540ae8e5bfece5ed5f342d88e
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-06-18 13:19:12 +0800

    Update CsghubPage.vue

--- app/javascript/components/csghub/CsghubPage.vue
+++ app/javascript/components/csghub/CsghubPage.vue
@@ -7,39 +7,9 @@
   </div>
 </template>
 
-<script>
-import CsghubBanner from "./CsghubBanner.vue";
-import CsghubIntroduce from "./CsghubIntroduce.vue";
-import CsghubPrice from "./CsghubPrice.vue";
-import CsghubServe from "./CsghubServe.vue";
-
-export default {
-  props: {},
-
-  data() {
-    const classParam = new URLSearchParams(window.location.search).get('class');
-    return {
-      scrollToClass: classParam ? classParam : ''
-    };
-  },
-
-  components: {
-    CsghubBanner,
-    CsghubIntroduce,
-    CsghubPrice,
-    CsghubServe
-  },
-
-  mounted() {
-    this.scrollToContent()
-  },
-  methods: {
-    scrollToContent() {
-      const element = document.getElementsByClassName(this.scrollToClass);
-      if (element.length > 0) {
-        element[0].scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  },
-};
+<script setup>
+  import CsghubBanner from "./CsghubBanner.vue";
+  import CsghubIntroduce from "./CsghubIntroduce.vue";
+  import CsghubPrice from "./CsghubPrice.vue";
+  import CsghubServe from "./CsghubServe.vue";
 </script>
