cdfd164b43257e4458adfbb1eb8b71fa845c10a1
zhendi <zhendi.wang@gmail.com>
2024-06-20 16:15:27 +0800

    fix conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -82,10 +82,6 @@ Rails.application.routes.draw do
     get '/models/:namespace/(*model_name)/readme', to: 'models#readme', namespace: /[^\/]+/
     get '/models/:namespace/(*model_name)/files', to: 'models#files', namespace: /[^\/]+/
     post '/models/:namespace/(*model_name)/files/:branch/upload_file', to: 'models#upload_file', namespace: /[^\/]+/
-<<<<<<< HEAD
-    put '/models/:namespace/(*model_name)/update_readme_tags', to: 'models#update_readme_tags', namespace: /[^\/]+/
-=======
->>>>>>> main
     delete '/models/:namespace/(*model_name)', to: 'models#destroy', format: false, defaults: {format: 'html'}, namespace: /[^\/]+/
     put '/models/:namespace/(*model_name)', to: 'models#update', format: false, defaults: {format: 'html'}, namespace: /[^\/]+/
     post '/models/:namespace/(*model_name)/predict', to: 'models#predict', namespace: /[^\/]+/
@@ -96,10 +92,6 @@ Rails.application.routes.draw do
     get '/datasets/:namespace/(*dataset_name)/files', to: 'datasets#files', namespace: /[^\/]+/
     get '/datasets/:namespace/(*dataset_name)/preview', to: 'datasets#preview_parquet', namespace: /[^\/]+/
     post '/datasets/:namespace/(*dataset_name)/files/:branch/upload_file', to: 'datasets#upload_file', namespace: /[^\/]+/
-<<<<<<< HEAD
-    put '/datasets/:namespace/(*dataset_name)/update_readme_tags', to: 'datasets#update_readme_tags', namespace: /[^\/]+/
-=======
->>>>>>> main
     delete '/datasets/:namespace/(*dataset_name)', to: 'datasets#destroy', format: false, defaults: {format: 'html'}, namespace: /[^\/]+/
     put '/datasets/:namespace/(*dataset_name)', to: 'datasets#update', format: false, defaults: {format: 'html'}, namespace: /[^\/]+/
     get '/datasets/:namespace/(*dataset_name)/related_repos', to: 'datasets#related_repos', namespace: /[^\/]+/
