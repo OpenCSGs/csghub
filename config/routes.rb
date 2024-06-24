d7a36ccb71ee5342d4892c337329377ca8a0d9de
Hiveer <ljp.hiveer@gmail.com>
2024-06-24 19:29:31 +0800

    Migrate access token logic to JWT way (#266)
    * get token refresh token ,create token
    
    * 修改方法名 信息展示后台返回的错误
    
    * Code refactor
    
    * Introduce uuid and use it in access token settings
    
    * Remove internal api access token controller
    
    * Set current user cookie after login: will be used to retrieve user info
    later
    
    * Fetch user token from api for repo clone
    
    * Fix code block format
    
    ---------
    
    Co-authored-by: ymh6315431 <86564687+ymh6315431@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -34,11 +34,6 @@ Rails.application.routes.draw do
       end
     end
     resources :comments, only: [:create, :destroy, :index]
-    resources :access_token, only: [] do
-      collection do
-        post 'refresh', to: 'access-tokens/refresh'
-      end
-    end
     resources :users, only: [:index, :update] do
       collection do
         put 'jwt_token', to: 'users/jwt_token'
