93f1e7fe669f8f1a3569c0f396ba25cba0467f1d
Hiveer <ljp.hiveer@gmail.com>
2024-06-24 23:41:08 +0800

    Support update user phone and login_identity to server

--- app/services/starhub/api.rb
+++ app/services/starhub/api.rb
@@ -13,20 +13,24 @@ module Starhub
       @client = Starhub::Client.init_with user_ip
     end
 
-    def create_user(name, nickname, email)
+    def create_user(name, nickname, phone, email, login_identity)
       options = {
         username: name,
         name: nickname,
-        email: email
+        email: email,
+        phone: phone,
+        casdoor_uid: login_identity
       }
       @client.post("/users?current_user=#{name}", options)
     end
 
-    def update_user(name, nickname, email)
+    def update_user(name, nickname, phone, email, login_identity)
       options = {
         username: name,
         name: nickname,
-        email: email
+        email: email,
+        phone: phone,
+        casdoor_uid: login_identity
       }
       @client.put("/users/#{name}?current_user=#{name}", options)
     end
