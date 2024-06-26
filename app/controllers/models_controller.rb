97dbebbb5c8bf11e07e9cf7092dffcf402a9504e
Hiveer <ljp.hiveer@gmail.com>
2024-06-26 15:59:19 +0800

    Bug fix for repo before action checks

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -6,7 +6,7 @@ class ModelsController < ApplicationController
   include BlobContentHelper
 
   before_action :check_user_info_integrity
-  before_action :authenticate_user, only: [:new, :new_file, :upload_file, :edit_file]
+  before_action :authenticate_user, only: [:new, :new_file, :upload_file, :edit_file, :settings]
   before_action :load_branch_and_path, only: [:files, :blob, :new_file, :upload_file, :resolve, :edit_file]
   before_action :load_model_detail, only: [:show, :files, :blob, :new_file, :upload_file, :edit_file, :community, :settings, :commits, :commit]
 
