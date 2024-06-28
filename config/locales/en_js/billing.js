aff46d03d6514a3a4a59c00620c0ce65f1bca8c9
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-06-28 21:25:47 +0800

    流水 i18n

---
+++ config/locales/en_js/billing.js
@@ -0,0 +1,14 @@
+export const billing = {
+  title: 'Billing',
+  balance: 'Account Balance',
+  inference: 'Inference Instances',
+  instanceID: 'Instance ID',
+  createTime: 'Creation Time',
+  usageTime: 'Usage Duration(h)',
+  cost: 'Cost(yuan)',
+  status: 'Status',
+  details: 'Details',
+  total: 'Total',
+  refresh: 'Refresh',
+  instanceBilling:'Instance Billing'
+}
