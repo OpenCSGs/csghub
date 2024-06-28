4eb85df24b603b61419aedda276856a5088a6b5a
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-06-29 03:15:16 +0800

    billingdetails starttime endtime

--- app/javascript/packs/datetimeUtils.js
+++ app/javascript/packs/datetimeUtils.js
@@ -8,6 +8,14 @@ export const getCurrentDate = () => {
   return `${year}-${month}-${day}`;
 }
 
+export const getCurrentTime = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // 月份从0开始，所以需要+1，并使用padStart来确保总是两位数
+  const day = String(now.getDate()).padStart(2, '0'); // 使用padStart来确保总是两位数
+  return `${year}-${month}-${day}-23:59:59`;
+}
+
 export const getFirstDayOfMonth = () => {
   const now = new Date();
   const firstDay = new Date(now.getFullYear(), now.getMonth(), 1); // 设置日期为1来获取这个月的第一天
@@ -15,6 +23,15 @@ export const getFirstDayOfMonth = () => {
   const month = String(firstDay.getMonth() + 1).padStart(2, '0');
   const day = String(firstDay.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
+}
+
+export const getFirstDayOfTime = () => {
+  const now = new Date();
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1); // 设置日期为1来获取这个月的第一天
+  const year = firstDay.getFullYear();
+  const month = String(firstDay.getMonth() + 1).padStart(2, '0');
+  const day = String(firstDay.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}-00:00:00`;
 }
 
 export const formatDate = (date=(new Date), format='YYYY-MM-DD') => {
