531ed7800a53e4be2e719a8b64bcee2a570cff5c
Hiveer <ljp.hiveer@gmail.com>
2024-07-04 13:17:43 +0800

    Show configs as JSON object

--- app/javascript/components/new_admin/system_configs/AdminSystemConfig.vue
+++ app/javascript/components/new_admin/system_configs/AdminSystemConfig.vue
@@ -6,43 +6,15 @@
 
   <hr />
 
+  <!-- feature flags -->
   <div class="my-[30px]">
     <h3 class="text-[18px] font-[500] mb-[8px]">Feature Flags</h3>
-    <el-form-item label="on_premise">
-      <el-select
-        v-model="systemConfigs.feature_flags.on_premise"
-        size="small"
-        style="width: 340px"
-      >
-        <el-option
-          v-for="item in ['false', 'true']"
-          :key="item"
-          :label="item"
-          :value="item"
-        />
-      </el-select>
-    </el-form-item>
     <el-form-item>
-      <el-button type="info" size="small" @click="">Update</el-button>
-    </el-form-item>
-  </div>
-
-  <hr />
-
-  <div class="my-[30px]">
-    <h3 class="text-[18px] font-[500] mb-[8px]">General Configs</h3>
-    <el-form-item label="docs_url">
       <el-input
-        v-model="systemConfigs.general_configs.docs_url"
+        :value="stringifyObject(systemConfigs.feature_flags)"
+        type="textarea"
+        rows="7"
         style="width: 580px"
-        size="small"
-      ></el-input>
-    </el-form-item>
-    <el-form-item label="starcloud_url">
-      <el-input
-        v-model="systemConfigs.general_configs.starcloud_url"
-        style="width: 580px"
-        size="small"
       ></el-input>
     </el-form-item>
     <el-form-item>
@@ -52,8 +24,38 @@
 
   <hr />
 
+  <!-- general configs -->
   <div class="my-[30px]">
     <h3 class="text-[18px] font-[500] mb-[8px]">General Configs</h3>
+    <el-form-item>
+      <el-input
+        :value="stringifyObject(systemConfigs.general_configs)"
+        type="textarea"
+        rows="7"
+        style="width: 580px"
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="info" size="small" @click="">Update</el-button>
+    </el-form-item>
+  </div>
+
+  <hr />
+
+  <!-- S3 configs -->
+  <div class="my-[30px]">
+    <h3 class="text-[18px] font-[500] mb-[8px]">S3 Configs</h3>
+    <el-form-item>
+      <el-input
+        :value="stringifyObject(systemConfigs.s3_configs)"
+        type="textarea"
+        rows="7"
+        style="width: 580px"
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="info" size="small" @click="">Update</el-button>
+    </el-form-item>
   </div>
 </template>
 
@@ -70,11 +72,20 @@
     starhub_configs: {},
   })
 
+  const stringifyObject = (obj) => {
+    return JSON.stringify(obj).replace(/,/g, ',\n').replace('{', '{\n').replace('}', '\n}')
+  }
+
   const fetchSystemConfig = async () => {
     const res = await csrfFetch('/internal_api/admin/system_config')
     if (res.ok) {
       body = await res.json()
-      systemConfigs.value = body.system_configs
+      systemConfigs.value.feature_flags = body.system_configs.feature_flags || {}
+      systemConfigs.value.general_configs = body.system_configs.general_configs || {}
+      systemConfigs.value.license_configs = body.system_configs.license_configs || {}
+      systemConfigs.value.oidc_configs = body.system_configs.oidc_configs || {}
+      systemConfigs.value.s3_configs = body.system_configs.s3_configs || {}
+      systemConfigs.value.starhub_configs = body.system_configs.starhub_configs || {}
     } else {
       console.log('Failed to fetch system config')
     }
