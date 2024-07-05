31cfb61ab326720cc3a8d55a196e6f7fbe3d13ab
Hiveer <ljp.hiveer@gmail.com>
2024-07-05 09:29:23 +0800

    Add code comment

--- app/controllers/internal_api/admin/system_config_controller.rb
+++ app/controllers/internal_api/admin/system_config_controller.rb
@@ -6,6 +6,8 @@ class InternalApi::Admin::SystemConfigController < InternalApi::Admin::Applicati
 
   def update
     @system_config = SystemConfig.first
+    # 这里必须使用 permit!，因为传递过来的参数结构包含两个同样的 key，
+    # params.permit(:feature_flags) 还不能工作，待研究
     system_config_params = params.permit!.slice(:feature_flags,
                                                 :general_configs,
                                                 :s3_configs)
