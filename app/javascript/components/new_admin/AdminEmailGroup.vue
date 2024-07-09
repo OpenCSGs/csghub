ae64f03bc580a0a8033abfcebe5aa5584a33db03
Hiveer <ljp.hiveer@gmail.com>
2024-07-09 11:20:14 +0800

    Create and retrieve mail groups

---
+++ app/javascript/components/new_admin/AdminEmailGroup.vue
@@ -0,0 +1,115 @@
+<template>
+  <el-form
+    :model="dataForm"
+    label-position="top"
+    label-width="auto"
+    style="max-width: 600px"
+    >
+    <el-form-item label="Name">
+      <el-input v-model="dataForm.name" />
+    </el-form-item>
+    <el-form-item label="Upload Email List (Only CSV supported)">
+      <!-- <input type="file" @change="processCSVFile" /> -->
+      <el-upload
+        v-model:file-list="dataForm.emailList"
+        class="upload-demo w-[600px]"
+        :auto-upload="false"
+        drag
+        :limit="1"
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">
+          Drop file here or <em>click to upload</em>
+        </div>
+      </el-upload>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="createEmailGroup">Create</el-button>
+    </el-form-item>
+  </el-form>
+
+  <!-- email group list -->
+  <el-table
+    :stripe="true"
+    :data="emailGroups"
+    style="width: 100%"
+    class="mt-[40px]"
+  >
+    <el-table-column
+      prop="name"
+      label="Name"
+    />
+    <el-table-column
+      prop="email_list"
+      label="EmailList"
+    />
+    <el-table-column
+      prop="created_at"
+      label="Created At"
+    />
+  </el-table>
+</template>
+
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import Papa from 'papaparse'
+  import csrfFetch from "../../packs/csrfFetch"
+  import { ElMessage } from 'element-plus'
+
+  const dataForm = ref({
+    name: '',
+    emailList: []
+  })
+
+  const emailGroups = ref([])
+
+  const createEmailGroup = () => {
+    const file = dataForm.value.emailList[0].raw
+    Papa.parse(file, {
+      preview: 0,
+      complete: (results, file) => {
+        postEmailGroup(results.data.join(','))
+      }
+    })
+  }
+
+  const postEmailGroup = async(emailListStr) => {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: dataForm.value.name,
+        email_list: emailListStr
+      })
+    }
+    const response = await csrfFetch('/internal_api/admin/email_groups', options)
+    if (response.ok) {
+      ElMessage({
+        message: 'Email group created successfully!',
+        type: 'success'
+      })
+      refreshEmailGroups()
+    } else {
+      ElMessage({
+        message: 'Failed to create email group.',
+        type: 'error'
+      })
+    }
+  }
+
+  const refreshEmailGroups = async() => {
+    const response = await csrfFetch('/internal_api/admin/email_groups', { method: 'GET' })
+    if (response.ok) {
+      response.json().then(data => {
+        console.log(data)
+        emailGroups.value = data.email_groups
+      })
+    } else {
+      console.log('Failed to fetch email groups')
+    }
+  }
+
+  onMounted(() => {
+    refreshEmailGroups()
+  })
+</script>
