7f79e6a55181a7830b71641a2495b48b08ed0529
Hiveer <ljp.hiveer@gmail.com>
2024-07-09 11:19:44 +0800

    Endpoint for create and retrieve mail groups

--- app/controllers/internal_api/admin/email_groups_controller.rb
+++ app/controllers/internal_api/admin/email_groups_controller.rb
@@ -1,7 +1,21 @@
 class InternalApi::Admin::EmailGroupsController < InternalApi::ApplicationController
   def index
+    @email_groups = EmailGroup.all
+    render json: { email_groups: @email_groups}
   end
 
   def create
+    email_group = EmailGroup.new(email_group_params)
+    if email_group.save
+      render json: {msg: 'Create email group success'}
+    else
+      render json: {msg: 'Failed to create email group'}, status: 400
+    end
+  end
+
+  private
+
+  def email_group_params
+    params.permit(:name, :email_list)
   end
 end
