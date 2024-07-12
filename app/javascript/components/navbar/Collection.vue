fb911dc26ddcda94184877fb14fb5c778baa214a
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-07-12 12:21:57 +0800

    collection navbar

---
+++ app/javascript/components/navbar/Collection.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    href="/collections"
+    class="flex items-center hover:text-[#009933]"
+    :class="active ? 'text-[#009933]' : ''"
+  >
+    <SvgIcon
+      name="collection_icon"
+      width="18"
+      height="18"
+    />
+    <span class="ml-[8px]"> 合集 </span>
+  </a>
+</template>
+
+<script setup>
+  import { ref, inject, onBeforeMount } from 'vue'
+  const active = ref(false)
+
+  onBeforeMount(() => {
+    if (window.location.pathname.match(/\/datasets/)) {
+      active.value = true
+    }
+  })
+</script>
