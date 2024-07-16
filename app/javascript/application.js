8b75492538500eec2eb334b5815449e0d81d1593
yoiteyou <ran.niu@opencsg.com>
2024-07-16 15:03:49 +0800

    Csghub  new admin dataset (#293)
    * add new_admin dataset
    
    * Remove redundant code & add prompt information
    
    * Remove redundant code & add prompt information
    
    * fix per page
    
    * Bug fix
    
    * Bug fix
    
    ---------
    
    Co-authored-by: Hiveer <ljp.hiveer@gmail.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -91,6 +91,8 @@ import AdminCodeDetail from "./components/new_admin/codes/AdminCodeDetail.vue"
 import AdminModelList from "./components/new_admin/models/AdminModelList.vue"
 import AdminModelDetail from "./components/new_admin/models/AdminModelDetail.vue"
 import AdminEmailGroup from "./components/new_admin/AdminEmailGroup.vue"
+import AdminDatasetList from "./components/new_admin/dataset/AdminDatasetList.vue"
+import AdminDatasetDetail from "./components/new_admin/dataset/AdminDatasetDetail.vue"
 import AdminSystemConfig from "./components/new_admin/system_configs/AdminSystemConfig.vue"
 
 const pinia = createPinia()
@@ -193,6 +195,8 @@ const routes = [
   { path: '/new_admin/models', component: AdminModelList },
   { path: '/new_admin/models/:namespace/:name', component: AdminModelDetail },
   { path: '/new_admin/email_group', component: AdminEmailGroup },
+  { path: '/new_admin/datasets', component: AdminDatasetList },
+  { path: '/new_admin/datasets/:namespace/:name', component: AdminDatasetDetail },
   { path: '/new_admin/system_config', component: AdminSystemConfig },
 ]
 
