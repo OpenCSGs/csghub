12925db255c8f4642ca9298b6af59ffe9ef4ee87
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-07-16 15:27:19 +0800

    collection index header

--- app/javascript/components/collections/CollectionIndex.vue
+++ app/javascript/components/collections/CollectionIndex.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="flex gap-[24px] max-w-[1280px] m-auto min-h-[calc(100vh-153px)] md:min-h-0">
-    Collection Index
+  <div class="flex flex-col gap-[24px] w-full max-w-[1280px] m-auto min-h-[calc(100vh-153px)] md:min-h-0">
+    <!-- header -->
+    <div class="flex w-full justify-between mb-[36px]">
+      <div class="flex flex-col">
+        <div class="flex gap-2 text-[20px] leading-[30px]">
+          <SvgIcon
+            name="collection_icon"
+            width="18"
+            height="18"
+          />
+          <span> 合集 </span>
+        </div>
+        <div class="text-[16px] leading-[24px] text-[#667085]">探索社区中的精选热门合集</div>
+      </div>
+      <div>搜索条搜索条搜索条</div>
+    </div>
+    <div class="flex gap-1 max-w-[max-content] px-2 rounded-[6px] border border-[#D5D9EB] text-[14px] leading-[20px] bg-[#D5D9EB] text-[#363F72]">
+      <SvgIcon name="star" />
+      本周热门合集
+    </div>
   </div>
 </template>
 
