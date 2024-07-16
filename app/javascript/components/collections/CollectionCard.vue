0d74862e19a3fe0ad04bc4d8cb8bd5474087245f
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-07-16 19:00:00 +0800

    collections use mock data

---
+++ app/javascript/components/collections/CollectionCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    v-if="reposData.repository_type === 'space'"
+    class="w-full xl:flex-col xl:pl-4 md:pl-0 flex flex-wrap gap-4 mb-4 mt-[16px]"
+  >
+    <application-space-item
+      :repo="reposData"
+      :repo-type="reposData.repository_type"
+      :isCollection="true"
+    />
+  </div>
+  <div
+    v-else
+    class="w-full xl:flex-col xl:w-full flex flex-wrap justify-between gap-y-4 mb-4 mt-[16px]"
+  >
+    <repo-item
+      :repo="reposData"
+      :repo-type="reposData.repository_type"
+      :isCollection="true"
+    />
+  </div>
+</template>
+<script setup>
+  import { ref, computed } from 'vue'
+  import RepoItem from '../shared/RepoItem.vue'
+  import ApplicationSpaceItem from '../application_spaces/ApplicationSpaceItem.vue'
+  const props = defineProps({
+    reposData: Object
+  })
+  console.log('1231231', props.reposData)
+</script>
--- app/javascript/components/collections/CollectionIndex.vue
+++ app/javascript/components/collections/CollectionIndex.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="flex flex-col gap-[24px] w-full max-w-[1280px] m-auto min-h-[calc(100vh-153px)] md:min-h-0">
+  <div
+    class="flex flex-col gap-[24px] w-full max-w-[1280px] m-auto min-h-[calc(100vh-153px)] md:min-h-0"
+  >
     <!-- header -->
-    <div class="flex w-full justify-between mb-[36px]">
+    <div class="flex w-full justify-between mb-[12px]">
       <div class="flex flex-col">
         <div class="flex gap-2 text-[20px] leading-[30px]">
           <SvgIcon
@@ -9,17 +11,157 @@
             width="18"
             height="18"
           />
-          <span> 合集 </span>
+          <span> {{ $t('collections.collection') }} </span>
         </div>
-        <div class="text-[16px] leading-[24px] text-[#667085]">探索社区中的精选热门合集</div>
+        <div class="text-[16px] leading-[24px] text-[#667085]">{{ $t('collections.title') }}</div>
       </div>
-      <div>搜索条搜索条搜索条</div>
+      <!-- <div>搜索条搜索条搜索条</div> -->
     </div>
-    <div class="flex gap-1 max-w-[max-content] px-2 rounded-[6px] border border-[#D5D9EB] text-[14px] leading-[20px] bg-[#D5D9EB] text-[#363F72]">
+    <div
+      class="flex gap-1 max-w-[max-content] px-2 rounded-[6px] border border-[#D5D9EB] text-[14px] leading-[20px] bg-[#D5D9EB] text-[#363F72]"
+    >
       <SvgIcon name="star" />
-      本周热门合集
+      {{ $t('collections.hot') }}
     </div>
+    <!-- collections -->
+    <CollectionCards :collections="collectionData"></CollectionCards>
+
+    <CsgPagination
+      :perPage="perPage"
+      :currentPage="currentPage"
+      @currentChange="fetchCollections"
+      :total="totalCollections"
+    />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import { ref, onMounted, inject } from 'vue'
+  import CsgPagination from '../shared/CsgPagination.vue'
+  import CollectionCards from './CollectionCards.vue'
+  import jwtFetch from '../../packs/jwtFetch'
+  import { ElMessage } from 'element-plus'
+
+  const perPage = ref(10)
+  const currentPage = ref(1)
+  const totalCollections = ref(0)
+  const collectionData = ref([
+    {
+      description: 'codeLlama finetune by OpenCSG',
+      id: 0,
+      name: 'opencsg-codeLlama',
+      nickname: 'string',
+      private: true,
+      repositories: [
+        {
+          created_at: 'created_at',
+          description: 'description',
+          download_count: 0,
+          id: 0,
+          likes: 0,
+          name: 'name',
+          nickname: 'nickname',
+          path: 'xxxxx/asdvg',
+          private: true,
+          repository_type: 'space',
+          tags: [
+            {
+              commit: {
+                id: 'string'
+              },
+              message: 'string',
+              name: 'string'
+            }
+          ],
+          updated_at: 'updated_at',
+          user_id: 0
+        },
+        {
+          created_at: 'created_at',
+          description: 'description',
+          download_count: 0,
+          id: 1,
+          likes: 0,
+          name: 'name',
+          nickname: 'nickname',
+          path: 'xxxxx/asdvg',
+          private: true,
+          repository_type: 'model',
+          tags: [
+            {
+              commit: {
+                id: 'string'
+              },
+              message: 'string',
+              name: 'string'
+            }
+          ],
+          updated_at: 'updated_at',
+          user_id: 0
+        }
+      ],
+      theme: '#ECFDF3',
+      username: 'string'
+    },
+    {
+      description: 'codeLlama finetune by OpenCSG2',
+      id: 1,
+      name: 'opencsg-codeLlama2',
+      nickname: 'string',
+      private: true,
+      repositories: [
+        {
+          created_at: 'created_at',
+          description: 'description',
+          download_count: 0,
+          id: 0,
+          likes: 0,
+          name: 'name',
+          nickname: 'nickname',
+          path: 'xxxxx/asdvg',
+          private: true,
+          repository_type: 'model',
+          tags: [
+            {
+              commit: {
+                id: 'string'
+              },
+              message: 'string',
+              name: 'string'
+            }
+          ],
+          updated_at: 'updated_at',
+          user_id: 0
+        }
+      ],
+      theme: '#EAECF0',
+      username: 'string'
+    }
+  ])
+
+  const csghubServer = inject('csghubServer')
+
+  const fetchCollections = async (childCurrent) => {
+    if (childCurrent) {
+      currentPage.value = childCurrent
+    }
+    const params = new URLSearchParams()
+    params.append('per', perPage.value)
+    params.append('page', currentPage.value)
+    const url = `${csghubServer}/api/v1/collections?${params.toString()}`
+    const res = await jwtFetch(url)
+    if (!res.ok) {
+      const { msg } = await res.json()
+      ElMessage({ message: msg, type: 'warning' })
+    } else {
+      const { data } = await res.json()
+      billings.value = data.data
+      totalBillings.value = data.total
+      totalPrice.value = data.total_value
+    }
+  }
+  onMounted(() => {
+    // 等api ready 后解除注释
+    // fetchCollections()
+  })
+</script>
