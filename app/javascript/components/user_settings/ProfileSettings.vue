f6ed9a100c173bd3036ef3edf2423e2379106122
Hiveer <ljp.hiveer@gmail.com>
2024-07-17 21:08:29 +0800

    Format code

--- app/javascript/components/user_settings/ProfileSettings.vue
+++ app/javascript/components/user_settings/ProfileSettings.vue
@@ -1,57 +1,64 @@
 <template>
-  <div class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white">
-    <Menu class="max-w-[411px] md:mb-[24px]"
-          :name="profileName"
-          :email="email"
-          :displayName="profileDisplayName"
-          :avatar="profileAvatar">
+  <div
+    class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white"
+  >
+    <Menu
+      class="max-w-[411px] md:mb-[24px]"
+      :name="profileName"
+      :email="email"
+      :displayName="profileDisplayName"
+      :avatar="profileAvatar"
+    >
     </Menu>
-    <ProfileEdit class="grow"
-                 :name="name"
-                 :nickname="nickname"
-                 :avatar="avatar"
-                 :phone="phone"
-                 :email="email"
-                 :homepage="homepage"
-                 :bio="bio"
-                 :displayName="displayName"
-                 @updateUserInfo="updateUserInfo">
+    <ProfileEdit
+      class="grow"
+      :name="name"
+      :nickname="nickname"
+      :avatar="avatar"
+      :phone="phone"
+      :email="email"
+      :homepage="homepage"
+      :bio="bio"
+      :displayName="displayName"
+      @updateUserInfo="updateUserInfo"
+    >
     </ProfileEdit>
   </div>
 </template>
+
 <script>
-import Menu from "./Menu.vue";
-import ProfileEdit from "./ProfileEdit.vue";
-export default {
-  props: {
-    name: String,
-    nickname: String,
-    phone: String,
-    avatar: String,
-    email: String,
-    homepage: String,
-    bio: String,
-    displayName: String,
-  },
-  components: {
-    Menu,
-    ProfileEdit,
-  },
-  data() {
-    return {
-      profileName: this.name,
-      profileDisplayName: this.displayName,
-      profileAvatar: this.avatar,
-    };
-  },
-  mounted() {},
-  methods: {
-    updateUserInfo(data) {
-      const { nickname, name, avatar } = data;
-      this.profileName = name || this.name;
-      this.profileDisplayName = nickname || this.displayName;
-      this.profileAvatar = avatar || this.avatar;
+  import Menu from './Menu.vue'
+  import ProfileEdit from './ProfileEdit.vue'
+  export default {
+    props: {
+      name: String,
+      nickname: String,
+      phone: String,
+      avatar: String,
+      email: String,
+      homepage: String,
+      bio: String,
+      displayName: String
     },
-  },
-};
+    components: {
+      Menu,
+      ProfileEdit
+    },
+    data() {
+      return {
+        profileName: this.name,
+        profileDisplayName: this.displayName,
+        profileAvatar: this.avatar
+      }
+    },
+    mounted() {},
+    methods: {
+      updateUserInfo(data) {
+        const { nickname, name, avatar } = data
+        this.profileName = name || this.name
+        this.profileDisplayName = nickname || this.displayName
+        this.profileAvatar = avatar || this.avatar
+      }
+    }
+  }
 </script>
