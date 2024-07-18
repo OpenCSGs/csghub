8f1be8d719a0cb9aa28831d01da1f8d5ce168083
Hiveer <ljp.hiveer@gmail.com>
2024-07-18 11:20:35 +0800

    Code format update

--- app/javascript/components/organizations/OrganizationSettings.vue
+++ app/javascript/components/organizations/OrganizationSettings.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white min-h-[calc(100vh-129px)]">
-    <Menu class="max-w-[411px] md:mb-[24px]"
-          :nickName="organizationNickname"
-          :homepage="organizationHomepage"
-          :name="organization.name"
-          :logo="organizationAvatar">
+  <div
+    class="flex justify-center md:flex-col px-[24px] rounded-[8px] md:px-[50px] sm:px-[20px] max-w-[1280px] m-auto bg-white min-h-[calc(100vh-129px)]"
+  >
+    <Menu
+      class="max-w-[411px] md:mb-[24px]"
+      :nickName="organizationNickname"
+      :homepage="organizationHomepage"
+      :name="organization.name"
+      :logo="organizationAvatar"
+    >
     </Menu>
     <OrganizationEdit
       v-if="action === 'edit'"
       class="grow py-[24px]"
       :organization="organization"
-      @updateOrganization="updateOrganization" >
+      @updateOrganization="updateOrganization"
+    >
     </OrganizationEdit>
     <OrganizationMembers
       v-if="action === 'members'"
@@ -22,34 +27,34 @@
   </div>
 </template>
 <script>
-  import Menu from "./Menu.vue";
-  import OrganizationEdit from "./OrganizationEdit.vue";
-  import OrganizationMembers from "./OrganizationMembers.vue";
+  import Menu from './Menu.vue'
+  import OrganizationEdit from './OrganizationEdit.vue'
+  import OrganizationMembers from './OrganizationMembers.vue'
   export default {
     props: {
       organization: Object,
       action: String,
-      admin: Boolean,
+      admin: Boolean
     },
     components: {
       Menu,
       OrganizationEdit,
-      OrganizationMembers,
+      OrganizationMembers
     },
     data() {
       return {
         organizationNickname: this.organization.nickname,
         organizationHomepage: this.organization.homepage,
-        organizationAvatar: this.organization.avatar,
-      };
+        organizationAvatar: this.organization.avatar
+      }
     },
     methods: {
       updateOrganization(data) {
-        const { nickname, homepage, logo } = data;
-        this.organizationNickname = nickname || this.organizationNickname;
-        this.organizationHomepage = homepage || this.organizationHomepage;
-        this.organizationAvatar = logo || this.organizationAvatar;
-      },
+        const { nickname, homepage, logo } = data
+        this.organizationNickname = nickname || this.organizationNickname
+        this.organizationHomepage = homepage || this.organizationHomepage
+        this.organizationAvatar = logo || this.organizationAvatar
+      }
     }
   }
 </script>
