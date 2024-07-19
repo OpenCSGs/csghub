b793baaf2c92702defcda48d467b9728fe69e684
Hiveer <ljp.hiveer@gmail.com>
2024-07-19 22:42:04 +0800

    Fetch org data from JWT phase1

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -11,14 +11,15 @@ class OrganizationsController < ApplicationController
   end
 
   def show
-    @organization = Organization.find_by(name: params[:id])
-    unless @organization
-      flash[:alert] = "未找到对应的组织"
-      return redirect_to errors_not_found_path
-    end
-    user_org_role = current_user && current_user.org_role(@organization)
-    @admin = user_org_role == 'admin'
-    @members = @organization.members
+    @name = params[:id]
+    # @organization = Organization.find_by(name: params[:id])
+    # unless @organization
+    #   flash[:alert] = "未找到对应的组织"
+    #   return redirect_to errors_not_found_path
+    # end
+    # user_org_role = current_user && current_user.org_role(@organization)
+    # @admin = user_org_role == 'admin'
+    # @members = @organization.members
   end
 
   def members
