ecaf45db008f4493394bf91a29aca9af5a4ad923
Hiveer <ljp.hiveer@gmail.com>
2024-07-23 20:53:58 +0800

    Extract svg icons for navbar

--- app/javascript/components/navbar/TheNavbar.vue
+++ app/javascript/components/navbar/TheNavbar.vue
@@ -52,7 +52,18 @@
         @command="handleLocaleChange"
       >
         <span class="flex items-center text-base outline-none">
-          <SvgIcon name="i18n_language" />
+          <svg
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            width="1.2em"
+            height="1.2em"
+            data-v-12008bb2=""
+          >
+            <path
+              fill="currentColor"
+              d="m18.5 10l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3h-2.154L16.5 10h2zM10 2v2h6v2h-1.968a18.222 18.222 0 0 1-3.62 6.301a14.864 14.864 0 0 0 2.336 1.707l-.751 1.878A17.015 17.015 0 0 1 9 13.725a16.676 16.676 0 0 1-6.201 3.548l-.536-1.929a14.7 14.7 0 0 0 5.327-3.042A18.078 18.078 0 0 1 4.767 8h2.24A16.032 16.032 0 0 0 9 10.877a16.165 16.165 0 0 0 2.91-4.876L2 6V4h6V2h2zm7.5 10.885L16.253 16h2.492L17.5 12.885z"
+            ></path>
+          </svg>
           <el-icon class="el-icon--right">
             <arrow-down />
           </el-icon>
@@ -79,35 +90,7 @@
             :src="userAvatar"
           >
           </el-avatar>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            class="absolute bottom-0 right-0"
-          >
-            <rect
-              x="0.5"
-              y="0.5"
-              width="14"
-              height="14"
-              rx="7"
-              fill="white"
-            />
-            <path
-              d="M7.5 1C3.88208 1 1 3.88208 1 7.5C1 11.1179 3.88208 14 7.5 14C11.1179 14 14 11.1179 14 7.5C14 3.88208 11.1179 1 7.5 1ZM11.3019 11.3632H3.57547V10.5047H4.92453V7.00943H5.78302V10.5047H7.31604V5.66038H8.17453V7.62264H10.6274V8.48113H8.17453V10.5047H11.3019V11.3632ZM11.3632 7.00943C9.89151 6.21226 8.60377 5.23113 7.5 3.9434C6.51887 5.10849 5.23113 6.15094 3.63679 7.00943L3.14623 6.15094C4.74057 5.35377 6.08962 4.37264 7.1934 3.14623H7.86792C9.03302 4.43396 10.3821 5.41509 11.8538 6.15094L11.3632 7.00943Z"
-              fill="#67C23A"
-            />
-            <rect
-              x="0.5"
-              y="0.5"
-              width="14"
-              height="14"
-              rx="7"
-              stroke="white"
-            />
-          </svg>
+          <SvgIcon name="verified_company" />
         </span>
         <span
           v-else-if="JSON.parse(isCompanyUser.toLowerCase())"
@@ -117,37 +100,8 @@
             :size="35"
             :src="userAvatar"
           >
+          <SvgIcon name="company" />
           </el-avatar>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-            class="absolute bottom-0 right-0"
-          >
-            <rect
-              x="0.5"
-              y="0.5"
-              width="14"
-              height="14"
-              rx="7"
-              fill="black"
-              fill-opacity="0.8"
-            />
-            <path
-              d="M7.5 1C3.88208 1 1 3.88208 1 7.5C1 11.1179 3.88208 14 7.5 14C11.1179 14 14 11.1179 14 7.5C14 3.88208 11.1179 1 7.5 1ZM11.3019 11.3632H3.57547V10.5047H4.92453V7.00943H5.78302V10.5047H7.31604V5.66038H8.17453V7.62264H10.6274V8.48113H8.17453V10.5047H11.3019V11.3632ZM11.3632 7.00943C9.89151 6.21226 8.60377 5.23113 7.5 3.9434C6.51887 5.10849 5.23113 6.15094 3.63679 7.00943L3.14623 6.15094C4.74057 5.35377 6.08962 4.37264 7.1934 3.14623H7.86792C9.03302 4.43396 10.3821 5.41509 11.8538 6.15094L11.3632 7.00943Z"
-              fill="white"
-            />
-            <rect
-              x="0.5"
-              y="0.5"
-              width="14"
-              height="14"
-              rx="7"
-              stroke="#DCDFE6"
-            />
-          </svg>
         </span>
         <span
           v-else
