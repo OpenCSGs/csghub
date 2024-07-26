9345dcfc3042911396bed3efbf7d91d8f8efa754
Hiveer <ljp.hiveer@gmail.com>
2024-07-26 16:49:02 +0800

    User profile settings refactor (#356)
    * Fetch user data from Store
    
    * Update profile edit to get data from user store
    
    * Remove user data fetch logic from profile settings

--- app/javascript/components/user_settings/ProfileEdit.vue
+++ app/javascript/components/user_settings/ProfileEdit.vue
@@ -1,58 +1,82 @@
 <template>
   <div class="flex flex-col gap-[24px] px-6 py-10 border-l">
-    <div class="font-semibold text-[20px] leading-[28px]">{{ $t('profile.edit.title') }}</div>
+    <div class="font-semibold text-[20px] leading-[28px]">
+      {{ $t('profile.edit.title') }}
+    </div>
     <!-- name -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.userName') }}</div>
-      <el-input class="max-w-[600px]"
-                v-model="profileData.profileName"
-                disabled
-                :placeholder="this.$t('all.userName')">
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.userName') }}
+      </div>
+      <el-input
+        class="max-w-[600px]"
+        v-model="profileData.username"
+        disabled
+        :placeholder="$t('all.userName')">
       </el-input>
-      <p class="text-gray-500 text-[12px] italic pt-1">{{ $t('rule.nameRule') }}</p>
+      <p class="text-gray-500 text-[12px] italic pt-1">
+        {{ $t('rule.nameRule') }}
+      </p>
     </div>
     <!-- avatar -->
     <div>
       <div class="flex items-center gap-[4px] mb-[8px]">用户头像</div>
-      <el-avatar :size="120" :src="profileData.profileAvatar"> </el-avatar>
+      <el-avatar
+        :size="120"
+        :src="profileData.avatar">
+      </el-avatar>
       <div class="flex gap-[12px] fileInput">
-        <input ref="fileInput"
-              type="file"
-              class="hidden"
-              @change="uploadAvatar"/>
-        <div @click="uploadImage" class="text-[14px] border border-[#DCDFE6] px-[20px] py-[9px] leading-[22px] text-center rounded-[8px] text-white cursor-pointer bg-[#409EFF]">
+        <input
+          ref="fileInput"
+          type="file"
+          class="hidden"
+          @change="uploadAvatar" />
+        <div
+          @click="uploadImage"
+          class="text-[14px] border border-[#DCDFE6] px-[20px] py-[9px] leading-[22px] text-center rounded-[8px] text-white cursor-pointer bg-[#409EFF]">
           {{ $t('profile.edit.uploadAvatar') }}
         </div>
-        <div @click="removeImage" class="text-[14px] border border-[#DCDFE6] px-[20px] py-[9px] leading-[22px] text-center rounded-[8px] text-[#606266] cursor-pointer bg-white">
+        <div
+          @click="removeImage"
+          class="text-[14px] border border-[#DCDFE6] px-[20px] py-[9px] leading-[22px] text-center rounded-[8px] text-[#606266] cursor-pointer bg-white">
           {{ $t('profile.edit.removeAvatar') }}
         </div>
       </div>
     </div>
     <!-- nickname -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.nickName') }}</div>
-      <el-input class="max-w-[600px]"
-                v-model="profileData.profileNickname"
-                :placeholder="$t('all.nickName')">
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.nickName') }}
+      </div>
+      <el-input
+        class="max-w-[600px]"
+        v-model="profileData.nickname"
+        :placeholder="$t('all.nickName')">
       </el-input>
     </div>
     <!-- homepage -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.homepage') }}</div>
-      <el-input class="max-w-[600px]"
-                v-model="profileData.profileHomepage"
-                :placeholder="this.$t('all.homepage')">
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.homepage') }}
+      </div>
+      <el-input
+        class="max-w-[600px]"
+        v-model="profileData.homepage"
+        :placeholder="$t('all.homepage')">
       </el-input>
     </div>
     <!-- bio -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.bio') }}</div>
-      <el-input class="max-w-[600px]"
-                v-model="profileData.profileBio"
-                clearable
-                type="textarea"
-                :autosize="{minRows: 8, maxRows: 30}"
-                :placeholder="this.$t('all.bio')">
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.bio') }}
+      </div>
+      <el-input
+        class="max-w-[600px]"
+        v-model="profileData.bio"
+        clearable
+        type="textarea"
+        :autosize="{ minRows: 8, maxRows: 30 }"
+        :placeholder="$t('all.bio')">
       </el-input>
     </div>
     <!-- phone -->
@@ -62,7 +86,7 @@
       </div>
       <div class="flex flex-col gap-[8px]">
         <el-input class="max-w-[600px]"
-                  v-model="profileData.profilePhone"
+                  v-model="profileData.phone"
                   ref="phoneInputElement"
                   :disabled="!updatePhoneEnabled"
                   :placeholder="$t('all.phone')">
@@ -98,167 +122,183 @@
     </div>
     <!-- email -->
     <div>
-      <div class="flex items-center gap-[4px] mb-[8px]">{{ $t('all.email') }}<svg xmlns="http://www.w3.org/2000/svg" width="6" height="6" viewBox="0 0 6 6" fill="none"><path d="M2.21714 5.21809L3.35474 3.85009L4.49234 5.21809L5.12594 4.75729L4.20434 3.25969L5.77394 2.61169L5.52914 1.87729L3.88754 2.26609L3.74354 0.538086H2.96594L2.82194 2.28049L1.18034 1.87729L0.921143 2.61169L2.49074 3.25969L1.58354 4.75729L2.21714 5.21809Z" fill="#F56C6C"/></svg>
+      <div class="flex items-center gap-[4px] mb-[8px]">
+        {{ $t('all.email')
+        }}<svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="6"
+          height="6"
+          viewBox="0 0 6 6"
+          fill="none">
+          <path
+            d="M2.21714 5.21809L3.35474 3.85009L4.49234 5.21809L5.12594 4.75729L4.20434 3.25969L5.77394 2.61169L5.52914 1.87729L3.88754 2.26609L3.74354 0.538086H2.96594L2.82194 2.28049L1.18034 1.87729L0.921143 2.61169L2.49074 3.25969L1.58354 4.75729L2.21714 5.21809Z"
+            fill="#F56C6C" />
+        </svg>
       </div>
-      <el-input class="max-w-[600px]"
-                v-model="profileData.profileEmail"
-                :placeholder="this.$t('all.email')">
+      <el-input
+        class="max-w-[600px]"
+        v-model="profileData.email"
+        :placeholder="$t('all.email')">
       </el-input>
     </div>
-    <div @click="saveProfile" class="w-[111px] text-[14px] border border-[#DCDFE6] px-[16px] py-[5px] leading-[22px] text-center rounded-[8px] text-white cursor-pointer bg-[#409EFF]">
+    <div
+      @click="saveProfile"
+      class="w-[111px] text-[14px] border border-[#DCDFE6] px-[16px] py-[5px] leading-[22px] text-center rounded-[8px] text-white cursor-pointer bg-[#409EFF]">
       {{ $t('all.save') }}
     </div>
   </div>
 </template>
-<script>
-import csrfFetch from "../../packs/csrfFetch.js"
-import jwtFetch from "../../packs/jwtFetch.js"
-import { ElMessage } from "element-plus";
-import { inject } from "vue"
 
-export default {
-  props: {},
-  data() {
-    return {
-      profileData: inject('profileData'),
-      csghubServer: inject('csghubServer'),
-      inputSmsCode: '',
-      generatedSmscode: '',
-      updatePhoneEnabled: false,
-      inCountDown: false,
-      countdown: 30,
-      intervalId: null,
-      phoneJustSendcode: this.phone
-    };
-  },
-  mounted() {},
-  methods: {
-    async sendSmsCode() {
-      const profileSmsEndpoint = `/internal_api/users/sms`;
-      this.generatedSmscode = Math.floor(100000 + Math.random() * 900000).toString()
-      this.phoneJustSendcode = this.profileData.profilePhone
-      const options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          phone: this.profileData.profilePhone,
-          code: this.generatedSmscode
-        })
-      }
-      const response = await csrfFetch(profileSmsEndpoint, options)
-      if (!response.ok) {
-        response.json().then(data => {
-          ElMessage({
-            message: data.Message,
-            type: "warning",
-          })
-        })
-      } else {
-        this.inCountDown = true
-        this.intervalId = setInterval(() => {
-          if (this.countdown > 0) {
-            this.countdown--
-          } else {
-            clearInterval(this.intervalId)
-            this.inCountDown = false
-          }
-        }, 1000)
+<script setup>
+  import csrfFetch from '../../packs/csrfFetch.js'
+  import jwtFetch from '../../packs/jwtFetch.js'
+  import { ElMessage } from 'element-plus'
+  import { ref, inject } from 'vue'
+  import useUserStore from '../../stores/UserStore.js'
+  import { storeToRefs } from 'pinia'
+  import { useI18n } from 'vue-i18n'
 
+  const { t } = useI18n()
+  const userStore = useUserStore()
+  const csghubServer = inject('csghubServer')
+  const profileData = ref(storeToRefs(userStore))
+
+  const fileInput = ref(null)
+  const phoneInputElement = ref(null)
+  const inputSmsCode = ref('')
+  const generatedSmscode = ref('')
+  const updatePhoneEnabled = ref(false)
+  const inCountDown = ref(false)
+  const countdown = ref(30)
+  const intervalId = ref(null)
+  const phoneJustSendcode = ref(profileData.value.phone)
+
+  const uploadImage = () => {
+    fileInput.value.click()
+  }
+
+  const removeImage = () => {
+    fileInput.value = null
+    profileData.value.avatar = ''
+  }
+
+  const sendSmsCode = async () => {
+    const profileSmsEndpoint = `/internal_api/users/sms`;
+    generatedSmscode.value = Math.floor(100000 + Math.random() * 900000).toString()
+    phoneJustSendcode.value = profileData.value.phone
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        phone: profileData.value.phone,
+        code: generatedSmscode.value
+      })
+    }
+    const response = await csrfFetch(profileSmsEndpoint, options)
+    if (!response.ok) {
+      response.json().then(data => {
         ElMessage({
-          message: this.$t('profile.edit.smsSented'),
-          type: "success",
+          message: data.Message,
+          type: "warning",
         })
-      }
-    },
-    enableUpdatePhone() {
-      this.updatePhoneEnabled = true
-      this.$refs.phoneInputElement.disabled = false
-    },
-    disableUpdatePhone() {
-      this.updatePhoneEnabled = false
-      this.inputSmsCode = ''
-      this.generatedSmscode = ''
-      this.$refs.phoneInputElement.disabled = true
-    },
-    uploadImage() {
-      this.$refs.fileInput.click();
-    },
-    removeImage() {
-      this.$refs.fileInput.value = null
-      this.profileData.profileAvatar = ""
-    },
-    async uploadAvatar() {
-      const uploadEndpoint = `/internal_api/upload`;
-      const formData = new FormData()
-      formData.append("file", this.$refs.fileInput.files[0])
-      const options = {
-        method: 'POST',
-        body: formData
-      }
-      const response = await csrfFetch(uploadEndpoint, options)
-      const result = await response.json()
-      if (!response.ok) {
-        ElMessage({ message: result.message, type: 'warning' })
-      } else {
-        this.profileData.profileAvatar = result.url
-      }
-    },
-    async updateProfile() {
-      const profileUpdateEndpoint = `${this.csghubServer}/api/v1/user/${this.profileData.profileName}`
-      const params = {
-        avatar: this.profileData.profileAvatar,
-        username: this.profileData.profileName,
-        name: this.profileData.profileNickname,
-        email: this.profileData.profileEmail.trim(),
-        phone: this.profileData.profilePhone,
-        homepage: this.profileData.profileHomepage,
-        bio: this.profileData.profileBio
-      }
-      const options = {
-        method: "PUT",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(params),
-      }
-      try {
-        const response = await jwtFetch(profileUpdateEndpoint, options)
-        if (!response.ok) {
-          response.json().then(data => {
-            ElMessage({
-              message: data.message,
-              type: "warning",
-            })
-          })
+      })
+    } else {
+      inCountDown.value = true
+      intervalId.value = setInterval(() => {
+        if (countdown.value > 0) {
+          countdown.value--
         } else {
+          clearInterval(intervalId.value)
+          inCountDown.value = false
+        }
+      }, 1000)
+
+      ElMessage({
+        message: t('profile.edit.smsSented'),
+        type: "success",
+      })
+    }
+  }
+
+  const enableUpdatePhone = () => {
+    updatePhoneEnabled.value = true
+    phoneInputElement.value.disabled = false
+  }
+
+  const disableUpdatePhone = () => {
+    updatePhoneEnabled.value = false
+    inputSmsCode.value = ''
+    generatedSmscode.value = ''
+    phoneInputElement.value.disabled = true
+  }
+
+  const uploadAvatar = async () => {
+    const uploadEndpoint = `/internal_api/upload`
+    const formData = new FormData()
+    formData.append('file', fileInput.value.files[0])
+    const options = {
+      method: 'POST',
+      body: formData
+    }
+    const response = await csrfFetch(uploadEndpoint, options)
+    const result = await response.json()
+    if (!response.ok) {
+      ElMessage({ message: result.message, type: 'warning' })
+    } else {
+      profileData.value.avatar = result.url
+    }
+  }
+
+  const updateProfile = async () => {
+    const profileUpdateEndpoint = `${csghubServer}/api/v1/user/${profileData.value.username}`
+    const params = {
+      avatar: profileData.value.avatar,
+      username: profileData.value.username,
+      name: profileData.value.nickname,
+      email: profileData.value.email.trim(),
+      phone: profileData.value.phone,
+      homepage: profileData.value.homepage,
+      bio: profileData.value.bio
+    }
+    const options = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(params)
+    }
+    try {
+      const response = await jwtFetch(profileUpdateEndpoint, options)
+      if (!response.ok) {
+        response.json().then((error) => {
           ElMessage({
-            message: this.$t('profile.edit.updateSuccess'),
-            type: "success",
-          });
-          this.disableUpdatePhone()
-          this.$emit("updateUserInfo", {
-            avatar: this.avatarUrl,
-            name: this.inputName,
-            nickname: this.inputNickname
-          });
+            message: error.msg,
+            type: 'warning'
+          })
+        })
+      } else {
+        ElMessage({
+          message: t('profile.edit.updateSuccess'),
+          type: 'success'
+        })
+          disableUpdatePhone()
           // 处理成功响应
           setTimeout(() => {
             window.location.reload()
           }, 1000)
-        }
-      } catch (error) {
-        console.error(error);
       }
-    },
-    saveProfile() {
-      if (this.profileData.profileName.trim().length === 0 || this.profileData.profileEmail.trim().length === 0) {
-        ElMessage({
-          message: "请提供用户名和邮箱",
-          type: "warning",
-        });
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-        return
-      }
-      this.updateProfile();
-    },
-  },
-};
+  const saveProfile = () => {
+    if (profileData.value.username.trim().length === 0 || profileData.value.email.trim().length === 0) {
+      ElMessage({
+        message: "请提供用户名和邮箱",
+        type: "warning",
+      });
+      return
+    }
+    updateProfile()
+  }
 </script>
+
