9c0f97401395601443ac05c019454361743f586a
Zhendi.Wang <zhendi.wang@gmail.com>
2024-07-30 17:59:37 +0800

    Show endpoint playground if status is running (#370)
    * show endpoint playground if status is running
    
    * fix resolve helper

--- app/javascript/components/shared/RepoTabs.vue
+++ app/javascript/components/shared/RepoTabs.vue
@@ -54,6 +54,7 @@
         <EndpointPage
           v-else-if="repoType === 'endpoint'"
           :appEndpoint="appEndpoint"
+          :appStatus="appStatus"
           :modelId="modelId"
           :private="private"
           :endpointReplica="endpointReplica"
