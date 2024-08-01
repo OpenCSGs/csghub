efa31c1fefc75bee7d2c334f01a17e6fd87fd566
Zhendi.Wang <zhendi.wang@gmail.com>
2024-08-01 11:21:42 +0800

    fix last commit init issue in file list (#391)

--- app/javascript/components/shared/FileList.vue
+++ app/javascript/components/shared/FileList.vue
@@ -156,7 +156,7 @@
 
   const breadcrumb = ref([])
   const files = ref([])
-  const lastCommit = ref({})
+  const lastCommit = ref()
   const lastCommitAvatar = ref('https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png')
   const prefixPath = document.location.pathname.split('/')[1]
 
