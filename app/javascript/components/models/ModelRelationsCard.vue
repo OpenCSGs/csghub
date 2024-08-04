038d4b79f23ca0212399ea69b74d27b1bf3cb9a9
ymh6315431 <86564687+ymh6315431@users.noreply.github.com>
2024-08-04 16:23:57 +0800

    Update space code model i18n icon (#424)
    Co-authored-by: minghao.yang <mh.yang@opencsg.com>

--- app/javascript/components/models/ModelRelationsCard.vue
+++ app/javascript/components/models/ModelRelationsCard.vue
@@ -2,7 +2,7 @@
   <div class="p-[16px] border-t border-[#EBEEF5]">
     <h3 class="flex items-center gap-[8px]">
       <SvgIcon name="models" width="18px" height="18px" />
-      <span class="font-[500] text-[#344054] text-[16px] leading-[24px]"> 关联模型 </span>
+      <span class="font-[500] text-[#344054] text-[16px] leading-[24px]"> {{ $t('models.modelsUsedToTrain') }} </span>
       <span class="text-[#344054]"> {{ namespacePath }} </span>
     </h3>
     <div class="mt-[16px] flex flex-col gap-[12px]">
