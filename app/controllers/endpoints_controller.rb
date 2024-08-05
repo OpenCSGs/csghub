492da4791781f252a6c0d03bbe9b96b84e6bb53b
zhendi <zhendi.wang@gmail.com>
2024-08-05 20:44:58 +0800

    fix endpoint path, remove validation

--- app/controllers/endpoints_controller.rb
+++ app/controllers/endpoints_controller.rb
@@ -1,11 +1,8 @@
 class EndpointsController < ApplicationController
   layout 'repo_application'
 
-  include LocalRepoValidation
-
   before_action :authenticate_user, except: :show
   before_action :check_user_info_integrity
-  before_action :load_endpoint_detail, except: :new
 
   def new
   end
@@ -27,11 +24,5 @@ class EndpointsController < ApplicationController
   def settings
     @default_tab = 'settings'
     render :show
-  end
-
-  private
-
-  def load_endpoint_detail
-    @settings_visibility = (current_user && @local_endpoint) ? current_user.can_manage?(@local_endpoint) : false
   end
 end
