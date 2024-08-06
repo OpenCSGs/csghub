b9ed9b55bcc91691a70fba241655781d440e3e30
Hiveer <ljp.hiveer@gmail.com>
2024-08-06 12:33:39 +0800

    Optimize JWT refresh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -68,7 +68,7 @@ class SessionsController < ApplicationController
     res = csghub_api.verify_jwt_token(params[:jwt])
     user_infos = JSON.parse(res)["data"]
     cookies['user_token'] = params[:jwt]
-    cookies['token_expire_at'] = Time.at(params[:expire].to_i).in_time_zone("Beijing").strftime("%Y-%m-%d %H:%M:%S")
+    cookies['token_expire_at'] = params[:expire]
     cookies['can_change_username'] = user_infos['can_change_username']
     login_by_server_user_infos user_infos
   end
