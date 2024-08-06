755e07a7a0fed56980ee60ed93b97d5448ffdb9a
Hiveer <ljp.hiveer@gmail.com>
2024-08-06 14:00:33 +0800

    Delete more

--- app/controllers/internal_api/admin/system_config_controller.rb
+++ app/controllers/internal_api/admin/system_config_controller.rb
@@ -23,7 +23,6 @@ class InternalApi::Admin::SystemConfigController < InternalApi::Admin::Applicati
   def system_config_params
     params.require(:system_config).permit(feature_flags: {},
                                           s3_configs: {},
-                                          oidc_configs: {},
                                           starhub_configs: {})
     params.require(:system_config).permit!.to_h
   end
--- app/dashboards/system_config_dashboard.rb
+++ app/dashboards/system_config_dashboard.rb
@@ -10,11 +10,9 @@ class SystemConfigDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     application_env: Field::String,
-    oidc_configs: Field::JSONB,
     starhub_configs: Field::JSONB,
     license_configs: Field::JSONB,
     feature_flags: Field::JSONB,
-    general_configs: Field::JSONB,
     s3_configs: Field::JSONB,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
@@ -36,8 +34,6 @@ class SystemConfigDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = %i[
     id
     application_env
-    general_configs
-    oidc_configs
     starhub_configs
     license_configs
     feature_flags
@@ -51,8 +47,6 @@ class SystemConfigDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     application_env
-    general_configs
-    oidc_configs
     starhub_configs
     license_configs
     feature_flags
