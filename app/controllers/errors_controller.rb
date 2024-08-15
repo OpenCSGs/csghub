e9cafa456a217d7690ed35544a0f541dfca4e76d
Hiveer <ljp.hiveer@gmail.com>
2024-08-15 13:50:09 +0800

    Add new error page for login failed

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -8,4 +8,9 @@ class ErrorsController < ApplicationController
   def unauthorized
     render 'unauthorized', status: 401
   end
+
+  def login_failed
+    @error_msg = params[:error_msg] || '未知登录错误，请联系管理员'
+    render 'login_failed'
+  end
 end
