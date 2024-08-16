49574530888d5b9a06597f0fd6cfd0d3d7bfbc0d
Hiveer <ljp.hiveer@gmail.com>
2024-08-16 22:31:51 +0800

    Revert model provenance functions for open source

--- config/locales/zh_js/all.js
+++ config/locales/zh_js/all.js
@@ -81,7 +81,6 @@ export const all = {
   fetchError: "获取数据失败",
   deploy: "部署",
   more:"更多",
-  baseModel:"基座模型",
   moreDesc: "展开更多",
   sdkTips: "请先注册，然后从 OpenCSG 的账号页面获取 Token",
   public: "公开",
