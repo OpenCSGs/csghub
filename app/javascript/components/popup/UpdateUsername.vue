cb393d80fa176f3829a43dcd338aed15a71a5b07
朱鑫睿 <xinrui.zhu@opencsg.com>
2024-08-19 17:16:35 +0800

    Create UodateUsername.vue

---
+++ app/javascript/components/popup/UpdateUsername.vue
@@ -0,0 +1,144 @@
+<template>
+  <el-dialog 
+    v-model="getChangeUsername" 
+    :title="$t('profile.edit.updateUsername')" 
+    width="30%" 
+    class="dialogWidth"
+    style="border-radius: 0.5rem" 
+    left
+  >
+    <el-form :model="formData" :rules="formRules" ref="formRef">
+      <div>
+        <div class="flex">
+          <SvgIcon name="exclamation_point" width="20px" height="20px" />
+          <p class="text-gray-500 text-[16px] ml-[8px]">
+            {{ $t('profile.edit.confirmUpdateMessage')}}
+          </p>
+        </div>
+        <p class="text-[#303133] text-[14px] mb-[8px] mt-[8px]">
+          {{ $t('profile.edit.username') }}
+          <span class="text-red-400">*</span>
+        </p>
+        <el-form-item prop="username">
+          <el-input v-model="formData.username" :rows="6" placeholder="请输入用户名" />        
+        </el-form-item>
+        <p class="text-gray-500 text-[12px] italic mt-[24px]">
+          {{ $t('rule.nameRule') }}
+        </p>
+      </div>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="canChangeUsername = 'false'">{{ $t('all.cancel') }}</el-button>
+        <el-button type="primary" @click="submiteUsername">
+          {{ $t('all.update') }}
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+  import { ref, inject, computed } from 'vue'
+  import { useI18n } from 'vue-i18n'
+  import { useCookies } from 'vue3-cookies'
+  import jwtFetch from '../../packs/jwtFetch.js'
+  import { ElMessage } from 'element-plus'
+
+  const { cookies } = useCookies()
+  const { t } = useI18n()
+  const csghubServer = inject('csghubServer')
+  const nameRule = inject('nameRule')
+  const formData = ref({ username: '' })
+  const formRef = ref(null)
+
+  const getChangeUsername = computed(() => {
+    const canChangeUsername = cookies.get('can_change_username')
+    return canChangeUsername == 'true'
+  })
+
+  const formRules = ref({
+      username: [
+    {
+      required: true,
+      message: t('all.pleaseInput', {
+        value: t('models.newModel.modelName')
+      }),
+      trigger: 'blur'
+    },
+    // limit 2-64 length
+    {
+      min: 2,
+      max: 64,
+      message: t('rule.lengthLimit', { min: 2, max: 64 }),
+      trigger: 'blur'
+    },
+    // 以字母开头
+    {
+      pattern: /^[a-zA-Z]/,
+      message: t('rule.startWithLetter'),
+      trigger: 'blur'
+    },
+    // 以数字或字母结尾
+    {
+      pattern: /[a-zA-Z0-9]$/,
+      message: t('rule.endWithLetterOrNumber'),
+      trigger: 'blur'
+    },
+    // 只能包含字母、数字与-_.
+    {
+      pattern: /^[a-zA-Z0-9-_\.]+$/,
+      message: t('rule.onlyLetterNumberAndSpecialStr'),
+      trigger: 'blur'
+    },
+    // 特殊字符不能并列出现
+    {
+      pattern: /^(?!.*[-_.]{2,}).*$/,
+      message: t('rule.specialStrNotTogether'),
+      trigger: 'blur'
+    },
+    // 保险起见最后一步还是加上最终的正则吧
+    { pattern: nameRule, message: t('rule.nameRule'), trigger: 'blur' }
+    ]
+  })
+
+  const submiteUsername = () => {
+    formRef.value.validate((valid) => {
+      if (valid) {
+        // 异步发送请求
+        updateUsername().catch((err) => {
+          ElMessage({
+            message: err.message,
+            type: 'warning'
+          })
+        })
+      }
+    })
+  }
+
+  const updateUsername = async () => {
+    const currentUsername = cookies.get('current_user')
+    const profileUpdateEndpoint = `${csghubServer}/api/v1/user/${currentUsername}`
+
+    const params = { new_username: formData.value.username }
+
+    const options = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(params)
+    }
+    try {
+      const response = await jwtFetch(profileUpdateEndpoint, options)
+      if (!response.ok) {
+        response.json().then((data) => {
+          ElMessage.warning(data.msg)
+        })
+      } else {
+        ElMessage.success(t('profile.edit.updateSuccess'))
+        window.location.href = '/logout'
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+</script>
