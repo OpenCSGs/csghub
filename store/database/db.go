cb38d9e9377a1fe9febda7bcacaeb3412dbbc448
Hiveer <ljp.hiveer@gmail.com>
2024-08-27 16:22:20 +0800

    Bug fix on package import order

--- store/database/db.go
+++ store/database/db.go
@@ -8,13 +8,12 @@ import (
 	"strings"
 	"time"
 
+	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/dialect/sqlitedialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 	"github.com/uptrace/bun/driver/sqliteshim"
 	"github.com/uptrace/bun/extra/bundebug"
-
-	"github.com/uptrace/bun"
 )
 
 type DatabaseDialect string
