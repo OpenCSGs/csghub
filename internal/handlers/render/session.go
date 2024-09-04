7ddf81a1aaca7ed79aad3403caffacbc0d628d0e
Hiveer <ljp.hiveer@gmail.com>
2024-09-04 16:56:32 +0800

    Set cookie for session create

--- internal/handlers/render/session.go
+++ internal/handlers/render/session.go
@@ -32,4 +32,9 @@ func (i *SessionHandlerImpl) Logout(ctx *gin.Context) {
 }
 
 func (i *SessionHandlerImpl) Create(ctx *gin.Context) {
+	// 调用 jwt token 验证接口获取 userinfos
+	ctx.SetCookie("user_token", ctx.Param("jwt"), 3600*24*7, "/", "", false, false)
+	ctx.SetCookie("can_change_username", ctx.Param("can_change_username"), 3600*24*7, "/", "", false, false)
+	// login_by_server_user_infos 通过 userinfo 创建用户
+	// log_in 设置 user session，其他 cookies
 }
