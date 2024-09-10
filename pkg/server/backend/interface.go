db80aeed575e308bb4cfe58aab20e4dc39655942
zhendi <zhendi.wang@gmail.com>
2024-09-10 18:13:57 +0800

    add resolve routes

--- pkg/server/backend/interface.go
+++ pkg/server/backend/interface.go
@@ -1,7 +1,6 @@
 package backend
 
 import (
-	"io"
 	"net/http"
 
 	"opencsg.com/portal/pkg/server/types"
@@ -11,6 +10,6 @@ type Server interface {
 	VerifyJWTToken(token string) (*types.User, *http.Response, error)
 	ImageSecureCheck(req types.ImageSensitiveCheckReq) (*types.ImageSensitiveCheckResp, *http.Response, error)
 	CreateJWTToken(req types.CreateJWTReq) (*types.CreateJWTResp, *http.Response, error)
-	DownloadFileRaw(req types.DownlaodReq) (*types.DownloadFileRawResp, *http.Response, error)
-	DownloadFile(req types.DownlaodReq) (io.ReadCloser, *http.Response, error)
+	DownloadFileRaw(req types.DownloadReq) (*types.DownloadFileRawResp, *http.Response, error)
+	DownloadFile(req types.DownloadReq) ([]byte, *http.Response, error)
 }
