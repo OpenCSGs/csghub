4748738839ee554dd9de5ce9d02c511ecfd791ec
XiaoXiAI <123750281+zzxr6@users.noreply.github.com>
2024-09-11 12:07:48 +0800

    Add dialog when redirect profile (#590)
    Co-authored-by: 朱鑫睿 <xinrui.zhu@opencsg.com>

--- app/javascript/components/user_settings/Menu.vue
+++ app/javascript/components/user_settings/Menu.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="pt-6">
       <div class="w-[294px] rounded-[8px] mx-[24px]">
-        <div class="flex p-[16px]">
+        <div @click="clickProfile" class="flex p-[16px] cursor-pointer">
           <el-avatar :size="60" :src="userStore.avatar"> </el-avatar>
           <div class="ml-[10px]">
             <div class="text-[18px] leading-[28px] font-semibold">
-              <a :href="'/profile/' + userStore.username">
-                {{ userStore.nickname || userStore.username }}
-              </a>
+              {{ userStore.nickname || userStore.username }}
             </div>
             <div class="text-[16px] text-[#909399] leading-[24px] font-light">@{{ userStore.username }}</div>
           </div>
@@ -77,17 +75,44 @@
         </div>
       </div>
     </div>
+    <el-dialog
+      v-model="showDialog"
+      width="350"
+      :style="{ borderRadius: '10px' }"
+    >
+      <template #header> 
+        {{ $t('profile.menu.warningTip') }}
+      </template>
+      <div class="flex justify-center flex-col m-auto w-full relative">
+        {{ $t('profile.menu.warningTipDesc') }}
+      </div>
+      <template #footer>
+        <div class="dialog-footer flex justify-between">
+          <el-button class="flex-1 mr-3 text-gray-700" size="large" @click="showDialog = false">{{ $t('organization.members.cancel') }}</el-button>
+          <el-button class="flex-1" size="large" color="#3250BD" type="primary" :loading="loading" @click="handleConfirm">
+            {{ $t('organization.members.confirm') }}
+          </el-button>
+        </div>
+      </template>
+    </el-dialog>
 </template>
 
 <script setup>
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
   import useUserStore from '../../stores/UserStore'
 
-  const userStore = useUserStore()
+  const props = defineProps({
+    hasSave: {
+      type: Boolean,
+      default: true
+    }
+  })
 
+  const userStore = useUserStore()
   const hasEmail = computed(() => {
     return !!userStore.email
   })
+  const showDialog = ref(false)
 
   const menuClass = (menuPath) => {
     if (menuPath === window.location.pathname) {
@@ -95,5 +120,17 @@
     } else {
       return ''
     }
+  }
+
+  const clickProfile = () => {
+    if (props.hasSave) {
+      window.location.href = '/profile/' + userStore.username
+    } else {
+      showDialog.value = true
+    }
+  }
+
+  const handleConfirm = () => {
+    window.location.href = '/profile/' + userStore.username
   }
 </script>
