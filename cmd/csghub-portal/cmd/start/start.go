7eccb0d1296b4d6c0f64afd33041dcb97ebad8c9
Hiveer <ljp.hiveer@gmail.com>
2024-09-13 16:52:16 +0800

    Skip migration check

--- cmd/csghub-portal/cmd/start/start.go
+++ cmd/csghub-portal/cmd/start/start.go
@@ -1,14 +1,7 @@
 package start
 
 import (
-	"context"
-	"fmt"
-	"time"
-
 	"github.com/spf13/cobra"
-	"opencsg.com/portal/config"
-	"opencsg.com/portal/pkg/database"
-	"opencsg.com/portal/pkg/database/migrations"
 )
 
 func init() {
@@ -19,29 +12,30 @@ var Cmd = &cobra.Command{
 	Use:   "start",
 	Short: "start a server",
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		ctx, cancel := context.WithTimeout(cmd.Context(), 10*time.Second)
-		defer cancel()
+		// Do not check unapplied migrations for now
+		// ctx, cancel := context.WithTimeout(cmd.Context(), 10*time.Second)
+		// defer cancel()
 
-		config, err := config.LoadConfig()
-		if err != nil {
-			return err
-		}
+		// config, err := config.LoadConfig()
+		// if err != nil {
+		// 	return err
+		// }
 
-		db, err := database.NewDB(config)
-		if err != nil {
-			return fmt.Errorf("initializing DB: %w", err)
-		}
-		migrator := migrations.NewMigrator(db)
+		// db, err := database.NewDB(config)
+		// if err != nil {
+		// 	return fmt.Errorf("initializing DB: %w", err)
+		// }
 
-		status, err := migrator.MigrationsWithStatus(ctx)
-		if err != nil {
-			return fmt.Errorf("checking migrations status: %w", err)
-		}
+		// migrator := migrations.NewMigrator(db)
+		// status, err := migrator.MigrationsWithStatus(ctx)
+		// if err != nil {
+		// 	return fmt.Errorf("checking migrations status: %w", err)
+		// }
+		// unapplied := status.Unapplied()
+		// if len(unapplied) > 0 {
+		// 	return fmt.Errorf("there are %d migrations to apply: %s", len(unapplied), unapplied)
+		// }
 
-		unapplied := status.Unapplied()
-		if len(unapplied) > 0 {
-			return fmt.Errorf("there are %d migrations to apply: %s", len(unapplied), unapplied)
-		}
 		return nil
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
