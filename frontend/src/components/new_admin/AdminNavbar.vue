7cd9ee7b0818a5535648972cb1ec4eef44352ede
Hiveer <ljp.hiveer@gmail.com>
2024-09-26 20:05:17 +0800

    Fix multisync button (#688)
    * Bug fix: check user roles in repo clone
    
    * Fix typo

--- frontend/src/components/new_admin/AdminNavbar.vue
+++ frontend/src/components/new_admin/AdminNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-[60px] bg-[#1C7A6C]">
     <div class="max-w-[1280px] m-auto h-full flex justify-between items-center">
-      <p class="font-[700] text-[#D1DDD0]"> OpenCSG Admin Pannel </p>
+      <p class="font-[700] text-[#D1DDD0]"> OpenCSG Admin Panel </p>
       <a href="/" target="_blank" class="font-[500] text-[#D1DDD0] underline"> Open Site </a>
       <p class="text-[#D1DDD0]">{{ userName }} ( {{ userRoles }} )</p>
     </div>
