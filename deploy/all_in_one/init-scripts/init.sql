3c6872fef40df9c57444c3e85c23aa0654658805
Sean <38311363@qq.com>
2024-09-26 12:48:25 +0800

    Add init data of runtime arch

--- deploy/all_in_one/init-scripts/init.sql
+++ deploy/all_in_one/init-scripts/init.sql
@@ -57,9 +57,108 @@ ON CONFLICT (name)
 
 INSERT INTO runtime_frameworks (id, frame_name, frame_version, frame_image, frame_cpu_image, enabled, container_port, type) VALUES ('1', 'VLLM', '2.7', 'vllm-local:2.7', 'vllm-cpu:2.3', 1, 8000, 1);
 INSERT INTO runtime_frameworks (id, frame_name, frame_version, frame_image, frame_cpu_image, enabled, container_port, type) VALUES ('3', 'TGI', '2.1', 'tgi:2.1', '', 1, 8000, 1);
-INSERT INTO runtime_frameworks (id, frame_name, frame_version, frame_image, frame_cpu_image, enabled, container_port, type) VALUES ('4', 'FastChat', '1.2', ' ', '', 1, 8000, 1);
-INSERT INTO runtime_frameworks (id, frame_name, frame_version, frame_image, frame_cpu_image, enabled, container_port, type) VALUES ('6', 'MindIE', '1.0', ' ', '', 1, 8000, 1);
 INSERT INTO runtime_frameworks (id, frame_name, frame_version, frame_image, frame_cpu_image, enabled, container_port, type) VALUES ('2', 'LLaMA-Factory', '1.11', 'llama-factory:1.18-cuda12.1-devel-ubuntu22.04-py310-torch2.1.2', '', 1, 8000, 2);
+
+--
+-- Init data runtime frameworks architecture
+--
+
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'AquilaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'ArcticForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'BaiChuanForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'BloomForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'ChatGLMModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'CohereForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'DbrxForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'DeciLMForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'FalconForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'GemmaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Gemma2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'GPT2LMHeadModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'GPTBigCodeForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'GPTJForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'GPTNeoXForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'InternLMForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'InternLM2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'JAISLMHeadModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'JambaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'LlamaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'MiniCPMForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'MistralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'MixtralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'MPTForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'OLMoForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'OPTForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'OrionForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'PhiForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Phi3ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Phi3SmallForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'PersimmonForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'QWenLMHeadModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Qwen2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Qwen2MoeForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'StableLmForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Starcoder2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'XverseForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'ChameleonForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'FuyuForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'LlavaForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'LlavaNextForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'PaliGemmaForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(1, 'Phi3VForCausalLM');
+
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'BaiChuanForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'BloomForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'ChatGLMModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'CohereForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'DeepseekV2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'FalconForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'Gemma2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'GemmaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'InternLM2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'InternLM2ForRewardModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'LlamaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'LlavaForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'LlavaNextForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'MistralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'MixtralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'OlmoForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'PaliGemmaForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'PhiForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'Phi3ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'Qwen2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'QWenLMHeadModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'Qwen2MoeForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'Starcoder2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'XverseForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'LlavaLlamaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(2, 'YuanForCausalLM');
+
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'DeepseekV2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'Idefics2ForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'LlavaNextForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'LlamaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'Phi3ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'GemmaForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'PaliGemmaForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'CohereForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'DbrxForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'MistralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'MixtralForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'GPTBigCodeForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'PhiForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'BaichuanForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'FalconForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'Starcoder2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'Qwen2ForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'OPTForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'T5ForConditionalGeneration');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'GPT2LMHeadCustomModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'BloomForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'MPTForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'GPT2LMHeadModel');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'GPTNeoXForCausalLM');
+INSERT INTO runtime_architectures (runtime_framework_id, architecture_name) VALUES(3, 'IdeficsForVisionText2Text');
 
 --
 -- Name: runtime_frameworks_id_seq; Type: SEQUENCE SET;  Owner: postgres
