e5d2bc1870765434a20c8f69421a0cdfd912a8b0
Hiveer <ljp.hiveer@gmail.com>
2024-11-18 13:39:45 +0800

    Unit test for render admin handler

---
+++ internal/handlers/render/admin_test.go
@@ -0,0 +1,64 @@
+package renderHandlers
+
+import (
+	"testing"
+
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	mockjwt "opencsg.com/portal/_mocks/opencsg.com/portal/pkg/utils/jwt"
+	"opencsg.com/portal/internal/models"
+)
+
+func TestAdminHandlerImpl_Index(t *testing.T) {
+	// Define a type for the arguments
+	type args struct {
+		ctx *gin.Context
+	}
+
+	mockJwtUtils := mockjwt.NewMockJwtUtils(t)
+	mockJwtUtils.EXPECT().GetCurrentUser(mock.Anything).Return(&models.User{})
+
+	// Define test cases
+	tests := []struct {
+		name    string
+		i       *AdminHandlerImpl
+		args    args
+		setup   func() // Setup function to initialize mocks and context
+		wantErr bool   // Whether we expect an error
+	}{
+		{
+			name: "Valid currentUser and roles",
+			i: &AdminHandlerImpl{
+				jwtUtils: mockJwtUtils,
+			},
+			args: args{
+				ctx: func() *gin.Context {
+					w := httptest.NewRecorder()
+					ctx, _ := gin.CreateTestContext(w)
+					return ctx
+				}(),
+			},
+			setup:   func() {},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Run the setup function to initialize mocks and context
+			if tt.setup != nil {
+				tt.setup()
+			}
+
+			// Call the method under test
+			tt.i.Index(tt.args.ctx)
+
+			// Check if the correct status code is returned
+			assert.Equal(t, http.StatusOK, tt.args.ctx.Writer.Status())
+		})
+	}
+}
