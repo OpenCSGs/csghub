bf56820f5f5854af073665df94572b67f314d7cb
youngbeom-shin <youngbeomshin@163.com>
2024-11-21 17:23:31 +0800

    style(ui): update form styles and color scheme

--- frontend/src/components/application_spaces/NewApplicationSpace.vue
+++ frontend/src/components/application_spaces/NewApplicationSpace.vue
@@ -7,13 +7,13 @@
         height="36"
       />
     </div>
-    <h3 class="text-gray-700 text-xl font-semibold mt-6 mb-3">
+    <h3 class="text-gray-700 text-xl font-medium mt-6 mb-3">
       {{ t('application_spaces.new.title') }}
     </h3>
-    <p class="text-gray-500 text-base font-medium md:text-center">
+    <p class="text-gray-500 text-md font-regular md:text-center">
       {{ t('application_spaces.new.subTitle1') }}
     </p>
-    <p class="text-gray-500 text-base font-medium md:text-center">
+    <p class="text-gray-500 text-md font-regular md:text-center">
       {{ $t('application_spaces.new.subTitle2') }}
     </p>
     <div class="mt-9">
@@ -312,19 +312,19 @@
           />
         </el-form-item>
         <p
-          class="mb-[18px] rounded-sm bg-brand-25 text-brand-500 text-[13px] py-[9px] px-4"
+          class="mb-[18px] rounded-md bg-brand-25 text-brand-500 text-[13px] py-[9px] px-4"
         >
           {{ t('application_spaces.new.notes') }}
         </p>
         <div class="flex justify-end">
           <el-form-item>
-            <el-button
+            <button
               :loading="loading"
-              class="!text-center !h-9 !text-md !text-white !bg-brand-600 !rounded-md !border-[1px] !border-brand-600"
+              class="btn btn-primary btn-md"
               @click="handleSubmit"
             >
               {{ t('application_spaces.new.create') }}
-            </el-button>
+            </button>
           </el-form-item>
         </div>
       </el-form>
