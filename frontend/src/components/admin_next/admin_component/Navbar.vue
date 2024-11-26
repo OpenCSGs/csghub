c9ce3a69a2d4e77240ee68130b0d63f6a7c1d922
Hiveer <ljp.hiveer@gmail.com>
2024-11-26 09:46:27 +0800

    Responsive width for admin page

--- frontend/src/components/admin_next/admin_component/Navbar.vue
+++ frontend/src/components/admin_next/admin_component/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-[80px] flex-shrink-0 border-b border-gray-200">
-    <div class="max-w-[1280px] m-auto h-full flex justify-between items-center">
+    <div class="2xl:w-[1280px] xl:w-[1024px] lg:w-[826px] md:w-[640px] sm:w-auto m-auto h-full flex justify-between items-center">
       <p class="font-[700] text-white"> OpenCSG Admin Panel </p>
       <a href="/" target="_blank" class="font-[500] text-white underline"> Open Site </a>
       <p class="text-white">{{ userName }} ( {{ userRoles }} )</p>
