24f2bc4471a24e1afee6e7c90401744cef940c94
申永范 <youngbeomshin@163.com>
2024-11-28 14:04:42 +0800

    refactor(tags): Standardize tag components styling and remove redundant SVG (#819)

--- frontend/src/components/tags/frameworks/TensorFlow.vue
+++ frontend/src/components/tags/frameworks/TensorFlow.vue
@@ -1,37 +1,9 @@
 <template>
-  <span class="flex gap-[4px] items-center text-sm border px-[8px] py-[3px] rounded-sm cursor-pointer"
+  <span class="flex gap-1 items-center text-sm font-normal text-gray-700 bg-white border border-gray-300 px-2 py-1 rounded-sm cursor-pointer hover:bg-gray-50"
       :style="setTagColor()"
       @click="toggleActive"
   >
-    <svg class="grayscale" :style="setSvgColor()" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="#ffffff">
-      <g clip-path="url(#clip0_3946_96047)">
-        <mask id="mask0_3946_96047" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="6" y="0" width="6" height="12">
-          <path d="M11.2246 3.02222L6.22461 0.17778V11.8667L8.22461 10.7111V7.42222L9.73572 8.28889L9.7135 6.04445L8.22461 5.17778V3.86667L11.2246 5.62222V3.02222Z" fill="white"/>
-        </mask>
-        <g mask="url(#mask0_3946_96047)">
-          <path d="M0.313477 0.133331H12.5801V11.8889H0.313477V0.133331Z" fill="url(#paint0_linear_3946_96047)"/>
-        </g>
-        <mask id="mask1_3946_96047" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="6" height="12">
-          <path d="M0.780029 3.02222L5.78003 0.17778V11.8667L3.78003 10.7111V3.86667L0.780029 5.62222V3.02222Z" fill="white"/>
-        </mask>
-        <g mask="url(#mask1_3946_96047)">
-          <path d="M0.246582 0.133331H12.5132V11.8889H0.246582V0.133331Z" fill="url(#paint1_linear_3946_96047)"/>
-        </g>
-      </g>
-      <defs>
-        <linearGradient id="paint0_linear_3946_96047" x1="0.313477" y1="6.01111" x2="12.5801" y2="6.01111" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#FF6F00"/>
-          <stop offset="1" stop-color="#FFA800"/>
-        </linearGradient>
-        <linearGradient id="paint1_linear_3946_96047" x1="0.246582" y1="6.01111" x2="12.5132" y2="6.01111" gradientUnits="userSpaceOnUse">
-          <stop stop-color="#FF6F00"/>
-          <stop offset="1" stop-color="#FFA800"/>
-        </linearGradient>
-        <clipPath id="clip0_3946_96047">
-          <rect width="12" height="12" fill="white"/>
-        </clipPath>
-      </defs>
-    </svg>
+    <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="g" x1="0.25" y1="6" x2="12.58" y2="6"><stop stop-color="#FF6F00"/><stop offset="1" stop-color="#FFA800"/></linearGradient></defs><g clip-path="url(#c)"><path mask="url(#m1)" d="M11.11 5.43L8.28 3.77L8.11 3.67v1.57l0.06 0.03l1.43 0.84l0.02 1.99L8.28 7.33L8.11 7.23v3.42L6.34 11.67V0.37L11.11 3.09v2.34z" fill="url(#g)"/><path mask="url(#m2)" d="M3.72 3.77L0.89 5.43V3.09L5.67 0.37v11.31L3.89 10.65V3.67L3.72 3.77z" fill="url(#g)"/></g></svg>
     TensorFlow
   </span>
 </template>
