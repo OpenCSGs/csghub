c7166f20fb61d24de9416511f145bd02e3c23eb5
Hiveer <ljp.hiveer@gmail.com>
2024-12-03 19:48:53 +0800

    Remove cookie admin_user and get user roles from userStore (#833)

--- internal/handlers/render/session.go
+++ internal/handlers/render/session.go
@@ -135,7 +135,6 @@ func (i *SessionHandlerImpl) Create(ctx *gin.Context) {
 
 	ctx.SetCookie("login_identity", user.LoginIdentity, cookieMaxAge, "/", "", false, false)
 	ctx.SetCookie("current_user", userResp.Username, cookieMaxAge, "/", "", false, false)
-	ctx.SetCookie("admin_user", fmt.Sprintf("%t", user.IsAdmin()), cookieMaxAge, "/", "", false, false)
 
 	user.SessionIP = ctx.ClientIP()
 	err = i.userModel.Update(ctx, user)
