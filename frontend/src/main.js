2b004d73bc3b0d252d2cb066a6a6855151f31806
Hiveer <ljp.hiveer@gmail.com>
2024-12-13 10:14:47 +0800

    Remove sync token and starship key

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -27,8 +27,6 @@ import ProfileEdit from "./components/user_settings/ProfileEdit.vue"
 import ProfileSettings from "./components/user_settings/ProfileSettings.vue"
 import NewOrganization from "./components/organizations/NewOrganization.vue"
 import AccessTokenSettings from "./components/user_settings/AccessTokenSettings.vue"
-import StarshipAccessTokenSettings from "./components/user_settings/StarshipAccessTokenSettings.vue"
-import SyncAccessTokenSettings from "./components/user_settings/SyncAccessTokenSettings.vue"
 import SshKeySettings from "./components/user_settings/SshKeySettings.vue"
 import NewModel from "./components/models/NewModel.vue"
 import TagSidebar from "./components/tags/TagSidebar.vue"
@@ -100,8 +98,6 @@ const app = createApp({
     NewEndpoint,
     NewFinetune,
     finetuneDetail,
-    StarshipAccessTokenSettings,
-    SyncAccessTokenSettings,
     ApplicationSpaceDetail,
     EndpointDetail,
     CollectionIndex,
