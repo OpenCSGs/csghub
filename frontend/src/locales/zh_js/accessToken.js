7209a0ffadba8ccabbf9b7490669f4b5f0737cac
申永范 <youngbeomshin@163.com>
2024-12-16 18:13:43 +0800

    docs(i18n): update-access-token-description (#854)
    - Revised the description of access tokens to better explain their purpose and the importance of keeping them confidential.
    - Ensured consistency in messaging across both English and Chinese locale files.
    
    These changes aim to enhance user understanding of access tokens and promote best security practices.

--- frontend/src/locales/zh_js/accessToken.js
+++ frontend/src/locales/zh_js/accessToken.js
@@ -1,6 +1,6 @@
 export const accessToken = {
   title: "用户 Access Token",
-  desc: "Access Token 用于授权您执行 Git 相关操作",
+  desc: "Access Tokens 主要用于验证您的身份，允许应用程序基于Token权限执行操作。请不要与任何人分享您的访问令牌。",
   refresh: "刷新 Access Token",
   refreshWarning: "刷新之后现有的 Access Token 将会失效，确认继续吗?",
   confirm: "继续刷新",
