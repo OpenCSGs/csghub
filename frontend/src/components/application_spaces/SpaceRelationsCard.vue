a021f5d322dbacbf9243906623805d980a2c5223
申永范 <youngbeomshin@163.com>
2025-01-15 14:55:58 +0800

    style(ui): standardize relation cards styling and spacing (#903)
    * style(ui): standardize relation cards styling and spacing
    
    * style(ui): update text size in relation cards for improved readability

--- frontend/src/components/application_spaces/SpaceRelationsCard.vue
+++ frontend/src/components/application_spaces/SpaceRelationsCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="p-[16px] border-t border-gray-200">
-    <h3 class="flex items-center gap-[8px]">
+    <h3 class="flex items-center gap-2">
       <SvgIcon name="spaces" width="18px" height="18px" />
-      <span class="font-medium text-gray-700 text-md leading-[24px]"> {{ $t('spaces.associated') }} </span>
+      <span class="font-medium text-gray-700 text-md leading-6"> {{ $t('spaces.associated') }} </span>
       <span class="text-gray-500"> {{ spaces.length }} </span>
     </h3>
-    <div class="mt-[16px] flex gap-[12px]">
+    <div class="mt-4 flex flex-wrap gap-3">
       <a v-for="space in spaces" :href="`/spaces/${space.path}`" data-test="space-item">
-        <p class="rounded-md border border-gray-300 bg-white py-0.5 pr-2.5 pl-1.5">
+        <p class="rounded-sm text-sm text-gray-700 border border-gray-300 bg-white py-1 px-2 hover:bg-gray-50">
           {{ space.path }}
         </p>
       </a>
