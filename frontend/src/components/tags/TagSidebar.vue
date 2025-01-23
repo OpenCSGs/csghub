c9657f200fced8aa0c487fbd73ae25cf37ffdfc2
Hiveer <ljp.hiveer@gmail.com>
2025-01-23 17:23:09 +0800

    Filter tag by enabled attr (#909)

--- frontend/src/components/tags/TagSidebar.vue
+++ frontend/src/components/tags/TagSidebar.vue
@@ -57,7 +57,7 @@
   const activeTags = ref({})
 
   const avaliableCategories = computed(() => {
-    return tagCategories.value.filter((c) => c.scope === props.repoType)
+    return tagCategories.value.filter((c) => c.scope === props.repoType && c.enabled)
   })
 
   watch(avaliableCategories,
