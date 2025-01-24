49f7c18d1d9f79d0c7869ff1e49f1d602d7bf51b
niuran <ran.niu@opencsg.com>
2025-01-24 17:59:33 +0800

    add error notice

--- frontend/src/components/endpoints/playground/TestWidget.vue
+++ frontend/src/components/endpoints/playground/TestWidget.vue
@@ -53,7 +53,7 @@
         <img
           v-if="imageSrc"
           :src="imageSrc"
-          style="max-width: 90% !important;"
+          style="max-width: 90% !important"
           alt="Fetched Image" />
       </div>
 
@@ -72,6 +72,7 @@
 <script setup>
   import { ref, computed } from 'vue'
   import { useCookies } from 'vue3-cookies'
+  import { ElMessage } from 'element-plus'
 
   const { cookies } = useCookies()
 
@@ -140,7 +141,7 @@
   })
 
   const handleSendMessage = async () => {
-    if(loading.value) return
+    if (loading.value) return
     loading.value = true
     const data = {
       inputs: message.value,
@@ -168,6 +169,10 @@
       loading.value = false
     } catch (error) {
       console.error('Error fetching image:', error)
+      ElMessage({
+        message: error.error || 'Failed to fetch image',
+        type: 'warning'
+      })
       loading.value = false
     }
   }
