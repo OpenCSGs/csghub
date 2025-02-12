e2d0992af91a448229824bcd010ef7fc7a30fc91
youngbeom-shin <youngbeomshin@163.com>
2025-02-12 17:15:00 +0800

    refactor(header): rename avatar prop to userAvatar

--- frontend/src/components/shared/RepoHeader.vue
+++ frontend/src/components/shared/RepoHeader.vue
@@ -48,7 +48,7 @@
     >
       <el-avatar
         :size="24"
-        :src="avatar"
+        :src="userAvatar"
         class="flex-shrink-0"
       ></el-avatar>
       <span class="max-w-full break-words text-gray-700 font-medium">{{
