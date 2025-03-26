bb640362922af9c816c07b0313f415ee6d31ce46
ErrLei <2235907780@qq.com>
2025-03-26 20:09:34 +0800

    fix(ui):Tips for optimizing your username (#1033)
    * Draft MR
    
    * fix(ui):Tips for optimizing your username
    
    ---------
    
    Co-authored-by: jiuling <jl.lei@opencsg.com>

--- frontend/src/locales/zh_js/rule.js
+++ frontend/src/locales/zh_js/rule.js
@@ -1,5 +1,7 @@
 export const rule = {
-  nameRule: "2-64个字母数字_.-的字符串，_.-不能并列出现，只能字母开头，字母或者数字结尾",
+  notChangable: "设置后不可更改",
+  // nameRule: "2-64个字母数字_.-的字符串，_.-不能并列出现，只能字母开头，字母或者数字结尾",
+  nameRule: "请输入2~10位的字母、数字组合",
   collectionNameRule: "2-70个字母数字_.-的字符串，_.-不能并列出现，只能字母开头，字母或者数字结尾",
   lengthLimit: "长度在{min}到{max}字符范围",
   startWithLetter: "必须以字母开头",
