2f981b787d44cb7e75ff35642186c2d75aabeb60
申永范-UX <yf.shen@opencsg.com>
2025-06-05 07:44:59 +0000

    feat(pagination): improve button styles and layout

--- frontend/src/components/shared/CsgPagination.vue
+++ frontend/src/components/shared/CsgPagination.vue
@@ -10,7 +10,7 @@
       :page-size="perPage"
       layout="prev, pager, next"
       @update:current-page="currentChange"
-      class="my-[52px] flex justify-center"
+      class="my-12 flex justify-between"
     />
 </template>
 
@@ -38,30 +38,76 @@ watch(() => props.currentPage, (newPage) => {
 });
 </script>
 <style scoped>
+:deep(.btn-prev.is-first) {
+  border-radius: 0 !important;
+  border: none !important;
+  color: #182230 !important;
+  font-weight: 400 !important;
+  margin-right: 8px !important;
+  height: 40px !important;
+}
+:deep(.btn-prev.is-first *) {
+  font-weight: 400 !important;
+}
+:deep(.btn-prev.is-first:hover) {
+  background-color: #F9FAFB !important;
+  border-radius: 8px !important;
+}
+:deep(.btn-next.is-last) {
+  border-radius: 0 !important;
+  border: none !important;
+  color: #182230 !important;
+  font-weight: 400 !important;
+  margin-left: 8px !important;
+  height: 40px !important;
+}
+:deep(.btn-next.is-last *) {
+  font-weight: 400 !important;
+}
+:deep(.btn-next.is-last:hover) {
+  background-color: #F9FAFB !important;
+  border-radius: 8px !important;
+}
 :deep(.btn-prev) {
-  border-radius: var(--border-radius-sm) 0 0 6px;
+  border-radius: 8px;
   margin: 0 !important;
+  color: #182230 !important;
   background: #ffffff !important;
   border: 1px solid #d0d5dd;
+  font-weight: 400 !important;
+  height: 40px !important;
+}
+:deep(.btn-prev *) {
+  font-weight: 400 !important;
 }
 :deep(.btn-next) {
-  border-radius: 0 6px 6px 0;
+  border-radius: 8px;
   margin: 0 !important;
+  color: #182230 !important;
   background: #ffffff !important;
   border: 1px solid #d0d5dd;
+  font-weight: 400 !important;
+  height: 40px !important;
+}
+:deep(.btn-next *) {
+  font-weight: 400 !important;
 }
 :deep(.el-pager li) {
   margin: 0 !important;
   background-color: #ffffff !important;
-  border: 1px solid #d0d5dd;
+  font-weight: 400 !important;
+  border-radius: 0 !important;
   border-right: none;
   color: #182230 !important;
+  height: 40px !important;
 }
 :deep(.el-pager li:first-child) {
   border-left: none;
 }
 :deep(.el-pager li.is-active) {
   background-color: #F9FAFB !important;
+  font-weight: 400 !important;
   color: #182230 !important;
+  border-radius: 8px !important;
 }
 </style>
