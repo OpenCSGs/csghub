fe710994b6259ed94f61c6d4e4ca89c37b7fbecf
申永范 <youngbeomshin@163.com>
2025-07-04 14:52:39 +0800

    feat(application_spaces): enhance SVG overlay design (#1215)
    * feat(application_spaces): enhance SVG overlay design
    
    * feat(application_spaces): enhance SVG overlay design
    
    ---------
    
    Co-authored-by: 申永范-UX <yf.shen@opencsg.com>

--- frontend/src/components/application_spaces/ApplicationSpaceItem.vue
+++ frontend/src/components/application_spaces/ApplicationSpaceItem.vue
@@ -5,19 +5,23 @@
     :style="`width: ${itemWidth};`"
   >
     <!-- Cover with Overlay -->
-    <div class="relative bg-gray-900 h-[150px]">
+    <div class="relative bg-black h-[150px]">
       
-      <!-- Dark gradient overlay -->
-      <div class="absolute inset-0 bg-gradient-to-t from-gray-900/80 to-gray-900/30"></div>
-      
-      <!-- Decorative overlay -->
-      <svg class="absolute left-0 top-0 w-full h-full" viewBox="0 0 412 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g>
-          <path d="M411 80.5337L411 142C411 146.418 407.418 150 403 150L8.99998 150C4.5817 150 0.999987 146.418 0.999988 142L0.999998 80.5337C83.1626 15.6052 246.431 196.126 411 80.5337Z"
-            :class="['stroke-none blur-3xl', stableGradientColor]"
-          />
-        </g>
-      </svg>
+    <!-- Decorative overlay -->
+    <svg class="opacity-80 absolute -inset-4 w-[calc(100%+2rem)] h-[calc(100%+2rem)]" viewBox="0 0 412 150" fill="none" xmlns="http://www.w3.org/2000/svg" style="overflow: visible;">
+      <defs>
+        <filter id="blur-filter" x="-100%" y="-100%" width="300%" height="300%">
+          <feGaussianBlur stdDeviation="64" />
+        </filter>
+      </defs>
+      <g>
+        <path 
+          d="M411 60L411 142C411 146.418 407.418 150 403 150L8.99998 150C4.5817 150 0.999987 146.418 0.999988 142L0.999988 60C83.1626 -20 246.431 180 411 60Z"
+          :class="[stableGradientColor]"
+          style="filter: url(#blur-filter);"
+        />
+      </g>
+    </svg>
       
       <!-- Content overlay on image -->
       <div class="absolute inset-0 flex flex-col justify-between p-4">
