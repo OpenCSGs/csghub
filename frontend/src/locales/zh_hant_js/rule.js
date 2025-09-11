fa8f9f66dd572dfc43252c947a6f34f6f31d91f4
申永范-UX <yf.shen@opencsg.com>
2025-09-11 08:30:51 +0000

    feat(user_settings): enhance profile edit functionality

--- frontend/src/locales/zh_hant_js/rule.js
+++ frontend/src/locales/zh_hant_js/rule.js
@@ -1,7 +1,7 @@
 export const rule = {
   notChangable: "設定後不可變更",
     // nameRule: "2-64个字母数字_.-的字符串，_.-不能并列出现，只能字母开头，字母或者数字结尾",
-    nameRule: "請輸入2~10位的字母、數字組合",
+    nameRule: "請輸入2~10位的字母、數字組合，作為唯一ID",
     collectionNameRule: "2-70個字母數字_.-的字串，_.-不能並列出現，只能字母開頭，字母或者數字結尾",
     lengthLimit: "長度在{min}到{max}字元範圍",
     startWithLetter: "必須以字母開頭",
